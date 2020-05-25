b3be033249ba56eceac7d6a3fea65b1f4efdbc94
Ayush Baid <ayushrakeshbaid@gmail.com>
2020-05-25 19:14:32 -0400

    Adding conda env and module install code

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+"""
+Ref: https://github.com/argoai/argoverse-api/blob/master/setup.py
+A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+from codecs import open  # To use a consistent encoding
+from os import path
+
+# Always prefer setuptools over distutils
+from setuptools import find_packages, setup
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="gtsfm",
+    version="0.1.0",
+    description="",
+    long_description=long_description,
+    url="",
+    author="",
+    author_email="",
+    license="APACHE",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: APACHE License",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    keywords="computer-vision",
+    packages=find_packages(),
+    python_requires=">= 3.6",
+    install_requires=[
+        "pytest"
+    ]
+)
