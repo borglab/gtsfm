8d582d39c5c50cc507f3a44a16fdcc15bd62c87c
Ayush Baid <ayushrakeshbaid@gmail.com>
2020-05-25 19:15:47 -0400

    adding image manipulation utils from DMV

---
+++ utils/image_utils.py
@@ -0,0 +1,33 @@
+"""
+Common utlities for image manipulation
+
+Authors: Ayush Baid
+"""
+import cv2 as cv
+import numpy as np
+
+
+def rgb_to_gray_cv(image: np.array) -> np.array:
+    """
+    RGB to Grayscale converion using opencv
+
+    Args:
+        image (np.array): Input RGB/RGBA image
+
+    Raises:
+        ValueError: wrong input dimensions
+
+    Returns:
+        np.array: grayscale transformed image
+    """
+
+    if len(image.shape) == 2:
+        pass
+    elif image.shape[2] == 4:
+        image = cv.cvtColor(image, cv.COLOR_RGBA2GRAY)
+    elif image.shape[2] == 3:
+        image = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
+    else:
+        raise ValueError('Input image dimensions are wrong')
+
+    return image
