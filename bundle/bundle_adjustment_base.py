45bfa8c48f6fe9ec30d47720ee45598f163d97fe
John Lambert <johnwlambert@gmail.com>
2020-07-22 21:03:57 -0400

    start BA example

--- bundle/bundle_adjustment_base.py
+++ bundle/bundle_adjustment_base.py
@@ -5,10 +5,14 @@ Accepts intrinsics, point-correspondence constraints, and initialization
 of global rotations and translations, to optimize final camera poses
 and point locations.
 
+L and X represent landmark and vehicle/camera poses respectively
+
 Authors: John Lambert
 """
 import abc
 from typing import Dict, List, Tuple
+
+import numpy as np
 
 import gtsam
 from gtsam import symbol_shorthand_L as L
@@ -29,41 +33,47 @@ from gtsam.gtsam import (
 	Values
 )
 
+MEASUREMENT_NOISE = gtsam.noiseModel_Isotropic.Sigma(2, 1.0)  # one pixel in u and v
+
+
 class BundleAdjustmentBase(metaclass=abc.ABCMeta):
-    """Base class for all rotation averaging algorithms."""
+	"""Base class for all rotation averaging algorithms."""
 
-    @abc.abstractmethod
-    def run(
-    	self,
-    	intrinsics: List[np.ndarray],
-    	global_rotations: List[gtsam.Rot3],
-    	global_translations: List[gtsam.Point3],
-    	correspondences: Dict[Tuple[int,int],np.ndarray],
-    	) -> Tuple[List[gtsam.Rot3], np.ndarray]:
-        """
-        Based off of the example in:
-        https://github.com/borglab/gtsam/blob/develop/cython/gtsam/examples/SFMExample.py
-        gtsam/cython/gtsam/examples/VisualISAM2Example.py
+	@abc.abstractmethod
+	def run(
+		self,
+		intrinsics: List[np.ndarray],
+		global_rotations: List[gtsam.Rot3],
+		global_translations: List[gtsam.Point3],
+		correspondence_dict: Dict[Tuple[int,int],np.ndarray],
+		) -> Tuple[List[gtsam.Rot3], np.ndarray]:
+		"""
+		Based off of the example in:
+		https://github.com/borglab/gtsam/blob/develop/cython/gtsam/examples/SFMExample.py
+		gtsam/cython/gtsam/examples/VisualISAM2Example.py
 
-        	Args:
-        		intrinsics: Length N list with 3x3 matrices for each camera ID
-        		global_rotations: Length N list of global rotations
-        			For each image index, we have an
-        			estimated of its global rotation to world frame
-        			TODO: decide if its camera_R_world, world_R_camera
-        		global_translations: Length N list of globbal translations
-        			list index is image index
-        		correspondences: dictionary from (i,j) camera
-        			pair to Nx4 array, with each matrix row
-        			representing x_i,y_i,x_j,y_j
+		Args:
+			intrinsics: Length N list with 3x3 matrices for each camera ID
+				[fx,s,px]
+				[0,fy,py]
+				[0, 0, 1]
+			global_rotations: Length N list of global rotations
+				For each image index, we have an
+				estimated of its global rotation to world frame
+				TODO: decide if its camera_R_world, world_R_camera
+			global_translations: Length N list of globbal translations
+				list index is image index
+			correspondence_dict: dictionary from (i,j) camera
+				pair to Nx4 array, with each matrix row
+				representing x_i,y_i,x_j,y_j
 
-        	Returns:
-        		optimized_poses: output of BA. poses are indexed in the list
-        			by their image index
-				point_cloud: Numpy array of shape Nx3
-        """
-        assert len(intrinsics) == len(global_rotations)
-        assert len(global_rotations) == len(global_translations)
+		Returns:
+			optimized_poses: output of BA. poses are indexed in the list
+				by their image index
+			point_cloud: Numpy array of shape Nx3
+		"""
+		assert len(intrinsics) == len(global_rotations)
+		assert len(global_rotations) == len(global_translations)
 
 		# Create a factor graph
 		graph = NonlinearFactorGraph()
@@ -75,26 +85,38 @@ class BundleAdjustmentBase(metaclass=abc.ABCMeta):
 		factor = PriorFactorPose3(X(0), poses[0], pose_noise)
 		graph.push_back(factor)
 
+		# Create the data structure to hold the initial estimate to the solution
+		initial_estimate = Values()
 
 		# Simulated measurements from each camera pose, adding them to the factor graph
-		for i, K in enumerate(intrinsics):
-			camera_R_world = global_rotations[i]
-			camera_T_world = global_translations[i]
+		for k, K in enumerate(intrinsics):
+			camera_R_world = global_rotations[k]
+			camera_t_world = global_translations[k]
 
-			# TODO: use the actual poses
-		    up = gtsam.Point3(0, 0, 1)
-		    target = gtsam.Point3(0, 0, 0)
-	        position = gtsam.Point3(radius*np.cos(theta),
-	                                radius*np.sin(theta), height)
-	        camera = gtsam.PinholeCameraCal3_S2.Lookat(position, target, up, K)
-	        pose = camera.pose()
+			camera_SE3_world = gtsam.Pose3(
+				gtsam.Rot3(camera_R_world),
+				gtsam.Point3(camera_t_world.flatten())
+			)
+			initial_estimate.insert(X(k), camera_SE3_world)
 
-			camera = PinholeCameraCal3_S2(pose, K)
-			for j, point in enumerate(points):
-				measurement = camera.project(point)
+			camera = PinholeCameraCal3_S2(camera_SE3_world, K)
+
+			# TODO: vectorize data structure so no double-for loop here
+			for (i,j), (x_i, y_i, x_j, y_j) in correspondence_dict.items():
+				if i == k:
+					measurement = np.array([x_i, y_i])
+				if j == k:
+					measurement = np.array([x_j, y_j])
+
 				factor = GenericProjectionFactorCal3_S2(
-				measurement, measurement_noise, X(i), L(j), K)
+					measurement,
+					MEASUREMENT_NOISE,
+					X(k),
+					L(l_idx), # how should we index the landmarks? Need a counter?
+					K
+				)
 				graph.push_back(factor)
+				# initial_estimate.insert(L(l_idx), transformed_point) # Get initial triangulation?
 
 		# Because the structure-from-motion problem has a scale ambiguity, the problem is still under-constrained
 		# Here we add a prior on the position of the first landmark. This fixes the scale by indicating the distance
@@ -104,16 +126,7 @@ class BundleAdjustmentBase(metaclass=abc.ABCMeta):
 		graph.push_back(factor)
 		graph.print_('Factor Graph:\n')
 
-		# Create the data structure to hold the initial estimate to the solution
-		# Intentionally initialize the variables off from the ground truth
-		initial_estimate = Values()
-		for i, pose in enumerate(poses):
-			transformed_pose = pose.retract(0.1*np.random.randn(6,1))
-			initial_estimate.insert(X(i), transformed_pose)
-		for j, point in enumerate(points):
-			transformed_point = Point3(point.vector() + 0.1*np.random.randn(3))
-			initial_estimate.insert(L(j), transformed_point)
-			initial_estimate.print_('Initial Estimates:\n')
+		initial_estimate.print_('Initial Estimates:\n')
 
 		# TODO: Frank -- Is Dogleg preferred? vs L.M.
 		# Optimize the graph and print results
@@ -129,5 +142,6 @@ class BundleAdjustmentBase(metaclass=abc.ABCMeta):
 		optimized_poses = None
 		point_cloud = None
 		return optimized_poses, point_cloud
+
 
 
