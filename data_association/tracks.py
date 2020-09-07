fbc4f0546a66ba0237d694809994536cd8b72d64
Sushmita <swarrier6@gatech.edu>
2020-09-07 00:10:21 -0400

    basic feature tracks implemented

--- data_association/tracks.py
+++ data_association/tracks.py
@@ -2,12 +2,14 @@
 
 Authors: Ayush Baid
 """
-from typing import Dict, List, Tuple
+from typing import DefaultDict, Dict, List, Tuple
 
 import numpy as np
 from collections import defaultdict
 
 import gtsam
+from numpy.core.defchararray import array
+from numpy.core.records import ndarray
 
 
 class MeasurementToIndexMap:
@@ -17,24 +19,29 @@ class MeasurementToIndexMap:
     def __generate_key(self,
                        measurement: Tuple[float, float]) -> Tuple[int, int]:
         # converts float position measurements to int
-        # print("generate key output", tuple(map(lambda x: int(x), measurement)))
         return tuple(map(lambda x: int(x), measurement))
 
     def add(self, measurement: Tuple[float, float]):
         key = self.__generate_key(measurement)
 
         if key not in self.__map:
-            self.__map[key] = len(self.__map) # track length
-            print("map", self.__map)
+            # If new feature, assign it new idx number
+            self.__map[key] = len(self.__map) # feature idx
 
     def add_batch(self, measurements: np.ndarray):
         for row in measurements:
             self.add(tuple(row[:2])) #(x, y) positions
+    
 
     def lookup_index(self, measurement: Tuple[float, float]) -> int:
         key = self.__generate_key(measurement)
-
+        # return value (ie. feature idx) corresponding to feature position
         return self.__map[key]
+
+    def invert_map(self):
+        inv_map = {v: k for k, v in self.__map.items()}
+        return inv_map
+    
 
 
 class FeatureTracks:
@@ -42,34 +49,22 @@ class FeatureTracks:
     def __tracks_to_dsf(self, dsf: gtsam.DSFMapIndexPair) -> List:
         pass
 
-    def __dsf_to_tracks(self, dsf: gtsam.DSFMapIndexPair, key_set: set, matches : Dict[Tuple[int, int], Tuple[np.ndarray, np.ndarray]]) -> List:
-        """
-        Convert dsf to a landmark map
-        Args:
-            dsf (gtsam.DSFMapIndexPair):  DSF of all merged tracks
-            key_set (set(pose_idx, feature_idx)) : Placeholder till gtsam dsf.set is wrapped
-        """
-        landmark_map = defaultdict(list)
-        for (pose_idx, feature_idx) in key_set:
-            # get set representative
-             lndmrk_root = dsf.find(gtsam.IndexPair(pose_idx, feature_idx))
-
 
     def __init__(self,
                  matches: Dict[Tuple[int, int], Tuple[np.ndarray, np.ndarray]],
                  num_poses: int,
                  global_poses: List[gtsam.Pose3]
                  ):
-        self.tracks = []
 
         # Generate the DSF to form tracks
         dsf = gtsam.DSFMapIndexPair()
         key_set = set()
-        print("matches", matches)
+        self.landmark_map = defaultdict(list)
 
         measurement_to_index_maps = [MeasurementToIndexMap()] * num_poses
 
         # assign indices to measures for each camera pose
+        # feature coordinates converted to indices for gtsamIndexPair input
         for (pose_idx_1, pose_idx_2), (features_1, features_2) in matches.items():
             measurement_to_index_maps[pose_idx_1].add_batch(features_1)
             measurement_to_index_maps[pose_idx_2].add_batch(features_2)
@@ -81,16 +76,51 @@ class FeatureTracks:
             for match_idx in range(num_matches):
                 feature_idx_1 = measurement_to_index_maps[pose_idx_1].lookup_index(
                     tuple(features_1[match_idx, :2]))  #lookup indx input: (x,y) for all rows
-                # print("feature_idx1", feature_idx_1)
 
                 feature_idx_2 = measurement_to_index_maps[pose_idx_2].lookup_index(
                     tuple(features_2[match_idx, :2]))
+
 
                 dsf.merge(gtsam.IndexPair(pose_idx_1, feature_idx_1),
                           gtsam.IndexPair(pose_idx_2, feature_idx_2))
                 # Need to expose gtsam set to create key_set
                 key_set.add((pose_idx_1, feature_idx_1))
                 key_set.add((pose_idx_2, feature_idx_2))
-                # print("key set", key_set)
-                ld_map = self.__dsf_to_tracks(dsf, key_set,matches)
                 
+
+        # create a landmark map
+        for key in key_set:
+            pose_idx, feature_idx = key
+            # get set representative- Will be IndexPair type
+            lndmrk_root_node = dsf.find(gtsam.IndexPair(pose_idx, feature_idx))
+            landmark_key = (lndmrk_root_node.i(), lndmrk_root_node.j())
+            # for each representative, add (img_idx, feature)
+            # feature is extracted from feature_idx by inverting dict mapping feature coordinates to idx
+            feature_dict = measurement_to_index_maps[pose_idx].invert_map()
+            self.landmark_map[landmark_key].append((pose_idx, feature_dict[feature_idx]))
+
+def toy_case():
+    matches = dict()
+    key_list = [(0,1), (0,2), (1,2)]
+    matches_list = [
+        (
+            np.array([[1,3,2], [4,6,2], [9,8,1]]),
+            np.array([[8,2,2], [5,10,2], [11,12,2]])
+        ), 
+        (
+            np.array([[4,6,2]]),
+            np.array([[12,14,1]])
+        ),
+        (
+            np.array([[8,2,2], [4,1,5]]), 
+            np.array([[13,16,2],[8,1,3]])
+        )]
+    for i,key in enumerate(key_list):
+        matches[key] = matches_list[i]
+    return matches
+
+                
+if __name__ == "__main__":
+    dummy_matches = toy_case()
+    # for a sanity check
+    FT = FeatureTracks(dummy_matches, len(dummy_matches), None)
---
+++ tests/data_association/test_data_association.py
@@ -0,0 +1,142 @@
+"""Unit test for the DataAssociation class.
+
+Authors:Sushmita Warrier
+"""
+from random import uniform
+import unittest
+
+import dask
+import numpy as np
+import gtsam
+
+import utils.io as io_utils
+from data_association.data_assoc import DataAssociation
+from data_association.tracks import FeatureTracks, toy_case
+from frontend.matcher.dummy_matcher import DummyMatcher
+
+
+class TestDataAssociation(unittest.TestCase):
+    """
+    Unit tests for data association, which maps the feature tracks to their 3D landmarks.
+    """
+
+    def setUp(self):
+        """
+        Set up the data association module.
+        """
+        super().setUp()
+
+        # set up ground truth data for comparison
+
+        self.da = DataAssociation()
+        self.matcher = DummyMatcher()
+    
+    def test_track(self):
+        """
+        Tests that the tracks are being merged and mapped correctly
+        """
+        dummy_matches = toy_case()
+        self.track = FeatureTracks(dummy_matches, len(dummy_matches), None)
+        # len(track) value for toy case strictly
+        assert len(self.track.landmark_map) == 4, "tracks incorrectly mapped"
+        
+    
+    def test_filtering(self):
+        """
+        Tests that the tracks are being filtered correctly
+        """
+        pass
+
+
+    def test_create_computation_graph(self):
+        """
+        Tests the graph to create data association for images
+        """
+
+        correspondences = self.get_random_correspondences()  # gives matches for 3 images only
+
+        list_absolute_t = [gtsam.Point3(
+            np.random.uniform(100.2, 120.5), 
+            np.random.uniform(150.9, 140.5), 
+            np.random.uniform(12.2, 20.5)) for _ in range(len(correspondences))]
+
+        list_absolute_R = [gtsam.Rot3(1.0, 0.0, 0.0, 
+                                      0.0, 1.0, 0.0, 
+                                      0.0, 0.0, 1.0)] # first one is identity
+        for _ in range(1, len(correspondences)):
+            list_absolute_R.append(gtsam.Rot3(
+                np.random.rand(), np.random.rand(), np.random.rand(), 
+                np.random.rand(), np.random.rand(), np.random.rand(),
+                np.random.rand(), np.random.rand(), np.random.rand()))
+
+        pose_list = [gtsam.Pose3(R, t) for R, t in zip(list_absolute_R, list_absolute_t)]
+
+        self.track_list = FeatureTracks(correspondences, len(correspondences), pose_list)
+        
+
+    def __generate_random_binary_descriptors(self, num_descriptors: int, descriptor_length: int) -> np.ndarray:
+        """
+        Generates random binary descriptors.
+
+        Args:
+            num_descriptors (int): number of descriptors to generate
+            descriptor_length (int): length of each descriptor vector
+
+        Returns:
+            np.ndarray: generated descriptor
+        """
+        if num_descriptors == 0:
+            return np.array([], dtype=np.uint8)
+
+        return np.random.randint(
+            0, high=2, size=(num_descriptors, descriptor_length)
+        ).astype(np.uint8)
+
+    def get_random_correspondences(self):
+        """
+        Gets random verified correspondences
+        """
+        # generate three random descriptors and their features
+        num_descriptors_im1 = np.random.randint(5, 15)
+        num_descriptors_im2 = np.random.randint(5, 15)
+        num_descriptors_im3 = np.random.randint(5, 15)
+        print("num descr", num_descriptors_im1, num_descriptors_im2, num_descriptors_im3)
+
+        descriptor_length = np.random.randint(2, 10)
+        # print("descr length", descriptor_length)
+
+        descriptor_list = [
+            self.__generate_random_binary_descriptors(
+                num_descriptors_im1, descriptor_length),
+            self.__generate_random_binary_descriptors(
+                num_descriptors_im2, descriptor_length),
+            self.__generate_random_binary_descriptors(
+                num_descriptors_im3, descriptor_length)
+        ]
+
+        # print("descr list", descriptor_list[0])
+
+        features_list = [
+            np.random.rand(num_descriptors_im1, 3),
+            np.random.rand(num_descriptors_im2, 3),
+            np.random.rand(num_descriptors_im3, 3),
+        ]
+
+        # create computation graphs
+        detection_description_graph = [
+            dask.delayed((x, y)) for x, y in zip(features_list, descriptor_list)
+        ]
+
+        matcher_graph = self.matcher.create_computation_graph(
+            detection_description_graph)
+        
+        # run it in sequential mode
+        results = []
+        with dask.config.set(scheduler='single-threaded'):
+            results = dask.compute(matcher_graph)[0]
+
+        return results
+
+        
+if __name__ == "__main__":
+    unittest.main()
