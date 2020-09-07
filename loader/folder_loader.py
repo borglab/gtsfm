a64ff124acf3e2aacb3bfa112045359bd28ab074
Ayush Baid <ayushrakeshbaid@gmail.com>
2020-09-06 22:59:06 -0400

    Adding intrinsics and extrinsics support to folder loader

--- loader/folder_loader.py
+++ loader/folder_loader.py
@@ -6,13 +6,26 @@ Authors: Frank Dellaert and Ayush Baid
 import glob
 import os
 
+import numpy as np
+
 import utils.io as io_utils
 from common.image import Image
 from loader.loader_base import LoaderBase
 
 
 class FolderLoader(LoaderBase):
-    """Simple loader class that reads from a folder on disk."""
+    """Simple loader class that reads from a folder on disk.
+
+    Folder layout structure:
+    - RGB Images: images/
+    - Intrinsics data (optional): intrinsics/ 
+        - numpy arrays with the same name as images
+    - Extrinsics data (optional): extrinsics/
+        - numpy array with the same name as images
+
+
+    If explicit intrinsics are not provided, the exif data will be used.
+    """
 
     def __init__(self, folder: str, image_extension: str = 'jpg'):
         """
@@ -31,10 +44,42 @@ class FolderLoader(LoaderBase):
             folder, 'images', '*.{}'.format(image_extension)
         )
 
-        self.file_names = glob.glob(search_path)
+        self.image_paths = glob.glob(search_path)
 
         # sort the file names
-        self.file_names.sort()
+        self.image_paths.sort()
+
+        # check if intrisincs are available as numpy arrays
+        explicit_intrinsics_template = os.path.join(
+            folder, 'intrinsics', '{}.npy'
+        )
+
+        self.explicit_intrinsics_paths = []
+        for image_file_name in self.image_paths:
+            file_path = explicit_intrinsics_template.format(
+                os.path.splitext(os.path.basename(image_file_name))[0]
+            )
+            if not os.path.exists(file_path):
+                self.explicit_intrinsics_paths = []
+                break
+            else:
+                self.explicit_intrinsics_paths.append(file_path)
+
+        # check if extrinsics are available as numpy arrays
+        explicit_extrinsics_template = os.path.join(
+            folder, 'extrinsics', '{}.npy'
+        )
+
+        self.explicit_extrinsics_paths = []
+        for image_file_name in self.image_paths:
+            file_path = explicit_extrinsics_template.format(
+                os.path.basename(image_file_name).split('.')[0]
+            )
+            if not os.path.exists(file_path):
+                self.explicit_extrinsics_template = []
+                break
+            else:
+                self.explicit_extrinsics_paths.append(file_path)
 
     def __len__(self) -> int:
         """
@@ -43,7 +88,7 @@ class FolderLoader(LoaderBase):
         Returns:
             int: the number of images in the folder
         """
-        return len(self.file_names)
+        return len(self.image_paths)
 
     def get_image(self, index: int) -> Image:
         """
@@ -62,4 +107,50 @@ class FolderLoader(LoaderBase):
         if index < 0 or index > self.__len__():
             raise IndexError("Image index is invalid")
 
-        return io_utils.load_image(self.file_names[index])
+        return io_utils.load_image(self.image_paths[index])
+
+    def get_geometry(self, idx1: int, idx2: int) -> np.ndarray:
+        """Get the ground truth fundamental matrix/homography from idx1 to idx2.
+
+        The function returns either idx1_F_idx2 or idx1_H_idx2
+
+        Args:
+            idx1 (int): one of the index
+            idx2 (int): one of the index
+
+        Returns:
+            np.ndarray: fundamental matrix/homograph matrix
+        """
+
+        return None
+
+    def get_camera_intrinsics(self, index: int) -> np.ndarray:
+        """Get the camera intrinsics at the given index.
+
+        Args:
+            index (int): the index to fetch
+
+        Returns:
+            np.ndarray: the 3x3 intrinsics matrix of the camera
+        """
+        if len(self.explicit_intrinsics_paths) == 0:
+            # get intrinsics from exif
+
+            return io_utils.load_image(self.image_paths[index]).get_intrinsics_from_exif()
+
+        else:
+            return np.load(self.explicit_intrinsics_paths[index])
+
+    def get_camera_extrinsics(self, index: int) -> np.ndarray:
+        """Get the camera extrinsics (pose) at the given index.
+
+        The extrinsics format is [wRc, wTc]
+
+        Args:
+            index (int): the index to fetch
+
+        Returns:
+            np.ndarray: the 3x4 extrinsics matrix of the camer
+        """
+        if self.explicit_extrinsics_paths:
+            return np.load(self.explicit_extrinsics_paths[index])
