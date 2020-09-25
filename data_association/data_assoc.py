08e33dc25cefc3adf31c7848cca85a7f6ef45561
Sushmita <swarrier6@gatech.edu>
2020-09-25 12:35:29 -0400

    triangulation_fragment

--- data_association/data_assoc.py
+++ data_association/data_assoc.py
@@ -1,5 +1,5 @@
 import abc
-from typing import DefaultDict, Dict, List, Tuple
+from typing import DefaultDict, Dict, List, Tuple, Bool
 
 import dask
 import numpy as np
@@ -15,9 +15,19 @@ class LandmarkInitialization(metaclass=abc.ABCMeta):
     Class to initialize landmark points via triangulation
     """
 
-    def __init__(self, calibration, global_poses: List[gtsam.Pose3]) -> None:
-        self.__calibration = calibration
+    def __init__(self, calibrationFlag: Bool, global_poses: List[gtsam.Pose3]) -> None:
+        self.sharedCal_Flag = calibrationFlag
         self.__global_pose_list = global_poses
+    
+    def triangulate_landmark(self, calibration, img_measurements):
+        """
+        calibration: Can be sharedCal or array of individual camera calibrations
+        img_measurements: List of observations (Point2)
+        """
+        # if shared calibration provided for all cameras
+        if self.sharedCal_Flag:
+            return gtsam.triangulatePoint3(self.__global_pose_list, calibration, img_measurements)
+        return gtsam.triangulatePoint3()
     
 
 
