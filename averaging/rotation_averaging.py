d646d28a14c965ef2d879f5602c41ee5ef02ef75
jingwuOUO <wujing2951@gmail.com>
2020-09-28 02:59:44 -0400

    feat(ra): convert in/out type into dask required

--- averaging/rotation_averaging.py
+++ averaging/rotation_averaging.py
@@ -16,20 +16,31 @@ class RotationAveraging():
     Class for calling ShonanAveraging algorithm in gtsam
     """
 
-    def __init__(self, measurements: ShonanAveraging3.Measurements, params: ShonanAveraging3.Parameters):
+    def __init__(self, pose_num: int, measurements: dask.delayed(dict((int, int), (np.array, np.array, np.array)))):
         """
-        Initialize shonan from measurements and parameters.
+        Initialize shonan from measurements.
 
         Args:
             measurements (ShonanAveraging3.Measurements): the measurements
             params (ShonanAveraging3.Parameters): the parameters for shonan
         """
-        self.params = params
-        self.shonan = ShonanAveraging3(measurements, params)
+        shonan_measurements = []
+        for key, item in measurements.items():
+            # TODO(Jing): how to get the noise model
+            noise_model = item[2]
+            # TODO(Jing): make sure the correctness of converting key and rotation into binary_measurement_rot3 type
+            binary_measurements_rot3 = gtsam.BinaryMeasurementRot3(
+                key[0], key[1], item[0], gtsam.noiseModel.Gaussian.Covariance(noise_model[:3, :3], True))
+            shonan_measurements.append(binary_measurements_rot3)
+
+        self.params = ShonanAveraging3.ShonanAveragingParameters()
+        self.pose_num = pose_num
+        self.shonan_measurements = shonan_measurements
+        self.shonan = ShonanAveraging3(shonan_measurements, self.params)
         self._pMin = 5
         self._pMax = 30
 
-    def run(self, intial: dask.delayed(Values)) -> tuple(dask.delayed(Values), dask.delayed(np.array)):
+    def run(self) -> dask.delayed(dict((int, int)), (np.array))):
         """
         Really run Shonan
 
@@ -41,4 +52,20 @@ class RotationAveraging():
             Values: the result estimation in Rot3 type
             np.ndarray: the descriptors for the input features
         """
-        return dask.delayed(self.shonan.run)(initial, self._pMin, self._pMax)
+        initial = gtsam.Values()
+        for i in range(self.pose_num):
+            initial.insert(i, self.random_Rot3())
+        result = self.shonan.run(initial, self._pMin, self._pMax)
+        #TODO(Jing): convert result into dask delayed type
+        return dask.delayed(result)
+
+    def random_Rot3(self):
+        """
+        Create random Values in ROt3 type
+
+        Returns:
+            Value: one estimation in Rot3 type
+        """
+        u = np.random.randn(3)
+        theta = np.random.uniform(-np.pi, np.pi)
+        return gtsam.Rot3.Rodrigues(u*(theta/np.linalg.norm(u)))
