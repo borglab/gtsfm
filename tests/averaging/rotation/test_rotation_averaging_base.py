5553da066ace77476057f1570f204f8838ceecb5
Ayush Baid <ayushrakeshbaid@gmail.com>
2020-10-04 16:37:35 -0400

    adding a feasible test case as that global rotations can be computed

--- tests/averaging/rotation/test_rotation_averaging_base.py
+++ tests/averaging/rotation/test_rotation_averaging_base.py
@@ -22,26 +22,15 @@ class TestRotationAveragingBase(unittest.TestCase):
         self.obj = DummyRotationAveraging()
 
     def test_computation_graph(self):
-        """Test the dask computation graph execution."""
+        """Test the dask computation graph execution using a valid collection
+        of relative poses."""
 
-        num_poses = 5
+        num_poses = 3
 
-        # pose indices for relative rotation input
-        pose_pairs_ij = [
-            (0, 1),
-            (0, 2),
-            (1, 3),
-            (3, 4),
-            (2, 4)
-        ]
-
-        iRj_dict = dict()
-
-        # generate random relative rotations
-        for i, j in pose_pairs_ij:
-            random_vector = np.random.rand(3)*2*np.pi
-            iRj_dict[(i, j)] = Rot3.Rodrigues(
-                random_vector[0], random_vector[1], random_vector[2])
+        iRj_dict = {
+            (0, 1): Rot3.RzRyRx(0, 30*np.pi/180, 0),
+            (1, 2): Rot3.RzRyRx(0, 0, 20*np.pi/180),
+        }
 
         # use the normal API for rotation averaging
         normal_result = self.obj.run(num_poses, iRj_dict)
@@ -53,8 +42,9 @@ class TestRotationAveragingBase(unittest.TestCase):
         )
 
         with dask.config.set(scheduler='single-threaded'):
-            results = dask.compute(computation_graph)[0]
+            dask_result = dask.compute(computation_graph)[0]
 
         # compare the two entries
-        for idx in range(num_poses):
-            self.assertTrue(normal_result[idx].equals(results[idx], 1e-5))
+        for idx in range(1, num_poses):
+            self.assertTrue(normal_result[0].between(normal_result[idx]).equals(
+                dask_result[0].between(dask_result[idx]), 1e-5))
