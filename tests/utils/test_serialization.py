6e15ad2348af13ce52fe4c3044e96eb92aae353e
Ayush Baid <ayushrakeshbaid@gmail.com>
2020-10-10 02:21:52 -0400

    Adding serialization support for Rot3

---
+++ tests/utils/test_serialization.py
@@ -0,0 +1,24 @@
+"""Unit tests for custom serialization methods.
+
+Authors: Ayush Baid
+"""
+import unittest
+
+from gtsam import Rot3
+
+import utils.serialization as serialization_utils
+
+
+class TestSerialization(unittest.TestCase):
+    """Unit tests for custom serialization for GTSAM types."""
+
+    def test_rot3_roundtrip(self):
+        """Test the round-trip on Rot3 object."""
+
+        expected = Rot3.RzRyRx(0, 0.05, 0.1)
+
+        header, frames = serialization_utils.serialize_Rot3(expected)
+
+        recovered = serialization_utils.deserialize_Rot3(header, frames)
+
+        self.assertTrue(expected.equals(recovered, 1e-5))
---
+++ utils/serialization.py
@@ -0,0 +1,43 @@
+"""Functions to provide serialization support for GTSAM types.
+
+Authors: Ayush Baid
+"""
+from typing import Dict, List, Tuple
+
+from distributed.protocol import dask_deserialize, dask_serialize
+from gtsam import Rot3
+
+
+@dask_serialize.register(Rot3)
+def serialize_Rot3(rot3: Rot3) -> Tuple[Dict, List[bytes]]:
+    """Serialize Rot3 instance.
+
+    Args:
+        rot3: rotation instance to serielize.
+
+    Returns:
+        Tuple[Dict, List[bytes]]: Serialized data.
+    """
+    header = {}
+    frames = [bytes(rot3.serialize(), 'utf-8')]
+
+    return header, frames
+
+
+@ dask_deserialize.register(Rot3)
+def deserialize_Rot3(header: Dict, frames: List[bytes]) -> Rot3:
+    """Deserialize bytes into Rot3 instance.
+
+    Args:
+        header: Header of the serialized data.
+        frames: list of bytes in the serialized data.
+
+    Returns:
+        Rot3: deserialized instance
+    """
+    serialized_str = frames[0].decode('utf-8')
+
+    r = Rot3()
+    r.deserialize(serialized_str)
+
+    return r
