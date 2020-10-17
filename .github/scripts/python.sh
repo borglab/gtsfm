0f66c7e69da7bd8e4848978abdc5afb2618b7259
John Lambert <johnlambert@gatech.edu>
2020-10-17 01:32:40 -0400

    fix missing pyparsing dependency

--- .github/scripts/python.sh
+++ .github/scripts/python.sh
@@ -4,13 +4,11 @@
 # Build the GTSAM Python wrapper, then run GTSFM unit tests
 ##########################################################
 
-# Install a system package required by our library
-# maybe fix pyparsing error?
-sudo apt-get install -y wget libicu-dev python3-pip python3-setuptools
 
 git clone https://github.com/borglab/gtsam.git
 cd gtsam
-sudo $PYTHON -m pip install -r python/requirements.txt
+# install pyparsing
+pip install -r python/requirements.txt
 
 mkdir build
 cd build
