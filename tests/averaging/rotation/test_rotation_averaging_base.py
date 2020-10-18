bb8814717f887b59fe3bcd8d430f3554420abcb7
Ayush Baid <ayushrakeshbaid@gmail.com>
2020-10-18 00:16:46 -0400

    correcting variable names

--- tests/averaging/rotation/test_rotation_averaging_base.py
+++ tests/averaging/rotation/test_rotation_averaging_base.py
@@ -26,18 +26,18 @@ class TestRotationAveragingBase(unittest.TestCase):
 
         num_poses = 3
 
-        i1Ri2_dict = {
+        i1_R_i2_dict = {
             (0, 1): Rot3.RzRyRx(0, np.deg2rad(30), 0),
             (1, 2): Rot3.RzRyRx(0, 0, np.deg2rad(20)),
         }
 
         # use the GTSAM API directly (without dask) for rotation averaging
-        expected_result = self.obj.run(num_poses, i1Ri2_dict)
+        expected_result = self.obj.run(num_poses, i1_R_i2_dict)
 
         # use dask's computation graph
         computation_graph = self.obj.create_computation_graph(
             num_poses,
-            dask.delayed(i1Ri2_dict)
+            dask.delayed(i1_R_i2_dict)
         )
 
         with dask.config.set(scheduler='single-threaded'):
@@ -45,6 +45,6 @@ class TestRotationAveragingBase(unittest.TestCase):
 
         # compare the two entries
         for idx in range(1, num_poses):
-            expected_i1Ri2 = expected_result[0].between(expected_result[idx])
-            computed_i1Ri2 = dask_result[0].between(dask_result[idx])
-            self.assertTrue(expected_i1Ri2.equals(computed_i1Ri2), 1e-5))
+            expected_i1_R_i2 = expected_result[0].between(expected_result[idx])
+            computed_i1_R_i2 = dask_result[0].between(dask_result[idx])
+            self.assertTrue(expected_i1_R_i2.equals(computed_i1_R_i2, 1e-5))
