51edf87184911cdce28343c84ba06cd7d7bec32a
Ayush Baid <ayushrakeshbaid@gmail.com>
2020-10-17 21:56:53 -0400

    renaming conversion functions and improving docstring

--- utils/features.py
+++ utils/features.py
@@ -8,35 +8,38 @@ import cv2 as cv
 import numpy as np
 
 
-def keypoints_of_array(features: np.ndarray) -> List[cv.KeyPoint]:
+def keypoints_from_array(features: np.ndarray) -> List[cv.KeyPoint]:
     """Converts the features from numpy array to cv keypoints.
 
     Args:
-        features: Numpy array of shape (??,??) representing ...
+        features: Numpy array of shape (N,2+) representing feature points.
 
     Returns:
-        OpenCV KeyPoint objects representing the given features
+        OpenCV KeyPoint objects representing the given features.
     """
     # TODO(ayush): what should be scale if not provided?
 
     # input features is a 2D array
     if features.shape[1] < 3:
         keypoints = [cv.KeyPoint(x=f[0], y=f[1], _size=2) for f in features]
-    else:
+    elif features.shape[1] < 4:
         keypoints = [cv.KeyPoint(x=f[0], y=f[1], _size=f[2]) for f in features]
+    else:
+        keypoints = [cv.KeyPoint(
+            x=f[0], y=f[1], _size=f[2], _response=f[3]) for f in features]
 
     return keypoints
 
 
-def array_of_keypoints(keypoints: List[cv.KeyPoint]) -> np.ndarray:
-    """Converts the opencv keypoints to a numpy array, the standard feature 
+def array_from_keypoints(keypoints: List[cv.KeyPoint]) -> np.ndarray:
+    """Converts the opencv keypoints to a numpy array, the standard feature
     representation in GTSFM.
 
     Args:
-        keypoints: OpenCV's keypoint representation of the given features
+        keypoints: OpenCV's keypoint representation of the given features.
 
     Returns:
-        Array of shape (???,???) representing features 
+        Array of shape (N, 4) representing features.
     """
 
     feat_list = [[kp.pt[0], kp.pt[1], kp.size, kp.response] for kp in keypoints]
