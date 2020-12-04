da8c20382c507c547101c6aacff0da4d1970b50a
Ayush Baid <ayushrakeshbaid@gmail.com>
2020-12-03 23:57:03 -0500

    removing unrelated changes

--- utils/features.py
+++ utils/features.py
@@ -11,38 +11,6 @@ from gtsam import Cal3Bundler
 from common.keypoints import Keypoints
 
 
-def keypoints_from_array(features: np.ndarray) -> List[cv.KeyPoint]:
-    """Converts the features from numpy array to cv keypoints.
-
-    Args:
-        features: Numpy array of shape (N,2+) representing feature points.
-
-    Returns:
-        OpenCV KeyPoint objects representing the given features.
-    """
-    # TODO(ayush): what should be scale if not provided?
-
-    # input features is a 2D array
-    if features.shape[1] < 3:
-        keypoints = [cv.KeyPoint(
-            x=float(f[0]),
-            y=float(f[1]),
-            _size=2) for f in features]
-    elif features.shape[1] < 4:
-        keypoints = [cv.KeyPoint(
-            x=float(f[0]),
-            y=float(f[1]),
-            _size=float([2])) for f in features]
-    else:
-        keypoints = [cv.KeyPoint(
-            x=float(f[0]),
-            y=float(f[1]),
-            _size=float(f[2]),
-            _response=float(f[3])) for f in features]
-
-    return keypoints
-
-
 def cast_to_gtsfm_keypoints(keypoints: List[cv.KeyPoint]) -> Keypoints:
     """Cast list of OpenCV's keypoints to GTSFM's keypoints.
 
