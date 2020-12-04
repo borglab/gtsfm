fixing translation averaging results comparison

tests/averaging/translation/test_translation_averaging_base.py
@@ -8,7 +8,7 @@ from typing import List
 
 import dask
 import numpy as np
-from gtsam import Point3, Rot3, Unit3
+from gtsam import Pose3, Rot3, Unit3
 
 from averaging.translation.dummy_translation_averaging import \
     DummyTranslationAveraging
@@ -27,20 +27,55 @@ class TestTranslationAveragingBase(unittest.TestCase):
         self.obj = DummyTranslationAveraging()
 
     def assert_equal_upto_scale(self,
-                                wti_list1: List[Point3],
-                                wti_list2: List[Point3]):
-        """Helper function to assert that two lists of global Point3 are equal
-        (upto global scale ambiguity)."""
+                                wTi_list1: List[Pose3],
+                                wTi_list2: List[Pose3]):
+        """Helper function to assert that two lists of global Pose3 are equal,
+        upto global pose ambiguity."""
 
-        self.assertEqual(len(wti_list1), len(wti_list2),
+        self.assertEqual(len(wTi_list1), len(wTi_list2),
                          'two lists to compare have unequal lengths')
 
-        for i in range(1, len(wti_list1)):
-            # accounting for ambiguity in origin of the coordinate system.
-            iU0_1 = Unit3(wti_list1[i] - wti_list1[0])
+        # get a referenc pose to resolve ambiguity
+        reference_idx = -1
+        for i in range(len(wTi_list1)):
+            if wTi_list1[i] is not None and wTi_list2[i] is not None:
+                reference_idx = i
+                break
 
-            iU0_2 = Unit3(wti_list2[i] - wti_list2[0])
-            self.assertTrue(iU0_1.equals(iU0_2, 1e-1))
+        if reference_idx == -1:
+            # all entries in the list should be none
+            for pose in wTi_list1:
+                self.assertIsNone(pose)
+
+            for pose in wTi_list2:
+                self.assertIsNone(pose)
+
+            return
+
+        scale_factor_2to1 = None
+
+        for i in range(len(wTi_list1)):
+            if i == reference_idx:
+                continue
+
+            if wTi_list1[i] is None:
+                self.assertIsNone(wTi_list2[i])
+            else:
+                pose_1 = wTi_list1[i].between(wTi_list1[reference_idx])
+                pose_2 = wTi_list2[i].between(wTi_list2[reference_idx])
+
+            if scale_factor_2to1 is None:
+                # resolve the scale factor by using one measurement
+                scale_factor_2to1 = np.linalg.norm(pose_1.translation()) /\
+                    (np.linalg.norm(pose_2.translation()) + np.finfo(float).eps)
+
+            # assert equality upto scale
+            self.assertTrue(pose_1.rotation().equals(pose_2.rotation(), 1e-3))
+            np.testing.assert_allclose(
+                pose_1.translation(),
+                pose_2.translation()*scale_factor_2to1,
+                atol=1e-1,
+                rtol=1e-1)
 
     def test_computation_graph(self):
         """Test the dask computation graph execution using a valid collection
@@ -65,6 +100,9 @@ class TestTranslationAveragingBase(unittest.TestCase):
 
         # use the GTSAM API directly (without dask) for translation averaging
         expected_wti_list = self.obj.run(num_images, i2Ui1_dict, wRi_list)
+        expected_wTi_list = [Pose3(wRi, wti)
+                             if wti is not None else None
+                             for (wRi, wti) in zip(wRi_list, expected_wti_list)]
 
         # use dask's computation graph
         computation_graph = self.obj.create_computation_graph(
@@ -73,7 +111,11 @@ class TestTranslationAveragingBase(unittest.TestCase):
         with dask.config.set(scheduler='single-threaded'):
             wti_list = dask.compute(computation_graph)[0]
 
-        self.assert_equal_upto_scale(expected_wti_list, wti_list)
+        wTi_list = [Pose3(wRi, wti)
+                    if wti is not None else None
+                    for (wRi, wti) in zip(wRi_list, wti_list)]
+
+        self.assert_equal_upto_scale(wTi_list, expected_wTi_list)
 
     def test_pickleable(self):
         """Tests that the object is pickleable (required for dask)."""