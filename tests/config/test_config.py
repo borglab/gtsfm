4da72a56570c9d047fd484b04d85fe0cf92ba245
John Lambert <johnlambert@gatech.edu>
2020-12-15 22:47:42 -0500

    reformat using Python black and make separate assertTrue statements

--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -1,10 +1,12 @@
-from utils.cfgnode import CfgNode, ArgsCfgNode, YACS
-from config.default import get_cfg_defaults
-import unittest
 
+import unittest
 from pathlib import Path
 
-CONFIG_PATH = Path(__file__).resolve().parent.parent.parent / 'config'
+from utils.cfgnode import CfgNode, ArgsCfgNode, YACS
+from config.defaults import get_cfg_defaults
+
+CONFIG_PATH = Path(__file__).resolve().parent.parent.parent / "config"
+
 
 class TestCfgNode(unittest.TestCase):
     """ Main test for Configuration management applet """
@@ -14,50 +16,42 @@ class TestCfgNode(unittest.TestCase):
 
     def test_configuration_type(self):
         """ Test that correct types of input hyperparameters are read from 1 yaml file """
-        
-        self.config = CfgNode(
-            get_cfg_defaults()
-        )
-        self.config.load_file(str(CONFIG_PATH) + '/config1.yaml')
-        
-        self.assertTrue(
-            (isinstance(self.config.param.FeatureExtractor,                         YACS))   and
-            (isinstance(self.config.param.FeatureExtractor.matching,                YACS))   and
-            (isinstance(self.config.param.SceneOptimizer.path,                       str))    and
-            (isinstance(self.config.param.SceneOptimizer.enable_gpu,                 bool))   and
-            (isinstance(self.config.param.FeatureExtractor.matching.num_features,   int))    and
-            (isinstance(self.config.param.FeatureExtractor.matching.tol,            float))  and
-            (isinstance(self.config.param.SceneOptimizer.image_path,                 list))   and
-            (isinstance(self.config.param.FeatureExtractor.matching.pyramid_scale,  list))
-        )
+
+        self.config = CfgNode(get_cfg_defaults())
+        self.config.load_file(str(CONFIG_PATH) + "/config1.yaml")
+
+        self.assertTrue(isinstance(self.config.param.FeatureExtractor, YACS))
+        self.assertTrue(isinstance(self.config.param.FeatureExtractor.matching, YACS))
+        self.assertTrue(isinstance(self.config.param.SceneOptimizer.path, str))
+        self.assertTrue(isinstance(self.config.param.SceneOptimizer.enable_gpu, bool))
+        self.assertTrue(isinstance(self.config.param.FeatureExtractor.matching.num_features, int))
+        self.assertTrue(isinstance(self.config.param.FeatureExtractor.matching.tol, loat))
+        self.assertTrue(isinstance(self.config.param.SceneOptimizer.image_path, list))
+        self.assertTrue(isinstance(self.config.param.FeatureExtractor.matching.pyramid_scale, list))
 
     def test_configuration_value(self):
         """ Test whether values of input hyperparameters are correctly read from 1 yaml file """
         self.config = CfgNode(get_cfg_defaults())
-        self.config.load_file(str(CONFIG_PATH) + '/config1.yaml')
-        
-        self.assertTrue(
-            (self.config.param.SceneOptimizer.path == '~/gtsfm')                               and
-            (self.config.param.SceneOptimizer.enable_gpu == False)                             and
-            (self.config.param.FeatureExtractor.matching.num_features == 2000)                and
-            (self.config.param.FeatureExtractor.matching.tol == 1e-3)                         and
-            (self.config.param.SceneOptimizer.image_path == ['images1', 'images2', 'images3']) and
-            (self.config.param.FeatureExtractor.matching.pyramid_scale == [1, 2, 4])
-        )
+        self.config.load_file(str(CONFIG_PATH) + "/config1.yaml")
+
+        self.assertTrue(self.config.param.SceneOptimizer.path == "~/gtsfm")
+        self.assertTrue(self.config.param.SceneOptimizer.enable_gpu == False)
+        self.assertTrue(self.config.param.FeatureExtractor.matching.num_features == 2000)
+        self.assertTrue(self.config.param.FeatureExtractor.matching.tol == 1e-3)
+        self.assertTrue(self.config.param.SceneOptimizer.image_path == ["images1", "images2", "images3"])
+        self.assertTrue(self.config.param.FeatureExtractor.matching.pyramid_scale == [1, 2, 4])
 
     def test_configuration_combination(self):
         """ Test whether 2 configuration files can be correctly merged and loaded together """
         self.config = CfgNode(get_cfg_defaults())
-        self.config.load_file(str(CONFIG_PATH) + '/config1.yaml')
-        self.config.load_file(str(CONFIG_PATH) + '/config2.yaml')
+        self.config.load_file(str(CONFIG_PATH) + "/config1.yaml")
+        self.config.load_file(str(CONFIG_PATH) + "/config2.yaml")
 
-        self.assertTrue(
-            (self.config.param.FeatureExtractor.matching.num_features == 1000)          and
-            (self.config.param.FeatureExtractor.matching.deep_feature == 'superpoint')  and
-            (self.config.param.TwoViewEstimator.mode_triangulation == 'ransac')          and
-            (self.config.param.MultiViewOptimizer.enable_vis == True)
-        )
-    
+        self.assertTrue(self.config.param.FeatureExtractor.matching.num_features == 1000)
+        self.assertTrue(self.config.param.FeatureExtractor.matching.deep_feature == "superpoint")
+        self.assertTrue(self.config.param.TwoViewEstimator.mode_triangulation == "ransac")
+        self.assertTrue(self.config.param.MultiViewOptimizer.enable_vis == True)
+
+
 if __name__ == "__main__":
     unittest.main()
-
