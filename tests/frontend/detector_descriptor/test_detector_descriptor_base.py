c2e5eab096d3080ab3526439a198296171944fb4
John Lambert <johnlambert@gatech.edu>
2020-12-16 22:22:48 -0500

    update DetectorDescriptor to be a single plate

--- tests/frontend/detector_descriptor/test_detector_descriptor_base.py
+++ tests/frontend/detector_descriptor/test_detector_descriptor_base.py
@@ -8,6 +8,7 @@ import unittest
 import dask
 import numpy as np
 
+from common.keypoints import Keypoints
 import tests.frontend.detector.test_detector_base as test_detector_base
 from frontend.descriptor.dummy_descriptor import DummyDescriptor
 from frontend.detector.detector_from_joint_detector_descriptor import \
@@ -59,35 +60,38 @@ class TestDetectorDescriptorBase(test_detector_base.TestDetectorBase):
         """Test the dask's computation graph formation."""
 
         image_graph = self.loader.create_computation_graph_for_images()
-        detection_graph, description_graph = \
-            self.detector_descriptor.create_computation_graph(image_graph)
-
-        with dask.config.set(scheduler='single-threaded'):
-            # TODO(ayush): check how many times detection is performed
-            detection_results = dask.compute(detection_graph)[0]
-            description_results = dask.compute(description_graph)[0]
-
-        # check the number of entries in results
-        self.assertEqual(
-            len(detection_results), len(self.loader),
-            "Dask workflow does not return the same number of results"
-        )
-        self.assertEqual(
-            len(description_results), len(self.loader),
-            "Dask workflow does not return the same number of results"
-        )
-
-        # compare the result values for a particular image
-        idx_under_test = 0
-
-        # check the results via normal workflow and dask workflow for an image
-        expected_kps, expected_descs = \
-            self.detector_descriptor.detect_and_describe(
-                self.loader.get_image(idx_under_test))
-
-        self.assertEqual(detection_results[idx_under_test], expected_kps)
-        np.testing.assert_array_equal(
-            description_results[idx_under_test], expected_descs)
+        for i, delayed_image in enumerate(image_graph):
+            det_graph, desc_graph = self.detector_descriptor.create_computation_graph(
+                image_graph
+            )
+            with dask.config.set(scheduler='single-threaded'):
+                # TODO(ayush): check how many times detection is performed
+                detection_results = dask.compute(det_graph)[0]
+                descriptor_results = dask.compute(desc_graph)[0]
+                
+                # check the number of entries in results
+                self.assertEqual(
+                    len(detection_results), 1,
+                    "Dask workflow does not return the same number of results"
+                )
+                self.assertEqual(
+                    len(descriptor_results), len(self.loader),
+                    "Dask workflow does not return the same number of results"
+                )
+                self.assertTrue(isinstance(detection_results, Keypoints))
+                self.assertTrue(isinstance(descriptor_results, np.ndarray))
+                
+                # compare the result values for a particular image
+                idx_under_test = 0
+                if i != idx_under_test:
+                    continue
+                
+                # check the results via normal workflow and dask workflow for an image
+                expected_kps, expected_descs = self.detector_descriptor.detect_and_describe(
+                    self.loader.get_image(idx_under_test)
+                )
+                self.assertEqual(detection_results[idx_under_test], expected_kps)
+                np.testing.assert_array_equal(description_results[idx_under_test], expected_descs)
 
 
 if __name__ == '__main__':
