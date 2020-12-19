fixing computation graph API design

frontend/detector_descriptor/detector_descriptor_base.py
@@ -4,7 +4,7 @@ Authors: Ayush Baid
 """
 
 import abc
-from typing import List, Tuple
+from typing import Tuple
 
 import dask
 import numpy as np
@@ -31,8 +31,7 @@ class DetectorDescriptorBase(metaclass=abc.ABCMeta):
         self.max_keypoints = max_keypoints
 
     @abc.abstractmethod
-    def detect_and_describe(self,
-                            image: Image) -> Tuple[Keypoints, np.ndarray]:
+    def detect_and_describe(self, image: Image) -> Tuple[Keypoints, np.ndarray]:
         """Perform feature detection as well as their description.
 
         Refer to detect() in DetectorBase and describe() in DescriptorBase for
@@ -47,7 +46,9 @@ class DetectorDescriptorBase(metaclass=abc.ABCMeta):
             descriptor.
         """
 
-    def create_computation_graph(self, image_graph: Delayed) -> Tuple[Delayed, Delayed]:
+    def create_computation_graph(
+        self, image_graph: Delayed
+    ) -> Tuple[Delayed, Delayed]:
         """
         Generates the computation graph for detections and their descriptors.
 
@@ -56,12 +57,12 @@ class DetectorDescriptorBase(metaclass=abc.ABCMeta):
 
         Returns:
             Delayed tasks for detections.
-            Delayed task for corr. descriptors
+            Delayed task for corr. descriptors.
         """
         # get delayed object, cannot separate two arguments immediately
         joint_graph = dask.delayed(self.detect_and_describe)(image_graph)
-        
-        detection_graph = joint_graph[0]
+
+        keypoints_graph = joint_graph[0]
         descriptor_graph = joint_graph[1]
 
-        return detection_graph, descriptor_graph
+        return keypoints_graph, descriptor_graph

frontend/matcher/matcher_base.py
@@ -13,6 +13,7 @@ from dask.delayed import Delayed
 
 class MatchingDistanceType(Enum):
     """Type of distance metric to use for matching descriptors."""
+
     HAMMING = 1
     EUCLIDEAN = 2
 
@@ -24,11 +25,12 @@ class MatcherBase(metaclass=abc.ABCMeta):
     """
 
     @abc.abstractmethod
-    def match(self,
-              descriptors_im1: np.ndarray,
-              descriptors_im2: np.ndarray,
-              distance_type: MatchingDistanceType =
-              MatchingDistanceType.EUCLIDEAN) -> np.ndarray:
+    def match(
+        self,
+        descriptors_im1: np.ndarray,
+        descriptors_im2: np.ndarray,
+        distance_type: MatchingDistanceType = MatchingDistanceType.EUCLIDEAN,
+    ) -> np.ndarray:
         """Match descriptor vectors.
 
         Output format:
@@ -51,17 +53,18 @@ class MatcherBase(metaclass=abc.ABCMeta):
         # TODO(ayush): should I define matcher on descriptors or the distance matrices.
         # TODO(ayush): how to handle deep-matchers which might require the full image as input
 
-    def create_computation_graph(self,
-                                 image_pair_indices: Tuple[int, int],
-                                 descriptor_graph: List[Delayed],
-                                 distance_type: MatchingDistanceType = MatchingDistanceType.EUCLIDEAN
-                                 ) -> Delayed:
+    def create_computation_graph(
+        self,
+        descriptors_im1_graph: Delayed,
+        descriptors_im2_graph: Delayed,
+        distance_type: MatchingDistanceType = MatchingDistanceType.EUCLIDEAN,
+    ) -> Delayed:
         """
         Generates computation graph for matched features using the detection and description graph.
 
         Args:
-            image_pair_indices: valid pair of images which are to be matched.
-            description: list of dask tasks for descriptor generation for each image.
+            descriptors_im1_graph: descriptors for im1 wrapped in Delayed.
+            descriptors_im2_graph: descriptors for im2 wrapped in Delayed.
             distance_type (optional): the space to compute the distance between
                                       descriptors. Defaults to
                                       MatchingDistanceType.EUCLIDEAN.
@@ -69,11 +72,6 @@ class MatcherBase(metaclass=abc.ABCMeta):
         Returns:
             Delayed dask tasks for matching for input camera pairs.
         """
-        i1, i2 = image_pair_indices
-        delayed_matches_graph = dask.delayed(self.match)(
-                descriptor_graph[i1],
-                descriptor_graph[i2],
-                distance_type
-            )
-
-        return delayed_matches_graph
+        return dask.delayed(self.match)(
+            descriptors_im1_graph, descriptors_im2_graph, distance_type
+        )

tests/frontend/detector_descriptor/test_detector_descriptor_base.py
@@ -11,11 +11,13 @@ import numpy as np
 from common.keypoints import Keypoints
 import tests.frontend.detector.test_detector_base as test_detector_base
 from frontend.descriptor.dummy_descriptor import DummyDescriptor
-from frontend.detector.detector_from_joint_detector_descriptor import \
-    DetectorFromDetectorDescriptor
+from frontend.detector.detector_from_joint_detector_descriptor import (
+    DetectorFromDetectorDescriptor,
+)
 from frontend.detector.dummy_detector import DummyDetector
-from frontend.detector_descriptor.combination_detector_descriptor import \
-    CombinationDetectorDescriptor
+from frontend.detector_descriptor.combination_detector_descriptor import (
+    CombinationDetectorDescriptor,
+)
 
 
 class TestDetectorDescriptorBase(test_detector_base.TestDetectorBase):
@@ -29,13 +31,11 @@ class TestDetectorDescriptorBase(test_detector_base.TestDetectorBase):
         """Setup the attributes for the tests."""
         super().setUp()
         self.detector_descriptor = CombinationDetectorDescriptor(
-            DummyDetector(),
-            DummyDescriptor()
+            DummyDetector(), DummyDescriptor()
         )
 
         # explicitly set the detector
-        self.detector = DetectorFromDetectorDescriptor(
-            self.detector_descriptor)
+        self.detector = DetectorFromDetectorDescriptor(self.detector_descriptor)
 
     def test_detect_and_describe_shape(self):
         """
@@ -45,9 +45,9 @@ class TestDetectorDescriptorBase(test_detector_base.TestDetectorBase):
         # test on random indexes
         test_indices = [0, 5]
         for idx in test_indices:
-            kps, descs = \
-                self.detector_descriptor.detect_and_describe(
-                    self.loader.get_image(idx))
+            kps, descs = self.detector_descriptor.detect_and_describe(
+                self.loader.get_image(idx)
+            )
 
             if len(kps) == 0:
                 # test-case for empty results
@@ -61,30 +61,29 @@ class TestDetectorDescriptorBase(test_detector_base.TestDetectorBase):
 
         image_graph = self.loader.create_computation_graph_for_images()
         for i, delayed_image in enumerate(image_graph):
-            det_graph, desc_graph = self.detector_descriptor.create_computation_graph(
-                delayed_image
-            )
-            with dask.config.set(scheduler='single-threaded'):
+            (
+                kp_graph,
+                desc_graph,
+            ) = self.detector_descriptor.create_computation_graph(delayed_image)
+            with dask.config.set(scheduler="single-threaded"):
                 # TODO(ayush): check how many times detection is performed
-                detection_results = dask.compute(det_graph)[0]
-                descriptor_results = dask.compute(desc_graph)[0]
-                
+                keypoints = dask.compute(kp_graph)[0]
+                descriptors = dask.compute(desc_graph)[0]
+
                 # check the types of entries in results
-                self.assertTrue(isinstance(detection_results, Keypoints))
-                self.assertTrue(isinstance(descriptor_results, np.ndarray))
-                
-                # compare the result values for a particular image
-                idx_under_test = 0
-                if i != idx_under_test:
-                    continue
-                
+                self.assertTrue(isinstance(keypoints, Keypoints))
+                self.assertTrue(isinstance(descriptors, np.ndarray))
+
                 # check the results via normal workflow and dask workflow for an image
-                expected_kps, expected_descs = self.detector_descriptor.detect_and_describe(
-                    self.loader.get_image(idx_under_test)
+                (
+                    expected_kps,
+                    expected_descs,
+                ) = self.detector_descriptor.detect_and_describe(
+                    self.loader.get_image(i)
                 )
-                self.assertEqual(detection_results, expected_kps)
-                np.testing.assert_array_equal(descriptor_results, expected_descs)
+                self.assertEqual(keypoints, expected_kps)
+                np.testing.assert_array_equal(descriptors, expected_descs)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()

tests/frontend/matcher/test_matcher_base.py
@@ -29,43 +29,57 @@ class TestMatcherBase(unittest.TestCase):
         """Tests if matched indices are valid feature indices."""
 
         # run matching on a random input pair
-        result, descriptors_im1, descriptors_im2 = self.__generate_matches_on_random_descriptors()
+        (
+            result,
+            descriptors_im1,
+            descriptors_im2,
+        ) = self.__generate_matches_on_random_descriptors()
 
         if result.size:
             # check that the match indices are out of bounds
-            self.assertTrue(np.all((result[:, 0] >= 0) & (
-                result[:, 0] < descriptors_im1.shape[0])))
-            self.assertTrue(np.all((result[:, 1] >= 0) & (
-                result[:, 1] < descriptors_im2.shape[0])))
+            self.assertTrue(
+                np.all(
+                    (result[:, 0] >= 0)
+                    & (result[:, 0] < descriptors_im1.shape[0])
+                )
+            )
+            self.assertTrue(
+                np.all(
+                    (result[:, 1] >= 0)
+                    & (result[:, 1] < descriptors_im2.shape[0])
+                )
+            )
 
     def test_empty_input(self):
         """Tests the matches when there are no descriptors."""
 
         num_descriptors = random.randint(5, 15)
 
-        descriptor_dim = random.randint(2, 10) # dimensionality
+        descriptor_dim = random.randint(2, 10)  # dimensionality
 
         descriptors = generate_random_binary_descriptors(
-            num_descriptors, descriptor_dim)
+            num_descriptors, descriptor_dim
+        )
 
         # the first descriptor is empty
         result = self.matcher.match(
-            generate_random_binary_descriptors(0, descriptor_dim),
-            descriptors)
+            generate_random_binary_descriptors(0, descriptor_dim), descriptors
+        )
 
         self.assertEqual(0, result.size)
 
         # the second descriptor is empty
         result = self.matcher.match(
-            descriptors,
-            generate_random_binary_descriptors(0, descriptor_dim))
+            descriptors, generate_random_binary_descriptors(0, descriptor_dim)
+        )
 
         self.assertEqual(0, result.size)
 
         # both descriptors are empty
         result = self.matcher.match(
             generate_random_binary_descriptors(0, descriptor_dim),
-            generate_random_binary_descriptors(0, descriptor_dim))
+            generate_random_binary_descriptors(0, descriptor_dim),
+        )
 
         self.assertEqual(0, result.size)
 
@@ -97,37 +111,30 @@ class TestMatcherBase(unittest.TestCase):
 
         # generate descriptors randomly
         descriptors_list = []
-        for i in range(num_images):
+        for _ in range(num_images):
             num_descriptors = random.randint(5, 15)
 
             descriptors_list.append(
                 generate_random_binary_descriptors(
-                    num_descriptors, descriptor_dimension),
+                    num_descriptors, descriptor_dimension
+                ),
             )
 
-        # create computation graph providing descriptors
-        descriptor_graph = [dask.delayed(x) for x in descriptors_list]
-
-        for (i1,i2) in pairs_list:
-            matcher_graph = self.matcher.create_computation_graph(
-                (i1,i2),
-                descriptor_graph
+        for (i1, i2) in pairs_list:
+            matches_graph = self.matcher.create_computation_graph(
+                dask.delayed(descriptors_list[i1]),
+                dask.delayed(descriptors_list[i2]),
             )
 
-            # run it in sequential mode
-            results = []
-            with dask.config.set(scheduler='single-threaded'):
-                dask_matches = dask.compute(matcher_graph)[0]
+            with dask.config.set(scheduler="single-threaded"):
+                matches = dask.compute(matches_graph)[0]
 
-            # check every pair of results
+            # run matching using normal APIs
             expected_matches = self.matcher.match(
-                descriptors_list[i1],
-                descriptors_list[i2]
+                descriptors_list[i1], descriptors_list[i2]
             )
 
-            np.testing.assert_array_equal(
-                dask_matches, expected_matches
-            )
+            np.testing.assert_array_equal(matches, expected_matches)
 
     def test_pickleable(self):
         """Tests that the matcher object is pickleable (required for dask)."""
@@ -136,7 +143,9 @@ class TestMatcherBase(unittest.TestCase):
         except TypeError:
             self.fail("Cannot dump matcher using pickle")
 
-    def __generate_matches_on_random_descriptors(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    def __generate_matches_on_random_descriptors(
+        self,
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
         """Generates a pair of random descriptors and uses the matcher under 
         test to match them.
 
@@ -154,17 +163,20 @@ class TestMatcherBase(unittest.TestCase):
         descriptor_dim = random.randint(2, 10)
 
         descriptors_im1 = generate_random_binary_descriptors(
-            num_descriptors_im1, descriptor_dim)
+            num_descriptors_im1, descriptor_dim
+        )
         descriptors_im2 = generate_random_binary_descriptors(
-            num_descriptors_im2, descriptor_dim)
+            num_descriptors_im2, descriptor_dim
+        )
 
         result = self.matcher.match(descriptors_im1, descriptors_im2)
 
         return result, descriptors_im1, descriptors_im2
 
 
-def generate_random_binary_descriptors(num_descriptors: int,
-                                       descriptor_dim: int) -> np.ndarray:
+def generate_random_binary_descriptors(
+    num_descriptors: int, descriptor_dim: int
+) -> np.ndarray:
     """Generates random binary descriptors.
 
     Args:
@@ -182,5 +194,5 @@ def generate_random_binary_descriptors(num_descriptors: int,
     ).astype(np.uint8)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()