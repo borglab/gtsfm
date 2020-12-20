c6872820411800a8fb29fa3ec4b818bd7e6f4042
John Lambert <johnlambert@gatech.edu>
2020-12-19 20:51:19 -0500

    add missing dependency to setup.py

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
     install_requires=[
         "pytest",
         "dask[complete]",
+        "networkx",
         "numpy",
         "opencv-python>=4.1.0.25",
         "pillow>=8.0.1" # old versions dont support getexif()
