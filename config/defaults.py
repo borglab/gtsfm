c640142fa58dbe8cf2ff09d02ab6564039e4aa12
John Lambert <johnwl@stanford.edu>
2021-01-05 22:09:33 -0500

    use actual class name in defaults.py

--- config/defaults.py
+++ config/defaults.py
@@ -1,4 +1,5 @@
-from yacs.config import CfgNode as YACS
+
+from yacs.config import CfgNode
 
 """
 YACS requires specifying a schema for the YAML config files. In this Python file,
@@ -7,17 +8,17 @@ sections -- parameters for the SceneOptimizer, FeatureExtractor,
 TwoViewEstimator, and MultiViewOptimizer. YAML files should be hierarchical.
 """
 
-_Cfg = YACS(new_allowed=True)
+_Cfg = CfgNode(new_allowed=True)
 
-_Cfg.SceneOptimizer = YACS(new_allowed=True)
+_Cfg.SceneOptimizer = CfgNode(new_allowed=True)
 
-_Cfg.FeatureExtractor = YACS(new_allowed=True)
+_Cfg.FeatureExtractor = CfgNode(new_allowed=True)
 
-_Cfg.TwoViewEstimator = YACS(new_allowed=True)
+_Cfg.TwoViewEstimator = CfgNode(new_allowed=True)
 
-_Cfg.MultiViewOptimizer = YACS(new_allowed=True)
+_Cfg.MultiViewOptimizer = CfgNode(new_allowed=True)
 
 
-def get_cfg_defaults() -> YACS:
+def get_cfg_defaults() -> CfgNode:
     """Provides a YACS object with default modules for GTSFM."""
     return _Cfg.clone()
