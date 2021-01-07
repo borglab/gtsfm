f3552b37bc9397e423aa5c964cf6eb103bdafd53
Ayush Baid <ayushrakeshbaid@gmail.com>
2021-01-07 13:54:10 +0530

    refactoring code to modular functions and improving docstring

--- gtsfm/bundle/bundle_adjustment.py
+++ gtsfm/bundle/bundle_adjustment.py
@@ -7,19 +7,25 @@ import logging
 import sys
 
 import dask
-import gtsam
 from dask.delayed import Delayed
 from gtsam import (
     GeneralSFMFactor2Cal3Bundler,
+    LevenbergMarquardtOptimizer,
+    LevenbergMarquardtParams,
+    NonlinearFactorGraph,
+    PinholeCameraCal3Bundler,
+    PriorFactorCal3Bundler,
+    PriorFactorPose3,
     SfmData,
     SfmTrack,
+    Values,
     symbol_shorthand,
 )
+from gtsam.noiseModel import Isotropic
 
 from gtsfm.common.sfm_result import SfmResult
 
 # TODO: any way this goes away?
-C = symbol_shorthand.C  # camera
 P = symbol_shorthand.P  # 3d point
 X = symbol_shorthand.X  # camera pose
 K = symbol_shorthand.K  # calibration
@@ -43,6 +49,86 @@ class BundleAdjustmentOptimizer:
 
         self._shared_calib = shared_calib
 
+    def __add_camera_prior_and_initial_value(
+        self,
+        graph: NonlinearFactorGraph,
+        initial_values: Values,
+        camera: PinholeCameraCal3Bundler,
+        camera_idx: int,
+    ) -> None:
+        """Add a prior factor in the factor graph and initial values for the
+        camera parameters.
+
+        Args:
+            graph: factor graph for the problem.
+            initial_values: object holding the initial values for camera
+                            parameters and 3d points.
+            camera: the camera object to add to graph and to initial values.
+            camera_idx: index of the camera.
+        """
+
+        # add prior factor for pose
+        graph.push_back(
+            PriorFactorPose3(
+                X(camera_idx),
+                camera.pose(),
+                Isotropic.Sigma(6, 0.1),
+            )
+        )
+
+        # add initial value for pose
+        initial_values.insert(X(camera_idx), camera.pose())
+
+        # process just one camera if shared calibration; otherwise process all
+        if camera_idx == 0 or not self._shared_calib:
+            # add prior factor for calibration
+            graph.push_back(
+                PriorFactorCal3Bundler(
+                    K(camera_idx),
+                    camera.calibration(),
+                    Isotropic.Sigma(3, 0.1),
+                )
+            )
+
+            # add initial value for calibration
+            initial_values.insert(K(camera_idx), camera.calibration())
+
+    def __add_measurement_factors_and_initial_values(
+        self,
+        graph: NonlinearFactorGraph,
+        initial_values: Values,
+        track: SfmTrack,
+        track_idx: int,
+        measurement_noise: Isotropic,
+    ) -> None:
+        """Add prior factor for each 2D measurement and initial values for each
+        3d point.
+
+        Args:
+            graph: factor graph for the problem.
+            initial_values: object holding the initial values for camera
+                            parameters and 3d points.
+            track: [description]
+            measurement_noise (Isotropic): [description]
+        """
+
+        # Add measurements to the factor graph
+        for m_idx in range(track.number_measurements()):
+            # i represents the camera index, and uv is the 2d measurement
+            i, uv = track.measurement(m_idx)
+            graph.add(
+                GeneralSFMFactor2Cal3Bundler(
+                    uv,
+                    measurement_noise,
+                    X(i),
+                    P(track_idx),
+                    K(0 if self._shared_calib else i),
+                )
+            )
+
+        # add initial value for 3d point
+        initial_values.insert(P(track_idx), track.point3())
+
     def run(self, initial_data: SfmData) -> SfmResult:
         """Run the bundle adjustment by forming factor graph and optimizing using Levenberg–Marquardt optimization.
 
@@ -57,103 +143,51 @@ class BundleAdjustmentOptimizer:
         )
 
         # noise model for measurements
-        measurement_noise = gtsam.noiseModel.Isotropic.Sigma(
-            2, 1.0
-        )  # one pixel in u and v
+        measurement_noise = Isotropic.Sigma(2, 1.0)  # one pixel in u and v
 
         # Create a factor graph
-        graph = gtsam.NonlinearFactorGraph()
-
-        # Add measurements to the factor graph
-        j = 0
-        for t_idx in range(initial_data.number_tracks()):
-            track = initial_data.track(t_idx)  # SfmTrack
-            # retrieve the SfmMeasurement objects
-            for m_idx in range(track.number_measurements()):
-                # i represents the camera index, and uv is the 2d measurement
-                i, uv = track.measurement(m_idx)
-                # note use of shorthand symbols C and P
-                graph.add(
-                    GeneralSFMFactor2Cal3Bundler(
-                        uv, measurement_noise, C(i), P(j)
-                    )
-                )
-            j += 1
-
-        # Add a prior on camera x0's pose. This indirectly specifies where the origin is.
-        graph.push_back(
-            gtsam.PriorFactorPose3(
-                X(0),
-                initial_data.camera(0).pose(),
-                gtsam.noiseModel.Isotropic.Sigma(6, 0.1),
-            )
-        )
-
-        # Add prior on camera x0's intrinsics.
-        graph.push_back(
-            gtsam.PriorFactorCal3Bundler(
-                K(0),
-                initial_data.camera(0).calibration(),
-                gtsam.noiseModel.Isotropic.Sigma(3, 0.1),
-            )
-        )
-
-        # Also add a prior on the position of the first landmark to fix the scale
-        graph.push_back(
-            gtsam.PriorFactorPoint3(
-                P(0),
-                initial_data.track(0).point3(),
-                gtsam.noiseModel.Isotropic.Sigma(3, 0.1),
-            )
-        )
+        graph = NonlinearFactorGraph()
 
         # Create initial estimate
-        initial = gtsam.Values()
+        initial_values = Values()
 
-        i = 0
-        # add each PinholeCameraCal3Bundler
-        for cam_idx in range(initial_data.number_cameras()):
-            camera = initial_data.camera(cam_idx)
-            initial.insert(X(i), camera.pose())
-            i += 1
+        # adding factors for measurements and 3D points's initial values
+        for j in range(initial_data.number_tracks()):
+            self.__add_measurement_factors_and_initial_values(
+                graph,
+                initial_values,
+                initial_data.track(j),
+                j,
+                measurement_noise,
+            )
 
-        if self._shared_calib:
-            camera = initial_data.camera(0)
-            initial.insert(K(0), camera.calibration())
+        # add prior on all camera poses
+        for i in range(initial_data.number_cameras()):
+            initialized_cam = initial_data.camera(i)
 
-        else:
-            i = 0
-            # add each PinholeCameraCal3Bundler
-            for cam_idx in range(initial_data.number_cameras()):
-                camera = initial_data.camera(cam_idx)
-                initial.insert(K(i), camera.calibration())
-                i += 1
-
-        j = 0
-        # add each SfmTrack
-        for t_idx in range(initial_data.number_tracks()):
-            track = initial_data.track(t_idx)
-            initial.insert(P(j), track.point3())
-            j += 1
+            self.__add_camera_prior_and_initial_value(
+                graph, initial_values, initialized_cam, i
+            )
 
         # Optimize the graph and print results
         try:
-            params = gtsam.LevenbergMarquardtParams()
+            params = LevenbergMarquardtParams()
             params.setVerbosityLM("ERROR")
-            lm = gtsam.LevenbergMarquardtOptimizer(graph, initial, params)
+            lm = LevenbergMarquardtOptimizer(graph, initial_values, params)
             result_values = lm.optimize()
-        except Exception as e:
+        except Exception:
             logging.exception("LM Optimization failed")
             return
 
+        initial_error = graph.error(initial_values)
         final_error = graph.error(result_values)
 
         # Error drops from ~2764.22 to ~0.046
-        logging.info(f"initial error: {graph.error(initial):.2f}")
+        logging.info(f"initial error: {initial_error:.2f}")
         logging.info(f"final error: {final_error:.2f}")
 
         # construct the results
-        optimized_data = values_to_sfm_data(result_values, initial_data)
+        optimized_data = self.__values_to_sfm_data(result_values, initial_data)
         sfm_result = SfmResult(optimized_data, final_error)
 
         return sfm_result
@@ -169,37 +203,44 @@ class BundleAdjustmentOptimizer:
         """
         return dask.delayed(self.run)(sfm_data_graph)
 
+    def __values_to_sfm_data(
+        self, values: Values, initial_data: SfmData
+    ) -> SfmData:
+        """Cast results from the optimization to SfmData object.
 
-def values_to_sfm_data(values: Values, initial_data: SfmData) -> SfmData:
-    """Cast results from the optimization to SfmData object.
+        Args:
+            values: results of factor graph optimization.
+            initial_data: data used to generate the factor graph; used to
+                          extract information about poses and 3d points in the
+                          graph.
 
-    Args:
-        values: results of factor graph optimization.
-        initial_data: data used to generate the factor graph; used to extract
-                      information about poses and 3d points in the graph.
+        Returns:
+            optimized poses and landmarks.
+        """
+        result = SfmData()
 
-    Returns:
-        optimized poses and landmarks.
-    """
-    result = SfmData()
+        # add cameras
+        for i in range(initial_data.number_cameras()):
+            result.add_camera(
+                PinholeCameraCal3Bundler(
+                    values.atPose3(X(i)),
+                    values.atCal3Bundler(K(0 if self._shared_calib else i)),
+                )
+            )
 
-    # add cameras
-    for i in range(initial_data.number_cameras()):
-        result.add_camera(values.atPinholeCameraCal3Bundler(C(i)))
+        # add tracks
+        for j in range(initial_data.number_tracks()):
+            input_track = initial_data.track(j)
 
-    # add tracks
-    for j in range(initial_data.number_tracks()):
-        input_track = initial_data.track(j)
+            # populate the result with optimized 3D point
+            result_track = SfmTrack(
+                values.atPoint3(P(j)),
+            )
 
-        # populate the result with optimized 3D point
-        result_track = SfmTrack(
-            values.atPoint3(P(j)),
-        )
+            for measurement_idx in range(input_track.number_measurements()):
+                i, uv = input_track.measurement(measurement_idx)
+                result_track.add_measurement(i, uv)
 
-        for measurement_idx in range(input_track.number_measurements()):
-            i, uv = input_track.measurement(measurement_idx)
-            result_track.add_measurement(i, uv)
+            result.add_track(result_track)
 
-        result.add_track(result_track)
-
-    return result
+        return result
--- tests/bundle/test_bundle_adjustment.py
+++ tests/bundle/test_bundle_adjustment.py
@@ -8,7 +8,6 @@ import unittest
 import dask
 import gtsam
 import numpy as np
-from gtsam import SfmData, SfmTrack
 
 from gtsfm.bundle.bundle_adjustment import BundleAdjustmentOptimizer
 
@@ -19,36 +18,93 @@ EXAMPLE_DATA = gtsam.readBal(gtsam.findExampleDataFile(GTSAM_EXAMPLE_FILE))
 class TestBundleAdjustmentOptimizer(unittest.TestCase):
     """Unit tests for BundleAdjustmentOptimizer class."""
 
-    def setUp(self):
-        super().setUp()
+    def test_simple_scene_with_individual_calibration(self):
+        """Test the simple scene w/ individual calibration for each camera
+        using the `run` API.
+        """
+        expected_error = 2.197826405222743
+        expected_mean_track_length = 2.7142857142857144
+        expected_median_track_length = 3.0
 
-        self.obj = BundleAdjustmentOptimizer()
+        test_obj = BundleAdjustmentOptimizer(shared_calib=False)
 
-        self.test_data = EXAMPLE_DATA
+        computed_result = test_obj.run(EXAMPLE_DATA)
 
-    def test_simple_scene(self):
-        """Test the simple scene using the `run` API."""
+        (
+            mean_track_length,
+            median_track_length,
+        ) = computed_result.get_track_length_statistics()
 
-        computed_result = self.obj.run(self.test_data)
+        np.testing.assert_allclose(
+            expected_error,
+            computed_result.total_reproj_error,
+            atol=1e-2,
+            rtol=1e-2,
+        )
 
-        expected_error = 0.046137573704557046
+        np.testing.assert_allclose(
+            mean_track_length,
+            expected_mean_track_length,
+            atol=1e-2,
+            rtol=1e-2,
+        )
 
-        self.assertTrue(
-            np.isclose(
-                expected_error,
-                computed_result.total_reproj_error,
-                atol=1e-2,
-                rtol=1e-2,
-            )
+        np.testing.assert_allclose(
+            median_track_length,
+            expected_median_track_length,
+            atol=1e-2,
+            rtol=1e-2,
+        )
+
+    def test_simple_scene_with_shared_calibration(self):
+        """Test the simple scene w/ shared calibration across all camera
+        using the `run` API.
+
+        Note: higher error expected because intrinsics are different for
+        cameras in the example data.
+        """
+        expected_error = 25.10717447946933
+        expected_mean_track_length = 2.7142857142857144
+        expected_median_track_length = 3.0
+
+        test_obj = BundleAdjustmentOptimizer(shared_calib=True)
+
+        computed_result = test_obj.run(EXAMPLE_DATA)
+        (
+            mean_track_length,
+            median_track_length,
+        ) = computed_result.get_track_length_statistics()
+
+        np.testing.assert_allclose(
+            expected_error,
+            computed_result.total_reproj_error,
+            atol=1e-2,
+            rtol=1e-2,
+        )
+
+        np.testing.assert_allclose(
+            mean_track_length,
+            expected_mean_track_length,
+            atol=1e-2,
+            rtol=1e-2,
+        )
+
+        np.testing.assert_allclose(
+            median_track_length,
+            expected_median_track_length,
+            atol=1e-2,
+            rtol=1e-2,
         )
 
     def test_create_computation_graph(self):
         """Test the simple scene as dask computation graph."""
-        sfm_data_graph = dask.delayed(self.test_data)
+        test_obj = BundleAdjustmentOptimizer(shared_calib=True)
 
-        expected_result = self.obj.run(self.test_data)
+        sfm_data_graph = dask.delayed(EXAMPLE_DATA)
 
-        computed_result = self.obj.create_computation_graph(
+        expected_result = test_obj.run(EXAMPLE_DATA)
+
+        computed_result = test_obj.create_computation_graph(
             dask.delayed(sfm_data_graph)
         )
 
