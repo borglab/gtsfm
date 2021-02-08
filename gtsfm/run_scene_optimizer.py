f69fc230786eb40f2f4b27d431b51cbd009cea2d
John Lambert <johnwl@stanford.edu>
2021-02-08 13:52:50 -0500

    latest version has recursive instantiation

--- gtsfm/run_scene_optimizer.py
+++ gtsfm/run_scene_optimizer.py
@@ -3,55 +3,32 @@ import os
 from pathlib import Path
 from types import SimpleNamespace
 
-from omegaconf import DictConfig
-
 import hydra
 from dask.distributed import Client, LocalCluster, performance_report
 from hydra.utils import instantiate
+from omegaconf import DictConfig
 
+import gtsfm
 from gtsfm.common.sfm_result import SfmResult
 from gtsfm.loader.folder_loader import FolderLoader
 from gtsfm.scene_optimizer import SceneOptimizer
+
 
 TEST_ROOT = Path(__file__).resolve().parent.parent / "tests"
 
 
 @hydra.main(config_name="config")
 def run_scene_optimizer_hydra(cfg: DictConfig) -> None:
-	scene_optimizer: SceneOptimizer = instantiate(cfg.SceneOptimizer)
+	scene_optimizer: SceneOptimizer = instantiate(cfg.SceneOptimizer, _frozen_=False)
 
 	loader = FolderLoader(
 		os.path.join( TEST_ROOT, "data", "set1_lund_door"), image_extension="JPG"
 	)
 
-	scene_optimizer.feature_extractor = instantiate(scene_optimizer.feature_extractor)
-	scene_optimizer.feature_extractor.detector_descriptor = instantiate(
-		scene_optimizer.feature_extractor.detector_descriptor
-	)
-
-	scene_optimizer.two_view_estimator = instantiate(scene_optimizer.two_view_estimator)
-	scene_optimizer.two_view_estimator.matcher = instantiate(
-		scene_optimizer.two_view_estimator.matcher
-	)
-	scene_optimizer.two_view_estimator.verifier = instantiate(
-		scene_optimizer.two_view_estimator.verifier
-	)
-
-	scene_optimizer.multiview_optimizer = instantiate(scene_optimizer.multiview_optimizer)
-	scene_optimizer.multiview_optimizer.rot_avg_module = instantiate(
-		scene_optimizer.multiview_optimizer.rot_avg_module
-	)
-	scene_optimizer.multiview_optimizer.trans_avg_module = instantiate(
-		scene_optimizer.multiview_optimizer.trans_avg_module
-	)
-	scene_optimizer.multiview_optimizer.data_association_module = instantiate(
-		scene_optimizer.multiview_optimizer.data_association_module
-	)
-
 	import pdb
 	pdb.set_trace()
-
-	scene_optimizer.multiview_optimizer.data_association_module.mode = instantiate(
+	# bad support for Enum's currently
+	scene_optimizer.multiview_optimizer.data_association_module.mode = eval(
 		scene_optimizer.multiview_optimizer.data_association_module.mode
 	)
 
@@ -63,9 +40,6 @@ def run_scene_optimizer_hydra(cfg: DictConfig) -> None:
 		use_intrinsics_in_verification=True,
 		gt_pose_graph=loader.create_computation_graph_for_poses(),
 	)
-
-	import pdb
-	pdb.set_trace()
 
 	# create dask client
 	cluster = LocalCluster(n_workers=2, threads_per_worker=4)
@@ -146,6 +120,6 @@ def run_scene_optimizer():
 
 if __name__ == "__main__":
 	run_scene_optimizer_hydra()
-	run_scene_optimizer()
+	#run_scene_optimizer()
 
 
