a8fcb269c0a682165e37055237f2ca0203aac6ce
John Lambert <johnwl@stanford.edu>
2021-02-09 15:01:30 -0500

    import configs from gtsfm module

--- gtsfm/run_scene_optimizer.py
+++ gtsfm/run_scene_optimizer.py
@@ -4,6 +4,7 @@ from types import SimpleNamespace
 
 import hydra
 from dask.distributed import Client, LocalCluster, performance_report
+from hydra.experimental import compose, initialize_config_module
 from hydra.utils import instantiate
 from omegaconf import DictConfig
 
@@ -16,28 +17,32 @@ from gtsfm.scene_optimizer import SceneOptimizer
 TEST_ROOT = Path(__file__).resolve().parent.parent / "tests"
 
 
-@hydra.main(config_name="config")
-def run_scene_optimizer(cfg: DictConfig) -> None:
-    scene_optimizer: SceneOptimizer = instantiate(cfg.SceneOptimizer)
 
-    loader = FolderLoader(os.path.join(TEST_ROOT, "data", "set1_lund_door"), image_extension="JPG")
+def run_scene_optimizer() -> None:
+    """ """
+    with initialize_config_module(config_module="gtsfm.configs"):
+        # config is relative to the gtsfm module
+        cfg = compose(config_name="default_lund_door_set1_config.yaml")
+        scene_optimizer: SceneOptimizer = instantiate(cfg.SceneOptimizer)
 
-    sfm_result_graph = scene_optimizer.create_computation_graph(
-        len(loader),
-        loader.get_valid_pairs(),
-        loader.create_computation_graph_for_images(),
-        loader.create_computation_graph_for_intrinsics(),
-        use_intrinsics_in_verification=True,
-        gt_pose_graph=loader.create_computation_graph_for_poses(),
-    )
+        loader = FolderLoader(os.path.join(TEST_ROOT, "data", "set1_lund_door"), image_extension="JPG")
 
-    # create dask client
-    cluster = LocalCluster(n_workers=2, threads_per_worker=4)
+        sfm_result_graph = scene_optimizer.create_computation_graph(
+            len(loader),
+            loader.get_valid_pairs(),
+            loader.create_computation_graph_for_images(),
+            loader.create_computation_graph_for_intrinsics(),
+            use_intrinsics_in_verification=True,
+            gt_pose_graph=loader.create_computation_graph_for_poses(),
+        )
 
-    with Client(cluster), performance_report(filename="dask-report.html"):
-        sfm_result = sfm_result_graph.compute()
+        # create dask client
+        cluster = LocalCluster(n_workers=2, threads_per_worker=4)
 
-    assert isinstance(sfm_result, SfmResult)
+        with Client(cluster), performance_report(filename="dask-report.html"):
+            sfm_result = sfm_result_graph.compute()
+
+        assert isinstance(sfm_result, SfmResult)
 
 
 if __name__ == "__main__":
