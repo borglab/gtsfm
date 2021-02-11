6564a46f4351dacd85a818055458aa41c0068629
John Lambert <johnwl@stanford.edu>
2021-02-11 12:28:56 -0500

    add argoverse-api to dependencies

---
+++ gtsfm/loader/argoverse_dataset_loader.py
@@ -0,0 +1,142 @@
+""" Loader that reads images from Argoverse directories on disk.
+
+Authors: John Lambert
+"""
+
+import glob
+import os
+from pathlib import Path
+from typing import Optional
+
+import numpy as np
+from gtsam import Cal3Bundler, Point3, Pose3, Rot3
+
+from argoverse.data_loading.simple_track_dataloader import SimpleArgoverseTrackingDataLoader
+from argoverse.utils.calibration import get_calibration_config
+from argoverse.utils.se3 import SE3
+
+import gtsfm.utils.io as io_utils
+from gtsfm.common.image import Image
+from gtsfm.loader.loader_base import LoaderBase
+
+
+class ArgoverseDatasetLoader(LoaderBase):
+    """ """
+
+    def __init__(self, dataset_dir: str, log_id: str, stride: int = 4) -> None:
+        """
+        Args:
+            dataset_dir: directory where raw Argoverse logs are stored on disk
+            log_id:
+            stride: sampling rate, e.g. every 2 images, every 4 images, etc.
+        """
+        self.log_id = log_id
+        self.dl = SimpleArgoverseTrackingDataLoader(
+            data_dir=dataset_dir,
+            labels_dir=dataset_dir
+        )
+
+        max_num_imgs = 20
+        stride = 10
+
+        self.calib_data = self.dl.get_log_calibration_data(log_id)
+        camera_name = "ring_front_center"
+
+        image_paths = self.dl.get_ordered_log_cam_fpaths(log_id, camera_name)
+        image_paths = image_paths[::stride]
+        self.image_paths = image_paths[:max_num_imgs]
+
+        # for each image, cache its associated timestamp
+        self.image_timestamps = [ int(Path(path).stem.replace(f'{camera_name}_', '')) for path in self.image_paths ]
+ 
+        cam_config = get_calibration_config(self.calib_data, camera_name)
+        self.K = cam_config.intrinsic[:3,:3]
+
+        self.camera_SE3_egovehicle = SE3(
+            rotation=cam_config.extrinsic[:3,:3],
+            translation=cam_config.extrinsic[:3,3]
+        )
+        self.egovehicle_SE3_camera = self.camera_SE3_egovehicle.inverse()
+
+
+    def __len__(self) -> int:
+        """
+        The number of images in the dataset.
+
+        Returns:
+        the number of images.
+        """
+        return len(self.image_paths)
+
+    def get_image(self, index: int) -> Image:
+        """
+        Get the image at the given index.
+
+        Args:
+            index: the index to fetch.
+
+        Raises:
+            IndexError: if an out-of-bounds image index is requested.
+
+        Returns:
+            Image: the image at the query index.
+        """
+
+        if index < 0 or index > self.__len__():
+            raise IndexError("Image index is invalid")
+
+        return io_utils.load_image(self.image_paths[index])
+
+    def get_camera_intrinsics(self, index: int) -> Optional[Cal3Bundler]:
+        """Get the camera intrinsics at the given index.
+
+        Args:
+            the index to fetch.
+
+        Returns:
+            intrinsics for the given camera.
+        """
+        return Cal3Bundler(
+            fx=min(self.K[0, 0], self.K[1, 1]),
+            k1=0,
+            k2=0,
+            u0=self.K[0, 2],
+            v0=self.K[1, 2],
+        )
+
+    def get_camera_pose(self, index: int) -> Optional[Pose3]:
+        """Get the camera pose (in world coordinates) at the given index.
+
+        World coordinate frame is the "city" coordinate frame.
+
+        Args:
+            index: the index to fetch.
+
+        Returns:
+            the camera pose w_P_index.
+        """
+        timestamp = self.image_timestamps[index]
+        city_SE3_egovehicle = self.dl.get_city_SE3_egovehicle(self.log_id, timestamp)
+        if city_SE3_egovehicle is None:
+            return None
+
+        city_SE3_camera = city_SE3_egovehicle.compose(self.egovehicle_SE3_camera)
+
+        return Pose3(
+            Rot3(city_SE3_camera.rotation),
+            Point3(city_SE3_camera.translation)
+        )
+
+
+    def validate_pair(self, idx1: int, idx2: int) -> bool:
+        """Checks if (idx1, idx2) is a valid pair.
+
+        Args:
+            idx1: first index of the pair.
+            idx2: second index of the pair.
+
+        Returns:
+            validation result.
+        """
+
+        return (idx1 < idx2) and (idx2 < idx1 + 50)
---
+++ gtsfm/runner/run_scene_optimizer_argoverse.py
@@ -0,0 +1,55 @@
+import os
+from pathlib import Path
+
+import hydra
+from dask.distributed import Client, LocalCluster, performance_report
+from hydra.experimental import compose, initialize_config_module
+from hydra.utils import instantiate
+from omegaconf import DictConfig
+
+import gtsfm
+from gtsfm.common.sfm_result import SfmResult
+from gtsfm.loader.folder_loader import FolderLoader
+from gtsfm.scene_optimizer import SceneOptimizer
+
+from gtsfm.loader.argoverse_dataset_loader import ArgoverseDatasetLoader
+
+
+DATA_ROOT = Path(__file__).resolve().parent.parent.parent / "tests" / "data"
+
+
+def run_scene_optimizer() -> None:
+    """ """
+    with initialize_config_module(config_module="gtsfm.configs"):
+        # config is relative to the gtsfm module
+        cfg = compose(config_name="default_lund_door_set1_config.yaml")
+        scene_optimizer: SceneOptimizer = instantiate(cfg.SceneOptimizer)
+
+        loader = ArgoverseDatasetLoader(
+            dataset_dir = "/Users/johnlambert/Downloads/visual-odometry-tutorial/full_log",
+            log_id = "273c1883-673a-36bf-b124-88311b1a80be"
+        )
+
+        sfm_result_graph = scene_optimizer.create_computation_graph(
+            len(loader),
+            loader.get_valid_pairs(),
+            loader.create_computation_graph_for_images(),
+            loader.create_computation_graph_for_intrinsics(),
+            use_intrinsics_in_verification=True,
+            gt_pose_graph=loader.create_computation_graph_for_poses(),
+        )
+
+        # create dask client
+        cluster = LocalCluster(n_workers=2, threads_per_worker=4)
+
+        with Client(cluster), performance_report(filename="dask-report.html"):
+            sfm_result = sfm_result_graph.compute()
+
+        assert isinstance(sfm_result, SfmResult)
+
+
+if __name__ == "__main__":
+    run_scene_optimizer()
+
+
+
