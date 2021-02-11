88f8d495ae4c180f82226d1a87e0b29ba2b9c97f
Ayush Baid <ayushrakeshbaid@gmail.com>
2021-02-11 15:19:15 -0500

    adding function for epipolar distances

--- gtsfm/utils/verification.py
+++ gtsfm/utils/verification.py
@@ -18,8 +18,8 @@ def recover_relative_pose_from_essential_matrix(
     camera_intrinsics_i1: Cal3Bundler,
     camera_intrinsics_i2: Cal3Bundler,
 ) -> Tuple[Rot3, Unit3]:
-    """Recovers the relative rotation and translation direction from essential
-    matrix and verified correspondences using opencv's API.
+    """Recovers the relative rotation and translation direction from essential matrix and verified correspondences
+    using opencv's API.
 
     Args:
         i2Ei1: essential matrix as a numpy array, of shape 3x3.
@@ -42,23 +42,8 @@ def recover_relative_pose_from_essential_matrix(
     return Rot3(i2Ri1), Unit3(i2ti1.squeeze())
 
 
-def create_essential_matrix(i2Ri1: Rot3, i2Ui1: Unit3) -> EssentialMatrix:
-    """Creates essential matrix from rot3 and unit3.
-
-    Args:
-        i2Ri1: relative rotation.
-        i2Ui1: relative translation direction.
-
-    Returns:
-        essential matrix i2Ei1.
-    """
-    return EssentialMatrix(i2Ri1, i2Ui1)
-
-
 def fundamental_to_essential_matrix(
-    i2Fi1: np.ndarray,
-    camera_intrinsics_i1: Cal3Bundler,
-    camera_intrinsics_i2: Cal3Bundler,
+    i2Fi1: np.ndarray, camera_intrinsics_i1: Cal3Bundler, camera_intrinsics_i2: Cal3Bundler
 ) -> np.ndarray:
     """Converts the fundamental matrix to essential matrix using camera intrinsics.
 
@@ -71,3 +56,32 @@ def fundamental_to_essential_matrix(
             Estimated essential matrix i2Ei1 as numpy array of shape (3x3).
     """
     return camera_intrinsics_i2.K().T @ i2Fi1 @ camera_intrinsics_i1.K()
+
+
+def compute_epipolar_distances(
+    normalized_coords_i1: np.ndarray, normalized_coords_i2: np.ndarray, i2Ei1: EssentialMatrix
+) -> np.ndarray:
+    """Compute symmetric point-line epipolar distances between normalized coordinates of correspondences.
+
+    Args:
+        normalized_coords_i1: normalized coordinates in image i1, of shape Nx2.
+        normalized_coords_i2: corr. normalized coordinates in image i2, of shape Nx2.
+        i2Ei1: essential matrix between two images
+
+    Returns:
+        Symmetric epipolar distances for each row of the input, of shape N.
+    """
+    # construct the essential matrix in the opposite directin
+    i2Ti1 = Pose3(i2Ei1.rotation(), i2Ei1.direction().point3())
+    i1Ti2 = i2Ti1.inverse()
+    i1Ei2 = EssentialMatrix(i1Ti2.rotation(), Unit3(i1Ti2.translation()))
+
+    # get lines in i2 and i1
+    epipolar_lines_i2 = feature_utils.convert_to_epipolar_lines(normalized_coords_i1, i2Ei1)
+    epipolar_lines_i1 = feature_utils.convert_to_epipolar_lines(normalized_coords_i2, i1Ei2)
+
+    # compute two distances and average them
+    return 0.5 * (
+        feature_utils.compute_point_line_distances(normalized_coords_i1, epipolar_lines_i1)
+        + feature_utils.compute_point_line_distances(normalized_coords_i2, epipolar_lines_i2)
+    )
