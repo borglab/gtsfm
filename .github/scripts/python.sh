bf9ddc79c16a3c13d043174f7278cc2b43e1146a
Ayush Baid <ayushrakeshbaid@gmail.com>
2021-03-11 16:07:03 -0500

    switching to new gtsam wheels

--- .github/scripts/python.sh
+++ .github/scripts/python.sh
@@ -8,8 +8,9 @@ echo "Running .github/scripts/python.sh..."
 conda init
 conda info --envs
 
-wget -O 2020_01_13_gtsam_python38_wheel.zip --no-check-certificate "https://drive.google.com/uc?export=download&id=1b7zoYopU7jN3D62fuZMqwQgZdhZ4cH6P"
-unzip 2020_01_13_gtsam_python38_wheel.zip
+wget -O 2020_03_08_gtsam_python38_wheel.zip --no-check-certificate "https://drive.google.com/uc?export=download&id=1lpU4TFqh5puH41h2kcqfIgX7_br_a0nb"
+
+unzip 2020_03_08_gtsam_python38_wheel.zip
 pip install gtsam-4.1.1-cp38-cp38-manylinux2014_x86_64.whl
 
 ##########################################################
