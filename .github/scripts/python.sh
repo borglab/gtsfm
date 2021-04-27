b91c48bef2fe234771e04af446f8dd0bdc4918c0
Ayush Baid <ayushrakeshbaid@gmail.com>
2021-04-27 09:56:17 -0400

    adding pylint to CI

--- .github/scripts/python.sh
+++ .github/scripts/python.sh
@@ -33,8 +33,9 @@ pytest tests --cov gtsfm
 coverage report
 
 ##########################################################
-# Test with flake8
+# Lint with flake8 and pylint
 ##########################################################
 
 pip install flake8
 flake8 --max-line-length 120 --ignore E201,E202,E203,E231,W291,W293,E303,W391,E402,W503,E731 gtsfm
+pylint --indent-string='    ' --generated-members=numpy.* ,torch.* ,cv2.* , cv.*, scipy.*, gtsam.* --max-line-length=120
