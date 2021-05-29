cfc777ba625684ef3a7a1ffb0f0bb621a19fefb0
John Lambert <johnwl@stanford.edu>
2021-05-29 12:34:51 -0400

    fix typos in superglue unit test

--- tests/frontend/matcher/test_superglue_matcher.py
+++ tests/frontend/matcher/test_superglue_matcher.py
@@ -14,15 +14,15 @@ def test_SuperGlueMatcher() -> None:
     im_shape_i2 = (H,W)
 
     num_kps_i1 = 50
-    kps_i1 = Keypoints(coordinates=np.random.randint(0, img_height, size=(num_kps_i1, 2)), responses=np.random.rand(50))
+    kps_i1 = Keypoints(coordinates=np.random.randint(0, H, size=(num_kps_i1, 2)), responses=np.random.rand(50))
     descs_i1 = np.random.randn(num_kps_i1, 256)
 
     num_kps_i2 = 100
-    kps_i2 = Keypoints(coordinates=np.random.randint(0, img_height, size=(num_kps_i2, 2)), responses=np.random.rand(100))
+    kps_i2 = Keypoints(coordinates=np.random.randint(0, H, size=(num_kps_i2, 2)), responses=np.random.rand(100))
     descs_i2 = np.random.randn(num_kps_i2, 256)
 
     matcher = SuperGlueMatcher()
 
     match_indices = matcher.match(kps_i1, kps_i2, descs_i1, descs_i2, im_shape_i1, im_shape_i2)
     assert isinstance(match_indices, np.ndarray)
-    assert match_indices.dtype == np.int64
+    assert match_indices.dtype == np.uint32
