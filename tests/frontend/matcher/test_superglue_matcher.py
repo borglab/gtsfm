54868bb87a5baf3797af3c5af4b3bab1b59630d7
John Lambert <johnwl@stanford.edu>
2021-06-03 21:40:23 -0400

    use unittest class

--- tests/frontend/matcher/test_superglue_matcher.py
+++ tests/frontend/matcher/test_superglue_matcher.py
@@ -2,30 +2,41 @@
 
 Authors: John Lambert
 """
+import unittest
+
 import numpy as np
 
 from gtsfm.common.keypoints import Keypoints
 from gtsfm.frontend.matcher.superglue_matcher import SuperGlueMatcher
 
 
-def test_SuperGlueMatcher() -> None:
-    """Ensure the SuperGlue matcher returns output of the correct shape, for random input."""
-    # image height and width
-    H, W = 20, 20
+class TestSuperGlueMatcher(unittest.TestCase):
+    """Unit tests for the SuperGlueMatcher.
 
-    im_shape_i1 = (H, W)
-    im_shape_i2 = (H, W)
+    All unit test functions defined in TestMatcherBase are run automatically.
+    """
 
-    num_kps_i1 = 50
-    kps_i1 = Keypoints(coordinates=np.random.randint(0, H, size=(num_kps_i1, 2)), responses=np.random.rand(50))
-    descs_i1 = np.random.randn(num_kps_i1, 256)
+    def setUp(self) -> None:
+        super().setUp()
 
-    num_kps_i2 = 100
-    kps_i2 = Keypoints(coordinates=np.random.randint(0, H, size=(num_kps_i2, 2)), responses=np.random.rand(100))
-    descs_i2 = np.random.randn(num_kps_i2, 256)
+        self.matcher = SuperGlueMatcher()
 
-    matcher = SuperGlueMatcher()
+    def test_on_dummy_data(self) -> None:
+        """Ensure the SuperGlue matcher returns output of the correct shape, for random input."""
+        # image height and width
+        H, W = 20, 20
 
-    match_indices = matcher.match(kps_i1, kps_i2, descs_i1, descs_i2, im_shape_i1, im_shape_i2)
-    assert isinstance(match_indices, np.ndarray)
-    assert match_indices.dtype == np.uint32
+        im_shape_i1 = (H,W)
+        im_shape_i2 = (H,W)
+
+        num_kps_i1 = 50
+        kps_i1 = Keypoints(coordinates=np.random.randint(0, H, size=(num_kps_i1, 2)), responses=np.random.rand(50))
+        descs_i1 = np.random.randn(num_kps_i1, 256)
+
+        num_kps_i2 = 100
+        kps_i2 = Keypoints(coordinates=np.random.randint(0, H, size=(num_kps_i2, 2)), responses=np.random.rand(100))
+        descs_i2 = np.random.randn(num_kps_i2, 256)
+
+        match_indices = self.matcher.match(kps_i1, kps_i2, descs_i1, descs_i2, im_shape_i1, im_shape_i2)
+        assert isinstance(match_indices, np.ndarray)
+        assert match_indices.dtype == np.uint32
