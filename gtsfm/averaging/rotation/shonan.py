3bc085d5f673ba787b104cbcf512934dee5813aa
John Lambert <johnlambert@gatech.edu>
2021-06-08 11:28:04 -0400

    address Akshay's suggestions

--- gtsfm/averaging/rotation/shonan.py
+++ gtsfm/averaging/rotation/shonan.py
@@ -36,17 +36,14 @@ class ShonanRotationAveraging(RotationAveragingBase):
         self._p_min = 5
         self._p_max = 30
 
-    def run_w_consecutive_ordering(
+    def run_with_consecutive_ordering(
         self, num_connected_nodes: int, i2Ri1_dict: Dict[Tuple[int, int], Optional[Rot3]]
     ) -> List[Optional[Rot3]]:
         """Run the rotation averaging on a connected graph w/ N keys ordered consecutively [0,...,N-1].
 
-        Note: the assumption that Shonan keys are ordered as [0,...,N-1] goes deeper than
-        ShonanAveraging::NrUnknowns(), which just checks the assumption. Several other functions that construct
-        matrices related to the convergence certificate also use the fact that the graph is connected and
-        keys are 0..N-1. There are about 10 places where nrUnknowns is used to dimension a sparse or dense matrix.
+        Note: GTSAM requires the N input nodes to be connected and ordered from [0 ... N-1].
         Modifying GTSAM would require a major philosophical overhaul, so we perform the re-ordering
-        here in a sort of "wrapper". See: https://github.com/borglab/gtsam/issues/784
+        here in a sort of "wrapper". See https://github.com/borglab/gtsam/issues/784 for more details.
 
         Args:
             num_connected_nodes: number of unique connected nodes in the graph (can be far less than the number
@@ -78,13 +75,21 @@ class ShonanRotationAveraging(RotationAveragingBase):
         initial = obj.initializeRandomly()
         result_values, _ = obj.run(initial, self._p_min, self._p_max)
 
-        return [result_values.atRot3(i) if result_values.exists(i) else None for i in range(num_connected_nodes)]
+        wRi_list_consecutive = [None] * num_connected_nodes
+        for i in range(num_connected_nodes):
+            if result_values.exists(i):
+                wRi_list_consecutive[i] = result_values.atRot3(i)
+
+        return wRi_list_consecutive
 
     def run(self, num_images: int, i2Ri1_dict: Dict[Tuple[int, int], Optional[Rot3]]) -> List[Optional[Rot3]]:
-        """Wrapper that re-orders keys to prepare a connected graph, w/ N keys ordered [0,...,N-1].
+        """Run the rotation averaging with arbitrary keys, where each key is a image/pose index.
+
+        Note: run() functions as a wrapper that re-orders keys to prepare a connected graph,
+        w/ N keys ordered [0,...,N-1].
 
         Args:
-            num_images: number of poses.
+            num_images: number of images. Since we have one pose per image, it is also the number of poses.
             i2Ri1_dict: relative rotations for each image pair-edge as dictionary (i1, i2): i2Ri1.
 
         Returns:
@@ -100,11 +105,10 @@ class ShonanRotationAveraging(RotationAveragingBase):
 
         connected_nodes = sorted(list(connected_nodes))
 
-        # given original index, get back new temporary index
+        # given original index, this map gives back a new temporary index, starting at 0
         reordered_idx_map = {}
-        for i in range(num_images):
-            if i in connected_nodes:
-                reordered_idx_map[i] = connected_nodes.index(i)
+        for (new_idx, i) in enumerate(connected_nodes):
+            reordered_idx_map[i] = new_idx
 
         # now, re-order all of the indices
         i2Ri1_dict_reordered = {}
@@ -113,7 +117,7 @@ class ShonanRotationAveraging(RotationAveragingBase):
             i2_ = reordered_idx_map[i2]
             i2Ri1_dict_reordered[(i1_, i2_)] = i2Ri1
 
-        wRi_list_subset = self.run_w_consecutive_ordering(
+        wRi_list_subset = self.run_with_consecutive_ordering(
             num_connected_nodes=len(connected_nodes), i2Ri1_dict=i2Ri1_dict_reordered
         )
 
