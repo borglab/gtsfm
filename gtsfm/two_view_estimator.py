5d50b6ce0f99a36eed2c997da13bf37949a5b990
John Lambert <johnlambert@gatech.edu>
2021-07-01 10:15:17 -0400

    improve docstrings in TwoViewEstimationReport

--- gtsfm/two_view_estimator.py
+++ gtsfm/two_view_estimator.py
@@ -29,11 +29,19 @@ pil_logger.setLevel(logging.INFO)
 
 @dataclass(frozen=False)
 class TwoViewEstimationReport:
-    """
+    """Information about verifier result on an edge between two nodes (i1,i2).
+
+    In the spirit of COLMAP's Report class:
+    https://github.com/colmap/colmap/blob/dev/src/optim/ransac.h#L82
+
+    Inlier ratio is defined in Heinly12eccv: https://www.cs.unc.edu/~jheinly/publications/eccv2012-heinly.pdf
+    or in Slide 59: https://www.cc.gatech.edu/~afb/classes/CS4495-Fall2014/slides/CS4495-Ransac.pdf
+
     Args:
         v_corr_idxs: verified correspondence indices.
         num_inliers_est_model: #correspondences consistent with estimated model (not necessarily "correct")
-        inlier_ratio_est_model: measures how consistent the model is with the putative matches.
+        inlier_ratio_est_model: #matches consistent with est. model / # putative matches, i.e.
+           measures how consistent the model is with the putative matches.
         num_inliers_gt_model: measures how well the verification worked, w.r.t. GT
         inlier_ratio_gt_model: #correct matches/#putative matches. Only defined if GT relative pose provided.
         R_error_deg: relative pose error w.r.t. GT. Only defined if GT poses provided.
@@ -44,7 +52,7 @@ class TwoViewEstimationReport:
 
     v_corr_idxs: np.ndarray
     num_inliers_est_model: float
-    inlier_ratio_est_model: Optional[float] = None
+    inlier_ratio_est_model: Optional[float] = None  # TODO: make not optional (pass from verifier)
     num_inliers_gt_model: Optional[float] = None
     inlier_ratio_gt_model: Optional[float] = None
     R_error_deg: Optional[float] = None
