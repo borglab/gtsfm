added recall computation, fixed loransac bug

gtsfm/frontend/verifier/loransac.py
@@ -54,9 +54,10 @@ class LoRansac(VerifierBase):
         camera_intrinsics_i2: Cal3Bundler,
     ) -> Tuple[Optional[Rot3], Optional[Unit3], np.ndarray, float]:
         """ """
-        # # return if not enough matches
-        # if match_indices.shape[0] < self._min_matches:
-        #     return None, None, np.array([]), 0.0 
+        # return if not enough matches
+        if match_indices.shape[0] < self._min_matches:
+            logger.info('[LORANSAC] Not enough matches for verification.')
+            return self._failure_result 
 
         uv_i1 = keypoints_i1.coordinates
         uv_i2 = keypoints_i2.coordinates
@@ -92,6 +93,9 @@ class LoRansac(VerifierBase):
         )
 
         success = result_dict["success"]
+        if not success:
+            logger.info('[LORANSAC] Essential matrix estimation unsuccessful.')
+            return self._failure_result
         E = result_dict["E"]
         # See https://github.com/colmap/colmap/blob/dev/src/base/pose.h#L72
         qw, qx, qy, qz = result_dict["qvec"]

gtsfm/scene_optimizer.py
@@ -435,8 +435,8 @@ def persist_frontend_metrics_full(two_view_report_dict: Dict[Tuple[int, int], Tw
                 if report.U_error_deg
                 else None,
                 "num_inliers_gt_model": report.num_inliers_gt_model if report.num_inliers_gt_model else None,
-                "inlier_ratio_gt_model": round(report.inlier_ratio_gt_model, PRINT_NUM_SIG_FIGS)
-                if report.inlier_ratio_gt_model
+                "precision_gt_model": round(report.precision_gt_model, PRINT_NUM_SIG_FIGS)
+                if report.precision_gt_model
                 else None,
                 "inlier_ratio_est_model": round(report.inlier_ratio_est_model, PRINT_NUM_SIG_FIGS),
                 "num_inliers_est_model": report.num_inliers_est_model,
@@ -477,15 +477,18 @@ def aggregate_frontend_metrics(
     rot3_angular_errors = []
     trans_angular_errors = []
     avg_inlier_reproj_errors = []
-    avg_precisions = []
+    precisions = []
+    recalls = []
 
     for report in two_view_report_dict.values():
         rot3_angular_errors.append(report.R_error_deg)
         trans_angular_errors.append(report.U_error_deg)
         if report.avg_inlier_reproj_err is not None:
             avg_inlier_reproj_errors.append(report.avg_inlier_reproj_err)
-        if report.inlier_ratio_gt_model is not None:
-            avg_precisions.append(report.inlier_ratio_gt_model)
+        if report.precision_gt_model is not None:
+            precisions.append(report.precision_gt_model)
+        if report.recall_gt_model is not None:
+            recalls.append(report.recall_gt_model)
 
 
     rot3_angular_errors = np.array(rot3_angular_errors, dtype=float)
@@ -506,7 +509,9 @@ def aggregate_frontend_metrics(
     # all_correct = np.count_nonzero(metrics_array[:, 3] == 1.0)
 
     # Note: average of each two view average
-    logger.debug(f'[Front end] [Summary] Average precision: {np.mean(avg_precisions)}')
+    logger.debug(f'[Front end] [Summary] Average PMR:       TODO')
+    logger.debug(f'[Front end] [Summary] Average Recall:    {np.mean(recalls) if len(recalls) > 0 else -1}')
+    logger.debug(f'[Front end] [Summary] Average Precision: {np.mean(precisions)}')
     logger.debug(f'[Front end] [Summary] Average inlier reprojection error: {np.mean(avg_inlier_reproj_errors)}')
 
     logger.debug(

gtsfm/two_view_estimator.py
@@ -71,7 +71,8 @@ class TwoViewEstimationReport:
     num_inliers_est_model: float
     inlier_ratio_est_model: Optional[float] = None  # TODO: make not optional (pass from verifier)
     num_inliers_gt_model: Optional[float] = None
-    inlier_ratio_gt_model: Optional[float] = None
+    precision_gt_model: Optional[float] = None
+    recall_gt_model: Optional[float] = None
     inlier_mask_gt_model: Optional[np.ndarray] = None,
     avg_inlier_reproj_err: Optional[float] = None
     R_error_deg: Optional[float] = None
@@ -181,11 +182,10 @@ class TwoViewEstimator:
             pose_error_graphs = (None, None)
 
         # Evaluate the correspondences
-        #if None not in (wTi1_expected_graph, wTi2_expected_graph, scene_mesh_expected):
         if (wTi1_expected_graph is not None and 
             wTi2_expected_graph is not None and 
             scene_mesh_expected is not None):
-            corr_error_graph = dask.delayed(compute_correspondence_metrics)(
+            corr_metric_graph = dask.delayed(compute_correspondence_metrics)(
                 keypoints_i1_graph,
                 keypoints_i2_graph,
                 v_corr_idxs_graph,
@@ -197,11 +197,11 @@ class TwoViewEstimator:
                 wTi2_expected_graph,
                 scene_mesh_expected,
             )
-            number_correct, inlier_ratio, inlier_mask, avg_inlier_reproj_err = (
-                corr_error_graph[0], corr_error_graph[1], corr_error_graph[2], corr_error_graph[3]
+            number_correct, precision, recall, inlier_mask, avg_inlier_reproj_err = (
+                corr_metric_graph[0], corr_metric_graph[1], corr_metric_graph[2], corr_metric_graph[3], corr_metric_graph[4]
             )
         elif i2Ti1_expected_graph is not None:
-            corr_error_graph = dask.delayed(compute_correspondence_metrics)(
+            corr_metric_graph = dask.delayed(compute_correspondence_metrics)(
                 keypoints_i1_graph,
                 keypoints_i2_graph,
                 v_corr_idxs_graph,
@@ -210,11 +210,11 @@ class TwoViewEstimator:
                 i2Ti1_expected_graph,
                 self._corr_metric_dist_threshold,
             )
-            number_correct, inlier_ratio, inlier_mask, avg_inlier_reproj_err = (
-                corr_error_graph[0], corr_error_graph[1], corr_error_graph[2], corr_error_graph[3]
+            number_correct, precision, recall, inlier_mask, avg_inlier_reproj_err = (
+                corr_metric_graph[0], corr_metric_graph[1], corr_metric_graph[2], corr_metric_graph[3], corr_metric_graph[4]
             )
         else:
-            number_correct, inlier_ratio, inlier_mask, avg_inlier_reproj_err = None, None, None, None
+            number_correct, precision, recall, inlier_mask, avg_inlier_reproj_err = [None for x in range(5)]
 
 
         result = dask.delayed(self._homography_estimator.estimate)(
@@ -231,7 +231,8 @@ class TwoViewEstimator:
             R_error_deg,
             U_error_deg,
             number_correct,
-            inlier_ratio,
+            precision,
+            recall,
             inlier_mask,
             avg_inlier_reproj_err,
             v_corr_idxs_graph,
@@ -290,7 +291,8 @@ def generate_two_view_report(
     R_error_deg: float,
     U_error_deg: float,
     number_correct: int,
-    inlier_ratio: float,
+    precision: float,
+    recall: float,
     inlier_mask: np.ndarray,
     avg_inlier_reproj_err: float,
     v_corr_idxs: np.ndarray,
@@ -302,7 +304,8 @@ def generate_two_view_report(
         inlier_ratio_est_model=inlier_ratio_est_model,
         num_inliers_est_model=v_corr_idxs.shape[0],
         num_inliers_gt_model=number_correct,
-        inlier_ratio_gt_model=inlier_ratio,
+        precision_gt_model=precision,
+        recall_gt_model=recall,
         inlier_mask_gt_model=inlier_mask,
         avg_inlier_reproj_err=avg_inlier_reproj_err,
         v_corr_idxs=v_corr_idxs,
@@ -325,7 +328,7 @@ def compute_correspondence_metrics(
     wTi1: Optional[Pose3] = None,
     wTi2: Optional[Pose3] = None,
     scene_mesh: Optional[trimesh.Trimesh] = None,
-) -> Tuple[int, float, np.ndarray, float]:
+) -> Tuple[int, float, float, np.ndarray, float]:
     """Compute the metrics for the generated verified correspondence.
 
     Args:
@@ -345,9 +348,18 @@ def compute_correspondence_metrics(
         Inlier Ratio, i.e. ratio of correspondences which are correct w.r.t. given relative pose.
     """
     if corr_idxs_i1i2.size == 0:
-        return 0, float("Nan")
+        return 0, float("Nan"), float("Nan"), None, float("Nan")
 
     if None not in (wTi1, wTi2, scene_mesh):
+        gt_corr_i1i2_model = metric_utils.compute_gt_correspondences(
+            keypoints_i1,
+            keypoints_i2,
+            intrinsics_i1,
+            intrinsics_i2,
+            wTi1,
+            wTi2,
+            scene_mesh,
+        )
         inlier_mask, avg_inlier_reproj_error = metric_utils.mesh_inlier_correspondences(
             keypoints_i1.extract_indices(corr_idxs_i1i2[:, 0]),
             keypoints_i2.extract_indices(corr_idxs_i1i2[:, 1]),
@@ -358,6 +370,7 @@ def compute_correspondence_metrics(
             scene_mesh,
         )
         logger.info(f'avg. inlier reproj. error:   {avg_inlier_reproj_error}')
+        logger.info(f'Recall: {np.count_nonzero(inlier_mask) / gt_corr_i1i2_model.shape[0]}')
     else:
         inlier_mask = metric_utils.count_correct_correspondences(
             keypoints_i1.extract_indices(corr_idxs_i1i2[:, 0]),
@@ -368,11 +381,14 @@ def compute_correspondence_metrics(
             epipolar_distance_threshold,
         )
         avg_inlier_reproj_error = None
-    num_inliers_gt_model = np.count_nonzero(inlier_mask)
-    inlier_ratio_gt_model = num_inliers_gt_model / inlier_mask.size
+        gt_corr_i1i2_model = np.array([])
+    num_inliers_gt_model = np.count_nonzero(inlier_mask) # TP
+    pmr_gt_model = num_inliers_gt_model / max(len(keypoints_i1), len(keypoints_i2)) # TP / (Total Features)
+    precision_gt_model = num_inliers_gt_model / inlier_mask.size # TP / (TP + FP)
+    recall_gt_model = num_inliers_gt_model / gt_corr_i1i2_model.shape[0] # TP / (TP + FN)
     logger.info(f'# corr outliers:   {inlier_mask.size - num_inliers_gt_model}')
-    logger.info(f'corr inlier ratio: {inlier_ratio_gt_model}')
-    return num_inliers_gt_model, inlier_ratio_gt_model, inlier_mask, avg_inlier_reproj_error
+    logger.info(f'Precision: {precision_gt_model}')
+    return num_inliers_gt_model, precision_gt_model, recall_gt_model, inlier_mask, avg_inlier_reproj_error
 
 
 def compute_relative_pose_metrics(

gtsfm/utils/metrics.py
@@ -79,7 +79,6 @@ def mesh_inlier_correspondences(
     gt_scene_mesh: trimesh.Trimesh,
 ) -> Tuple[np.ndarray, Optional[float]]:
     """Compute inlier correspondences using the ground truth triangular surface mesh of the scene.
-
     Args:
         keypoints_i1: N keypoints in image i1.
         keypoints_i2: N corresponding keypoints in image i2.
@@ -88,10 +87,8 @@ def mesh_inlier_correspondences(
         gt_wTi1: ground truth pose of the world frame relative to i1.
         gt_wTi2: ground truth pose of the world frame relative to i2.
         gt_scene_mesh: ground truth triangular surface mesh of the scene in the world frame.
-
     Raises:
         ValueError: when the number of keypoints do not match.
-
     Returns:
         is_inlier: (N, ) mask of inlier correspondences.
     """
@@ -154,11 +151,89 @@ def mesh_inlier_correspondences(
         x_i1i2, y_i1i2 = forward_project(loc_i1[i1_idx[i]], fx_i2, fy_i2, cx_i2, cy_i2, gt_wTi2.inverse())
         err_i2i1 = ((x_i1 - x_i2i1)**2 + (y_i1 - y_i2i1)**2)**0.5 # pixels
         err_i1i2 = ((x_i2 - x_i1i2)**2 + (y_i2 - y_i1i2)**2)**0.5 # pixels
-        is_inlier[idr[i]] = max(err_i2i1, err_i1i2) < 10
+        is_inlier[idr[i]] = max(err_i2i1, err_i1i2) < 4
         if is_inlier[idr[i]]:
             reproj_err.append(max(err_i2i1, err_i1i2))
+    if np.count_nonzero(is_inlier) == 0: reproj_err = 0
 
     return is_inlier, np.mean(reproj_err)
+
+
+def compute_gt_correspondences(
+    keypoints_i1: Keypoints,
+    keypoints_i2: Keypoints,
+    camera_intrinsics_i1: Cal3Bundler,
+    camera_intrinsics_i2: Cal3Bundler,
+    gt_wTi1: Pose3,
+    gt_wTi2: Pose3,
+    gt_scene_mesh: trimesh.Trimesh,
+) -> np.ndarray:
+    """Compute inlier correspondences using the ground truth triangular surface mesh of the scene.
+
+    Args:
+        keypoints_i1: N keypoints in image i1.
+        keypoints_i2: N corresponding keypoints in image i2.
+        intrinsics_i1: intrinsics for i1.
+        intrinsics_i2: intrinsics for i2.
+        gt_wTi1: ground truth pose of the world frame relative to i1.
+        gt_wTi2: ground truth pose of the world frame relative to i2.
+        gt_scene_mesh: ground truth triangular surface mesh of the scene in the world frame.
+
+    Raises:
+        ValueError: when the number of keypoints do not match.
+
+    Returns:
+        is_inlier: (N, ) mask of inlier correspondences.
+    """
+    def back_project(u, v, fx, fy, cx, cy, wRi: Rot3):            
+        """Back-project ray from pixel coord"""
+        zhat = (1 + (u - cx)**2/fx**2 + (v - cy)**2/fy**2)**(-1/2)
+        xhat = zhat/fx*(u - cx)
+        yhat = zhat/fy*(v - cy)
+        return np.dot(wRi.matrix(), np.vstack((xhat, yhat, zhat))).T # (N, 3)
+
+    def forward_project(wtlw: np.ndarray, fx, fy, cx, cy, iTw: Pose3):            
+        itwi = iTw.translation().reshape((3, 1))
+        wtlw = wtlw.reshape((3, 1))
+        itli = np.dot(iTw.rotation().matrix(), wtlw) + itwi    
+        x, y, z = itli
+        return np.array([fx/z*x + cx, fy/z*y + cy]).T
+
+    coords_i1 = keypoints_i1.coordinates
+    coords_i2 = keypoints_i2.coordinates
+    fx_i1, fy_i1, cx_i1, cy_i1 = camera_intrinsics_i1.fx(), camera_intrinsics_i1.fy(), camera_intrinsics_i1.px(), camera_intrinsics_i1.py()
+    fx_i2, fy_i2, cx_i2, cy_i2 = camera_intrinsics_i2.fx(), camera_intrinsics_i2.fy(), camera_intrinsics_i2.px(), camera_intrinsics_i2.py()
+    n_kpts_i1 = len(keypoints_i1)
+    n_kpts_i2 = len(keypoints_i2)
+    src_i1 = np.repeat(np.reshape(gt_wTi1.translation(), (-1, 3)), n_kpts_i1, axis=0) 
+    src_i2 = np.repeat(np.reshape(gt_wTi2.translation(), (-1, 3)), n_kpts_i2, axis=0) 
+
+    # compute ketpoint rays
+    drc_i1 = back_project(coords_i1[:, 0], coords_i1[:, 1], fx_i1, fy_i1, cx_i1, cy_i1, gt_wTi1.rotation())
+    drc_i2 = back_project(coords_i2[:, 0], coords_i2[:, 1], fx_i2, fy_i2, cx_i2, cy_i2, gt_wTi2.rotation())
+
+    # perform ray tracing
+    src = np.vstack((src_i1, src_i2))
+    drc = np.vstack((drc_i1, drc_i2))
+    loc, idr, _ = gt_scene_mesh.ray.intersects_location(src, drc, multiple_hits=False)
+
+    # unpack results
+    idr_i1 = idr[idr < n_kpts_i1]
+    loc_i1 = loc[idr_i1]
+    idr_i2 = idr[idr >= n_kpts_i1]-n_kpts_i1
+    loc_i2 = loc[idr_i2]
+
+    # forward project intersections into other image to compute correspondences
+    # Note: compute one-sided correspondence, i.e., i2 -> i1
+    gt_corr_i1i2 = []
+    for i in range(len(idr_i2)):
+        coord_i2i1 = forward_project(loc_i2[i], fx_i1, fy_i1, cx_i1, cy_i1, gt_wTi1.inverse())
+        dists = np.linalg.norm(coords_i1[idr_i1] - coord_i2i1, axis=1)
+        idx_min = np.argmin(dists)
+        if dists[idx_min] < 4:
+            gt_corr_i1i2.append([idr_i1[idx_min], i])
+
+    return np.array(gt_corr_i1i2)
 
 
 def compute_errors_statistics(errors: List[Optional[float]]) -> StatsDict:
@@ -203,7 +278,8 @@ def compute_translation_distance_metrics(
     wti_list: List[Optional[Point3]], gt_wti_list: List[Optional[Point3]]
 ) -> StatsDict:
     """Computes statistics for the distance between estimated and GT translations.
-
+.extract_indices(corr_idxs_i1i2[:, 0])
+.extract_indices(corr_idxs_i1i2[:, 1])
     Assumes that the estimated and GT translations have been aligned and do not
     have a gauge freedom (including scale).
 