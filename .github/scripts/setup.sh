4b3de321a90b3cb691ef501fb2c2fdc2a85c25e7
Ayush Baid <ayushrakeshbaid@gmail.com>
2021-08-12 23:11:42 -0700

    adding door dataset benchmark in CI

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -4,7 +4,7 @@
 # GTSFM dependencies (including GTSAM) previously installed using conda
 ##########################################################
 
-echo "Running .github/scripts/python.sh..."
+echo "Running .github/scripts/setup.sh..."
 conda init
 conda info --envs
 
@@ -26,19 +26,3 @@ pip install -e .
 
 cd $GITHUB_WORKSPACE
 ./download_model_weights.sh
-
-##########################################################
-# Run GTSFM unit tests
-##########################################################
-
-cd $GITHUB_WORKSPACE
-
-pytest tests --cov gtsfm
-coverage report
-
-##########################################################
-# Test with flake8
-##########################################################
-
-pip install flake8
-flake8 --max-line-length 120 --ignore E201,E202,E203,E231,W291,W293,E303,W391,E402,W503,E731 gtsfm
