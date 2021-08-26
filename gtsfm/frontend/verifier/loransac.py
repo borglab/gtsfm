8b6fe74657d11c765e3a06bd5d97bc1090ad988a
John Lambert <johnlambert@hayoungs-mbp.lan>
2021-08-26 11:49:06 -0400

    add support for F matrix estimation to loransac

--- gtsfm/frontend/verifier/loransac.py
+++ gtsfm/frontend/verifier/loransac.py
@@ -14,14 +14,14 @@ On Mac, no wheel is available, so system-wide installation of COLMAP is required
 Authors: John Lambert
 """
 
-from typing import Optional, Tuple
+from typing import Any, Dict, Optional, Tuple
 
 import numpy as np
-import pycolmap
+#import pycolmap
 from gtsam import Cal3Bundler, Rot3, Unit3
-from scipy.spatial.transform import Rotation
 
 import gtsfm.utils.logger as logger_utils
+import gtsfm.utils.verification as verification_utils
 from gtsfm.common.keypoints import Keypoints
 from gtsfm.frontend.verifier.verifier_base import VerifierBase, NUM_MATCHES_REQ_E_MATRIX, NUM_MATCHES_REQ_F_MATRIX
 
@@ -66,6 +66,51 @@ class LoRansac(VerifierBase):
         # for failure, i2Ri1 = None, and i2Ui1 = None, and no verified correspondences, and inlier_ratio_est_model = 0
         self._failure_result = (None, None, np.array([], dtype=np.uint64), 0.0)
 
+    def estimate_essential_matrix(
+        self,
+        uv_i1: np.ndarray,
+        uv_i2: np.ndarray, 
+        camera_intrinsics_i1: Cal3Bundler,
+        camera_intrinsics_i2: Cal3Bundler,
+     ) -> Dict[str, Any]:
+        """Use the pycolmap Pybind wrapper to estimate an Essential matrix using LORANSAC.
+
+        Args:
+            uv_i1: array of shape (N3,2) representing coordinates of 2d points in image 1.
+            uv_i2: array of shape (N3,2) representing coordinates of 2d points in image 2.
+            camera_intrinsics_i1: intrinsics for image #i1.
+            camera_intrinsics_i2: intrinsics for image #i2.
+
+        Returns:
+            result_dict: 
+        """
+        focal_length = camera_intrinsics_i1.fx()
+        cx, cy = camera_intrinsics_i1.px(), camera_intrinsics_i1.py()
+
+        # TODO: use more accurate proxy?
+        width = int(cx * 2)
+        height = int(cy * 2)
+
+        camera_dict1 = {
+            "model": "SIMPLE_PINHOLE",
+            "width": width,
+            "height": height,
+            "params": [focal_length, cx, cy],
+        }
+
+        focal_length = camera_intrinsics_i2.fx()
+        cx, cy = camera_intrinsics_i2.px(), camera_intrinsics_i2.py()
+        camera_dict2 = {
+            "model": "SIMPLE_PINHOLE",
+            "width": width,
+            "height": height,
+            "params": [focal_length, cx, cy],
+        }
+        result_dict = pycolmap.essential_matrix_estimation(
+            points2D1, points2D2, camera_dict1, camera_dict2, max_error_px=self._estimation_threshold_px
+        )
+        return result_dict
+
     def verify(
         self,
         keypoints_i1: Keypoints,
@@ -97,51 +142,51 @@ class LoRansac(VerifierBase):
         uv_i1 = keypoints_i1.coordinates
         uv_i2 = keypoints_i2.coordinates
 
-        focal_length = camera_intrinsics_i1.fx()
-        cx, cy = camera_intrinsics_i1.px(), camera_intrinsics_i1.py()
+        uv_i1_ = uv_i1[match_indices[:, 0]]
+        uv_i2_ = uv_i2[match_indices[:, 1]]
 
-        # TODO: use more accurate proxy?
-        width = int(cx * 2)
-        height = int(cy * 2)
-
-        camera_dict1 = {
-            "model": "SIMPLE_PINHOLE",
-            "width": width,
-            "height": height,
-            "params": [focal_length, cx, cy],
-        }
-
-        focal_length = camera_intrinsics_i2.fx()
-        cx, cy = camera_intrinsics_i2.px(), camera_intrinsics_i2.py()
-        camera_dict2 = {
-            "model": "SIMPLE_PINHOLE",
-            "width": width,
-            "height": height,
-            "params": [focal_length, cx, cy],
-        }
-
-        points2D1 = uv_i1[match_indices[:, 0]]
-        points2D2 = uv_i2[match_indices[:, 1]]
-
-        result_dict = pycolmap.essential_matrix_estimation(
-            points2D1, points2D2, camera_dict1, camera_dict2, max_error_px=self._estimation_threshold_px
-        )
+        if self._use_intrinsics_in_verification:
+            result_dict = self.estimate_essential_matrix(uv_i1_, uv_i2_, camera_intrinsics_i1, camera_intrinsics_i2)
+        else:
+            result_dict = pycolmap.fundamental_matrix_estimation(
+                uv_i1_, uv_i2_, max_error_px=self._estimation_threshold_px
+            )
 
         success = result_dict["success"]
         if not success:
-            logger.info("[LORANSAC] Essential matrix estimation unsuccessful.")
+            matrix_type = "Essential" if self.use_intrinsics_in_verification else "Fundamental"
+            logger.info(f"[LORANSAC] {matrix_type} matrix estimation unsuccessful.")
             return self._failure_result
-        # See https://github.com/colmap/colmap/blob/dev/src/base/pose.h#L72
-        qw, qx, qy, qz = result_dict["qvec"]
-        i2Ui1 = result_dict["tvec"]
-        num_inliers = result_dict["num_inliers"]
 
+        num_inliers = result_dict["num_inliers"]
         inlier_ratio_est_model = num_inliers / match_indices.shape[0]
+
+        # only extract the relative pose if we have sufficient inliers.
         if inlier_ratio_est_model >= self._min_allowed_inlier_ratio_est_model:
-            i2Ri1 = Rot3(Rotation.from_quat([qx, qy, qz, qw]).as_matrix())
-            i2Ui1 = Unit3(i2Ui1)
+
             inlier_mask = result_dict["inliers"]
             v_corr_idxs = match_indices[inlier_mask]
+
+            if self._use_intrinsics_in_verification:
+                # case where E-matrix was estimated
+                # See https://github.com/colmap/colmap/blob/dev/src/base/pose.h#L72 for quaternion ordering
+                qw, qx, qy, qz = result_dict["qvec"]
+                i2Ui1 = result_dict["tvec"]
+                i2Ri1 = Rot3(qw, qx, qy, qz)
+                i2Ui1 = Unit3(i2Ui1)
+            else:
+                # case where F-matrix was estimated
+                F = result_dict["F"]
+                i2Ei1 = verification_utils.fundamental_to_essential_matrix(
+                    i2Fi1, camera_intrinsics_i1, camera_intrinsics_i2
+                )
+                (i2Ri1, i2Ui1) = verification_utils.recover_relative_pose_from_essential_matrix(
+                    i2Ei1,
+                    uv_i1_[inlier_idxs, 0],
+                    uv_i2_[inlier_idxs, 1],
+                    camera_intrinsics_i1,
+                    camera_intrinsics_i2,
+                )
         else:
             i2Ri1 = None
             i2Ui1 = None
