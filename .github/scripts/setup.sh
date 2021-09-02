147d6cf308ce798eb769782e59ad42a86cbd9e1f
John Lambert <johnlambert@gatech.edu>
2021-09-01 19:17:38 -0600

    use updated gtsam wheel for linux

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -8,9 +8,9 @@ echo "Running .github/scripts/setup.sh..."
 conda init
 conda info --envs
 
-wget -O 2021_03_12_gtsam_python38_wheel.zip --no-check-certificate "https://drive.google.com/uc?export=download&id=1JvsLYngMzSqvmD6RwEgEb_HXXHfw_EJ2"
+wget -O 2021_09_01_gtsam_python38_wheel.zip --no-check-certificate "https://drive.google.com/uc?export=download&id=1hKp9tZ82uDBKjBy1-iu45Jxpsa_TglaY"
 
-unzip 2021_03_12_gtsam_python38_wheel.zip
+unzip 2021_09_01_gtsam_python38_wheel.zip
 pip install gtsam-4.1.1-cp38-cp38-manylinux2014_x86_64.whl
 
 ##########################################################
