f194a8eb511c51e1f24552ae3657b8740d368f81
Ayush Baid <ayushrakeshbaid@gmail.com>
2021-09-05 19:17:15 -0700

    rolling back changes in unit test

--- tests/bundle/test_bundle_adjustment.py
+++ tests/bundle/test_bundle_adjustment.py
@@ -7,9 +7,11 @@ import unittest
 
 import dask
 import gtsam
+import numpy as np
 
 import gtsfm.utils.io as io_utils
 from gtsfm.bundle.bundle_adjustment import BundleAdjustmentOptimizer
+from gtsfm.common.gtsfm_data import GtsfmData
 
 GTSAM_EXAMPLE_FILE = "dubrovnik-3-7-pre"
 EXAMPLE_DATA = io_utils.read_bal(gtsam.findExampleDataFile(GTSAM_EXAMPLE_FILE))
@@ -18,15 +20,30 @@ EXAMPLE_DATA = io_utils.read_bal(gtsam.findExampleDataFile(GTSAM_EXAMPLE_FILE))
 class TestBundleAdjustmentOptimizer(unittest.TestCase):
     """Unit tests for BundleAdjustmentOptimizer class."""
 
+    def setUp(self):
+        super().setUp()
+
+        output_reproj_error_thresh = 100
+        self.obj = BundleAdjustmentOptimizer(output_reproj_error_thresh)
+
+        self.test_data = EXAMPLE_DATA
+
+    # def test_simple_scene(self):
+    #     """Test the simple scene using the `run` API."""
+
+    #     computed_result = self.obj.run(self.test_data)
+
+    #     expected_error = 0.046137573704557046
+
+    #     self.assertTrue(np.isclose(expected_error, computed_result.total_reproj_error, atol=1e-2, rtol=1e-2))
+
     def test_create_computation_graph(self):
         """Test the simple scene as dask computation graph."""
-        test_obj = BundleAdjustmentOptimizer(shared_calib=True, output_reproj_error_thresh=3)
+        sfm_data_graph = dask.delayed(self.test_data)
 
-        sfm_data_graph = dask.delayed(EXAMPLE_DATA)
+        expected_result, _ = self.obj.run(self.test_data)
 
-        expected_result, _ = test_obj.run(EXAMPLE_DATA)
-
-        computed_result, _ = test_obj.create_computation_graph(dask.delayed(sfm_data_graph))
+        computed_result, _ = self.obj.create_computation_graph(dask.delayed(sfm_data_graph))
 
         with dask.config.set(scheduler="single-threaded"):
             result = dask.compute(computed_result)[0]
