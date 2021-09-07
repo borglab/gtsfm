7b16fefa0a220f527471401a75b8c42ec9ee701f
John Lambert <johnlambert@hayoungs-mbp.lan>
2021-09-07 10:47:48 -0400

    filter correspondences as well as R,t

--- gtsfm/scene_optimizer.py
+++ gtsfm/scene_optimizer.py
@@ -177,11 +177,14 @@ class SceneOptimizer:
         auxiliary_graph_list = []
 
         # ensure cycle consistency in triplets
-        i2Ri1_graph_dict, i2Ui1_graph_dict = dask.delayed(cycle_consistency.filter_to_cycle_consistent_edges, nout=2)(
-            i2Ri1_graph_dict, i2Ui1_graph_dict, two_view_reports_dict
+        cycle_consistent_graph = dask.delayed(cycle_consistency.filter_to_cycle_consistent_edges)(
+            i2Ri1_graph_dict, i2Ui1_graph_dict, v_corr_idxs_graph_dict, two_view_reports_dict
         )
+        i2Ri1_graph_dict = cycle_consistent_graph[0]
+        i2Ui1_graph_dict = cycle_consistent_graph[1]
+        v_corr_idxs_graph_dict = cycle_consistent_graph[2]
 
-        def _filter_dict_keys(dict: Dict[Any, Any], ref_dict: Dict[Any,Any]) -> Dict[Any, Any]:
+        def _filter_dict_keys(dict: Dict[Any, Any], ref_dict: Dict[Any, Any]) -> Dict[Any, Any]:
             """Return a subset of a dictionary based on keys present in the reference dictionary."""
             valid_keys = list(ref_dict.keys())
             return {k: v for k, v in dict.items() if k in valid_keys}
