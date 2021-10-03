e09dc70eae0c22a3b9daa433b26f54038c933853
John Lambert <johnlambert@gatech.edu>
2021-10-02 22:01:17 -0600

    address Akshay suggestions range_x_coordinate -> range_x

--- gtsfm/utils/plane.py
+++ gtsfm/utils/plane.py
@@ -1,6 +1,6 @@
 """Utilities for generating data on planar surfaces.
 
-Authors: Ayush Baid
+Authors: Ayush Baid, John Lambert
 """
 from typing import Tuple
 
@@ -9,16 +9,16 @@ import numpy as np
 
 def sample_points_on_plane(
     plane_coefficients: Tuple[float, float, float, float],
-    range_x_coordinate: Tuple[float, float],
-    range_y_coordinate: Tuple[float, float],
+    range_x: Tuple[float, float],
+    range_y: Tuple[float, float],
     num_points: int,
 ) -> np.ndarray:
     """Sample random points on a 3D plane ax + by + cz + d = 0.
 
     Args:
         plane_coefficients: coefficients (a,b,c,d) of the plane equation.
-        range_x_coordinate: desired range of the x coordinates of samples.
-        range_y_coordinate: desired range of the y coordinates of samples.
+        range_x: desired range of the x coordinates of samples.
+        range_y: desired range of the y coordinates of samples.
         num_points: number of points to sample.
 
     Returns:
@@ -31,8 +31,8 @@ def sample_points_on_plane(
         raise ValueError("z-coefficient for the plane should not be zero")
 
     # sample x and y coordinates randomly
-    x = np.random.uniform(low=range_x_coordinate[0], high=range_x_coordinate[1], size=(num_points, 1))
-    y = np.random.uniform(low=range_y_coordinate[0], high=range_y_coordinate[1], size=(num_points, 1))
+    x = np.random.uniform(low=range_x[0], high=range_x[1], size=(num_points, 1))
+    y = np.random.uniform(low=range_y[0], high=range_y[1], size=(num_points, 1))
 
     # calculate z coordinates using equation of the plane
     z = -(a * x + b * y + d) / c
