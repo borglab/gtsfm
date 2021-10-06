1312096321a47520bc4eceefaa76fde6a4fa87e9
John Lambert <johnlambert@hayoungs-mbp.lan>
2021-10-06 09:10:48 -0400

    clean up var names based on Ayush's suggestions

--- gtsfm/evaluation/merge_reports.py
+++ gtsfm/evaluation/merge_reports.py
@@ -9,12 +9,12 @@ from typing import Any, Dict, List, Tuple
 
 from tabulate import tabulate
 
-GTSFM_REPORT_TABLES = Dict[str, Dict[str, Any]]
+SINGLE_REPORT_TABLES = Dict[str, Dict[str, Any]]
 # contains info about each metric, from two separate reports
-GTSFM_MERGED_REPORT_TABLES = Dict[str, List[Tuple[str, Any, Any]]]
+MERGED_REPORT_TABLES = Dict[str, List[Tuple[str, Any, Any]]]
 
 
-def extract_tables_from_report(report_fpath: str) -> GTSFM_REPORT_TABLES:
+def extract_tables_from_report(report_fpath: str) -> SINGLE_REPORT_TABLES:
     """Given an HTML text file containing HTML table info (and Plotly rendering code), rip out the table information
     for each table.
 
@@ -61,7 +61,7 @@ def extract_tables_from_report(report_fpath: str) -> GTSFM_REPORT_TABLES:
     return table_dict
 
 
-def merge_tables(table_dict1: GTSFM_REPORT_TABLES, table_dict2: GTSFM_REPORT_TABLES) -> GTSFM_MERGED_REPORT_TABLES:
+def merge_tables(table_dict1: SINGLE_REPORT_TABLES, table_dict2: SINGLE_REPORT_TABLES) -> MERGED_REPORT_TABLES:
     """Combine 2-column tables from single reports, to 3-column tables, with columns corresponding to each report."""
 
     new_table_dict = defaultdict(list)
@@ -74,7 +74,7 @@ def merge_tables(table_dict1: GTSFM_REPORT_TABLES, table_dict2: GTSFM_REPORT_TAB
     return new_table_dict
 
 
-def print_tables_tabulate(merged_table_dict: GTSFM_MERGED_REPORT_TABLES, table_format: str) -> None:
+def print_tables_tabulate(merged_table_dict: MERGED_REPORT_TABLES, table_format: str) -> None:
     """Dump to stdout Markdown-formatted tables."""
     for tab_name, tab_metrics in merged_table_dict.items():
 
@@ -85,18 +85,18 @@ def print_tables_tabulate(merged_table_dict: GTSFM_MERGED_REPORT_TABLES, table_f
         print()
 
 
-def merge_reports(report_fpath1: str, report_fpath2: str, output_format: str) -> None:
+def merge_reports(report1_fpath: str, report2_fpath: str, output_format: str) -> None:
     """Combine all tables from two reports into a single table, for side-by-side comparisons."""
-    table_dict1 = extract_tables_from_report(report_fpath1)
-    table_dict2 = extract_tables_from_report(report_fpath2)
+    table_dict1 = extract_tables_from_report(report1_fpath)
+    table_dict2 = extract_tables_from_report(report2_fpath)
     merged_table_dict = merge_tables(table_dict1, table_dict2)
     print_tables_tabulate(merged_table_dict, output_format)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--report_fpath1", required=True, help="Path to previous report.")
-    parser.add_argument("--report_fpath2", required=True, help="Path to new report.")
+    parser.add_argument("--report1_fpath", required=True, help="Path to previous report.")
+    parser.add_argument("--report2_fpath", required=True, help="Path to new report.")
     parser.add_argument(
         "--table_format",
         default="github",
@@ -104,4 +104,4 @@ if __name__ == "__main__":
         help="Output format for exported table (markdown or HTML).",
     )
     args = parser.parse_args()
-    merge_reports(args.report_fpath1, args.report_fpath2, args.table_format)
+    merge_reports(args.report1_fpath, args.report2_fpath, args.table_format)
