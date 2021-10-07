649195fa5cd5848dd5b27f02d4baa765bc551a13
John Lambert <johnlambert@gatech.edu>
2021-10-06 22:03:56 -0600

    rename plane.py to sampling.py

--- tests/utils/test_plane_utils.py
+++ tests/utils/test_plane_utils.py
@@ -1,11 +1,11 @@
-"""Unit test on utility for generating data on planar surfaces.
+"""Unit test on utility for sampling/generating data on planar surfaces.
 
 Authors: Ayush Baid, John Lambert
 """
 
 import numpy as np
 
-import gtsfm.utils.plane as plane_utils
+import gtsfm.utils.sampling as sampling_utils
 
 
 def test_sample_points_on_plane() -> None:
@@ -21,7 +21,7 @@ def test_sample_points_on_plane() -> None:
     # plane at z=10, so ax + by + cz + d = 0 + 0 + -z + 10 = 0
     plane_coefficients = (0, 0, -1, 10)
 
-    pts = plane_utils.sample_points_on_plane(plane_coefficients, range_x, range_y, num_points)
+    pts = sampling_utils.sample_points_on_plane(plane_coefficients, range_x, range_y, num_points)
 
     # ensure ax + by + cz + d = 0
     pts_residuals = pts @ np.array(plane_coefficients[:3]).reshape(3, 1) + plane_coefficients[3]
