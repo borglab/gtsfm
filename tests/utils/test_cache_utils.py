e544b8096fa60375c8dd75d37719a4e744918990
Ayush Baid <ayushrakeshbaid@gmail.com>
2021-10-12 19:02:40 -0700

    adding asserts in cache util tests

--- tests/utils/test_cache_utils.py
+++ tests/utils/test_cache_utils.py
@@ -6,7 +6,6 @@ import unittest
 from pathlib import Path
 
 import numpy as np
-from gtsfm.common.image import Image
 
 import gtsfm.utils.cache as cache_utils
 import gtsfm.utils.io as io_utils
@@ -18,14 +17,17 @@ class TestCacheUtils(unittest.TestCase):
     """Unit tests for cache utils."""
 
     def test_generate_hash_for_numpy_array(self):
-        numpy_arr = np.random.randn(100, 200)
+        numpy_arr = np.array([1.0, 2.0, 3.5, -1.0, -2.0, 1.0]).reshape((2, 3))
         key = cache_utils.generate_hash_for_numpy_array(numpy_arr)
+        expected = "22ed44b44ded282578e67f7d4c759038be79db5b"
+        self.assertEqual(key, expected)
 
     def test_generate_hash_for_image(self):
         image_path = TEST_DATA_ROOT_PATH / "set1_lund_door" / "images" / "DSC_0001.JPG"
         image = io_utils.load_image(str(image_path))
-
         key = cache_utils.generate_hash_for_image(image=image)
+        expected = "033223b24a9edfe6e989b6853db295df51011f3cdc58c96e361e565c018e33ff4630345c79c09188"
+        self.assertEqual(key, expected)
 
 
 if __name__ == "__main__":
