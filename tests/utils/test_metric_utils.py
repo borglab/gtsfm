a2d8c4840166bd199d2862dc61023b6db7f8f1db
Travis Driver <travisdriver@gatech.edu>
2021-11-02 22:48:48 -0400

    Addressing comments

--- tests/utils/test_metric_utils.py
+++ tests/utils/test_metric_utils.py
@@ -21,17 +21,20 @@ class TestMetricUtils(unittest.TestCase):
         # Create cube mesh with side length one centered at origin.
         box = trimesh.primitives.Box()
 
-        # Create arrangement of two camera pointing at center of one of the cube's faces.
-        calibration = Cal3Bundler(10, 0, 0, 1, 1)
+        # Create arrangement of two cameras pointing at the center of one of the cube's faces.
+        fx, k1, k2, u0, v0 = 10, 0, 0, 1, 1
+        calibration = Cal3Bundler(fx, k1, k2, u0, v0)
+        cam_pos = [[2, 1, 0], [2, -1, 0]]
         target_pos = [0.5, 0, 0]
-        cam_i1 = PinholeCameraCal3Bundler().Lookat([2, 1, 0], target_pos, [0, -1, 0], calibration)
-        cam_i2 = PinholeCameraCal3Bundler().Lookat([2, -1, 0], target_pos, [0, -1, 0], calibration)
+        up_vector = [0, -1, 0]
+        cam_i1 = PinholeCameraCal3Bundler().Lookat(cam_pos[0], target_pos, up_vector, calibration)
+        cam_i2 = PinholeCameraCal3Bundler().Lookat(cam_pos[1], target_pos, up_vector, calibration)
         keypoints_i1 = Keypoints(coordinates=np.array([[1, 1]]).astype(np.float32))
         keypoints_i2 = Keypoints(coordinates=np.array([[1, 1]]).astype(np.float32))
 
         # Project keypoint at center of each simulated image and record intersection.
         is_inlier, reproj_err = metric_utils.mesh_inlier_correspondences(
-            keypoints_i1, keypoints_i2, cam_i1, cam_i2, box, 0.1
+            keypoints_i1, keypoints_i2, cam_i1, cam_i2, box, dist_threshold=0.1
         )
         assert np.count_nonzero(is_inlier) == 1
         assert reproj_err[0] < 1e-4
@@ -41,11 +44,13 @@ class TestMetricUtils(unittest.TestCase):
         # Create cube mesh with side length one centered at origin.
         box = trimesh.primitives.Box()
 
-        # Create arrangement of 4 cameras in x-y plane pointing at the cube.
-        calibration = Cal3Bundler(10, 0, 0, 1, 1)
+        # Create arrangement of 4 cameras in x-z plane pointing at the cube.
+        fx, k1, k2, u0, v0 = 10, 0, 0, 1, 1
+        calibration = Cal3Bundler(fx, k1, k2, u0, v0)
         cam_pos = [[2, 0, 0], [-2, 0, 0], [0, 0, 2], [0, 0, -2]]
         target_pos = [0, 0, 0]
-        cams = [PinholeCameraCal3Bundler().Lookat(c, target_pos, [0, -1, 0], calibration) for c in cam_pos]
+        up_vector = [0, -1, 0]
+        cams = [PinholeCameraCal3Bundler().Lookat(c, target_pos, up_vector, calibration) for c in cam_pos]
 
         # Project keypoint at center of each simulated image and record intersection.
         kpt = Keypoints(coordinates=np.array([[1, 1]]).astype(np.float32))
