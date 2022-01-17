43ae5c5899f0f039610e903f8fbd49824e4fef14
John Lambert <johnlambert@gatech.edu>
2022-01-17 13:36:30 -0500

    move get_valid_input_edges() to base class

--- gtsfm/view_graph_estimator/view_graph_estimator_base.py
+++ gtsfm/view_graph_estimator/view_graph_estimator_base.py
@@ -58,6 +58,28 @@ class ViewGraphEstimatorBase(metaclass=abc.ABCMeta):
             Edges of the view-graph, which are the subset of the image pairs in the input args.
         """
 
+    def __get_valid_input_edges(self, i2Ri1_dict: Dict[Tuple[int, int], Rot3]) -> List[Tuple[int, int]]:
+        """Gets the input edges (i1, i2) with the relative rotation i2Ri1 where:
+        1. i1 < i2
+        2. i2Ri1 is not None
+
+        Args:
+            i2Ri1_dict: input dictionary of relative rotations.
+
+        Returns:
+            List of valid edges.
+        """
+        valid_edges = []
+        for (i1, i2), i2Ri1 in i2Ri1_dict.items():
+            if i2Ri1 is None:
+                continue  # edge was previously discarded for insufficient support
+            if i1 >= i2:
+                logger.error("Incorrectly ordered edge indices found in cycle consistency for (%d, %d)", i1, i2)
+                continue
+            valid_edges.append((i1, i2))
+
+        return valid_edges
+
     def _filter_with_edges(
         self,
         i2Ri1_dict: Dict[Tuple[int, int], Rot3],
@@ -123,7 +145,6 @@ class ViewGraphEstimatorBase(metaclass=abc.ABCMeta):
         if len(two_view_reports) == 0:
             return GtsfmMetricsGroup(name="rotation_cycle_consistency_metrics", metrics=[])
 
-        input_edges = two_view_reports.keys()
         valid_edges = self.__get_valid_input_edges(i2Ri1_dict)
         inlier_i1_i2 = view_graph_edges
         outlier_i1_i2 = list(set(valid_edges) - set(inlier_i1_i2))
