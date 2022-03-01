6bc9d29841c7fe17152c96839153a5bdc2d28cd8
Akshay Krishnan <akshay.krishnan.30@gmail.com>
2022-03-01 08:21:23 -0800

    Add assert_all_results to repro test base

--- tests/repro_tests/test_repro_base.py
+++ tests/repro_tests/test_repro_base.py
@@ -3,12 +3,19 @@
 Authors: Ayush Baid
 """
 import abc
-from typing import Any
+from typing import Any, List
 
 NUM_REPETITIONS = 10
 
 
 class ReproducibilityTestBase(metaclass=abc.ABCMeta):
+    """A base class to define reproducibility tests.
+    
+    The class provides two ways to test reproducibility:
+        - assert_results(): To compare the result at each iteration to a reference.
+        - assert_all_results() (Optional): To check statistics on all results together.
+    """
+
     @abc.abstractmethod
     def run_once(self) -> Any:
         """Run the function under test once"""
@@ -20,7 +27,17 @@ class ReproducibilityTestBase(metaclass=abc.ABCMeta):
     def test_repeatability(self) -> None:
         """Tests the repeatability of the function under test."""
         reference_result = self.run_once()
+        all_results = [reference_result]
 
         for _ in range(NUM_REPETITIONS):
             current_result = self.run_once()
+            all_results.append(current_result)
             self.assert_results(current_result, reference_result)
+        
+        self.assert_all_results(all_results)
+
+    def assert_all_results(self, all_results: List[Any]) -> None:
+        """Check for any statistics across all_results.
+        
+        This default implementation does not check anything, but can be overrided by derived classes."""
+        return None
