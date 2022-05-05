947b678737561cebde27b39775130335d7e131f7
Frank Dellaert <dellaert@gmail.com>
2022-05-05 16:37:22 -0400

    Use unittest framework

--- tests/retriever/test_netvlad_retriever.py
+++ tests/retriever/test_netvlad_retriever.py
@@ -3,10 +3,10 @@
 Authors: John Lambert
 """
 
+import unittest
 from pathlib import Path
 
-from dask.distributed import LocalCluster, Client
-
+from dask.distributed import Client, LocalCluster
 from gtsfm.loader.colmap_loader import ColmapLoader
 from gtsfm.loader.olsson_loader import OlssonLoader
 from gtsfm.retriever.netvlad_retriever import NetVLADRetriever
@@ -16,73 +16,75 @@ DOOR_DATA_ROOT = DATA_ROOT_PATH / "set1_lund_door"
 SKYDIO_DATA_ROOT = DATA_ROOT_PATH / "crane_mast_8imgs_colmap_output"
 
 
-def test_netvlad_retriever_crane_mast() -> None:
-    """Test the NetVLAD retriever on 2 frames of the Skydio Crane-Mast dataset."""
-    colmap_files_dirpath = SKYDIO_DATA_ROOT
-    images_dir = SKYDIO_DATA_ROOT / "images"
+class TestNetVLADRetriever(unittest.TestCase):
+    def test_netvlad_retriever_crane_mast(self) -> None:
+        """Test the NetVLAD retriever on 2 frames of the Skydio Crane-Mast dataset."""
+        colmap_files_dirpath = SKYDIO_DATA_ROOT
+        images_dir = SKYDIO_DATA_ROOT / "images"
 
-    loader = ColmapLoader(
-        colmap_files_dirpath=colmap_files_dirpath, images_dir=images_dir, max_frame_lookahead=100, max_resolution=760
-    )
-    num_images = 2
+        loader = ColmapLoader(
+            colmap_files_dirpath=colmap_files_dirpath,
+            images_dir=images_dir,
+            max_frame_lookahead=100,
+            max_resolution=760,
+        )
+        num_images = 2
 
-    retriever = NetVLADRetriever(num_matched=2)
+        retriever = NetVLADRetriever(num_matched=2)
 
-    # create dask client
-    cluster = LocalCluster(n_workers=1, threads_per_worker=4)
-    pairs_graph = retriever.create_computation_graph(loader=loader)
-    with Client(cluster):
-        pairs = pairs_graph.compute()
+        # create dask client
+        cluster = LocalCluster(n_workers=1, threads_per_worker=4)
+        pairs_graph = retriever.create_computation_graph(loader=loader)
+        with Client(cluster):
+            pairs = pairs_graph.compute()
 
-    # only 1 pair possible between frame 0 and frame 1
-    assert len(pairs) == 1
-    assert pairs == [(0, 1)]
+        # only 1 pair possible between frame 0 and frame 1
+        self.assertEqual(len(pairs), 1)
+        self.assertEqual(pairs, [(0, 1)])
 
+    def test_netvlad_retriever_door(self) -> None:
+        """Test the NetVLAD retriever on 12 frames of the Lund Door Dataset."""
+        loader = OlssonLoader(folder=DOOR_DATA_ROOT, image_extension="JPG")
+        retriever = NetVLADRetriever(num_matched=2)
 
-def test_netvlad_retriever_door() -> None:
-    """Test the NetVLAD retriever on 12 frames of the Lund Door Dataset."""
-    loader = OlssonLoader(folder=DOOR_DATA_ROOT, image_extension="JPG")
-    retriever = NetVLADRetriever(num_matched=2)
+        # create dask client
+        cluster = LocalCluster(n_workers=1, threads_per_worker=4)
+        pairs_graph = retriever.create_computation_graph(loader=loader)
+        with Client(cluster):
+            pairs = pairs_graph.compute()
 
-    # create dask client
-    cluster = LocalCluster(n_workers=1, threads_per_worker=4)
-    pairs_graph = retriever.create_computation_graph(loader=loader)
-    with Client(cluster):
-        pairs = pairs_graph.compute()
+        self.assertEqual(len(pairs), 21)
 
-    assert len(pairs) == 21
+        for (i1, i2) in pairs:
+            self.assertTrue(i1 != i2)
+            self.assertTrue(i1 < i2)
 
-    for (i1, i2) in pairs:
-        assert i1 != i2
-        assert i1 < i2
-
-    # closest image is most similar for the Door dataset.
-    expected_pairs = [
-        (0, 1),
-        (0, 2),
-        (1, 2),
-        (1, 3),
-        (2, 3),
-        (2, 4),
-        (3, 4),
-        (3, 5),
-        (4, 5),
-        (4, 6),
-        (5, 6),
-        (5, 7),
-        (6, 7),
-        (6, 8),
-        (7, 8),
-        (7, 10),
-        (8, 9),
-        (8, 10),
-        (9, 10),
-        (9, 11),
-        (10, 11),
-    ]
-    assert pairs == expected_pairs
+        # closest image is most similar for the Door dataset.
+        expected_pairs = [
+            (0, 1),
+            (0, 2),
+            (1, 2),
+            (1, 3),
+            (2, 3),
+            (2, 4),
+            (3, 4),
+            (3, 5),
+            (4, 5),
+            (4, 6),
+            (5, 6),
+            (5, 7),
+            (6, 7),
+            (6, 8),
+            (7, 8),
+            (7, 10),
+            (8, 9),
+            (8, 10),
+            (9, 10),
+            (9, 11),
+            (10, 11),
+        ]
+        self.assertEqual(pairs, expected_pairs)
 
 
 if __name__ == "__main__":
-    test_netvlad_retriever_crane_mast()
-    test_netvlad_retriever_door()
+    unittest.main()
--- tests/retriever/test_sequential_retriever.py
+++ tests/retriever/test_sequential_retriever.py
@@ -3,6 +3,7 @@
 Author: John Lambert
 """
 
+import unittest
 from pathlib import Path
 
 from gtsfm.loader.olsson_loader import OlssonLoader
@@ -12,50 +13,51 @@ DATA_ROOT_PATH = Path(__file__).resolve().parent.parent / "data"
 DEFAULT_FOLDER = DATA_ROOT_PATH / "set1_lund_door"
 
 
-def test_sequential_retriever() -> None:
-    """Assert that we get 30 total matches with a lookahead of 3 frames on the Door Dataset."""
+class TestSequentialRetriever(unittest.TestCase):
+    def test_sequential_retriever(self) -> None:
+        """Assert that we get 30 total matches with a lookahead of 3 frames on the Door Dataset."""
 
-    max_frame_lookahead = 3
-    loader = OlssonLoader(str(DEFAULT_FOLDER), image_extension="JPG", max_frame_lookahead=max_frame_lookahead)
-    retriever = SequentialRetriever(max_frame_lookahead=max_frame_lookahead)
-    pairs = retriever.run(loader=loader)
+        max_frame_lookahead = 3
+        loader = OlssonLoader(str(DEFAULT_FOLDER), image_extension="JPG", max_frame_lookahead=max_frame_lookahead)
+        retriever = SequentialRetriever(max_frame_lookahead=max_frame_lookahead)
+        pairs = retriever.run(loader=loader)
 
-    expected_pairs = [
-        (0, 1),
-        (0, 2),
-        (0, 3),
-        (1, 2),
-        (1, 3),
-        (1, 4),
-        (2, 3),
-        (2, 4),
-        (2, 5),
-        (3, 4),
-        (3, 5),
-        (3, 6),
-        (4, 5),
-        (4, 6),
-        (4, 7),
-        (5, 6),
-        (5, 7),
-        (5, 8),
-        (6, 7),
-        (6, 8),
-        (6, 9),
-        (7, 8),
-        (7, 9),
-        (7, 10),
-        (8, 9),
-        (8, 10),
-        (8, 11),
-        (9, 10),
-        (9, 11),
-        (10, 11),
-    ]
-    assert pairs == expected_pairs
-    # all images have 3 potential forward match pairs, except last three which have only 2, 1, and 0 such forward pairs
-    assert len(pairs) == (9 * 3) + 2 + 1
+        expected_pairs = [
+            (0, 1),
+            (0, 2),
+            (0, 3),
+            (1, 2),
+            (1, 3),
+            (1, 4),
+            (2, 3),
+            (2, 4),
+            (2, 5),
+            (3, 4),
+            (3, 5),
+            (3, 6),
+            (4, 5),
+            (4, 6),
+            (4, 7),
+            (5, 6),
+            (5, 7),
+            (5, 8),
+            (6, 7),
+            (6, 8),
+            (6, 9),
+            (7, 8),
+            (7, 9),
+            (7, 10),
+            (8, 9),
+            (8, 10),
+            (8, 11),
+            (9, 10),
+            (9, 11),
+            (10, 11),
+        ]
+        self.assertEqual(pairs, expected_pairs)
+        # all images have 3 potential forward match pairs, except last three which have only 2, 1, and 0 such forward pairs
+        self.assertEqual(len(pairs), (9 * 3) + 2 + 1)
 
 
 if __name__ == "__main__":
-    test_sequential_retriever()
+    unittest.main()
