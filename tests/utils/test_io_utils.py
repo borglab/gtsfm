a6ab5904c4cbd947d7913805574ab046bbc76439
Frank Dellaert <dellaert@gmail.com>
2022-05-06 16:37:31 -0400

    Add tests, refactor to unittest

--- tests/utils/test_io_utils.py
+++ tests/utils/test_io_utils.py
@@ -1,167 +1,178 @@
 import os
-from pathlib import Path
 import tempfile
+import unittest
+from pathlib import Path
 
+import gtsam
+import gtsfm.utils.io as io_utils
 import numpy as np
 import numpy.testing as npt
 from gtsam import Cal3Bundler, PinholeCameraCal3Bundler, Pose3, Rot3
-
-import gtsfm.utils.io as io_utils
 from gtsfm.common.gtsfm_data import GtsfmData
 from gtsfm.common.image import Image
 
 TEST_DATA_ROOT = Path(__file__).resolve().parent.parent / "data"
 
 
-def test_load_image() -> None:
-    """Ensure focal length can be read from EXIF, for an image w/ known EXIF."""
-    img_fpath = TEST_DATA_ROOT / "set2_lund_door_nointrinsics/images/DSC_0001.JPG"
-    img = io_utils.load_image(img_fpath)
-    assert img.exif_data.get("FocalLength") == 29
+class TestUoUtils(unittest.TestCase):
+    def test_load_image(self) -> None:
+        """Ensure focal length can be read from EXIF, for an image w/ known EXIF."""
+        img_fpath = TEST_DATA_ROOT / "set2_lund_door_nointrinsics/images/DSC_0001.JPG"
+        img = io_utils.load_image(img_fpath)
+        self.assertEqual(img.exif_data.get("FocalLength"), 29)
 
+    def test_read_points_txt(self) -> None:
+        """ """
+        fpath = TEST_DATA_ROOT / "crane_mast_8imgs_colmap_output" / "points3D.txt"
+        point_cloud, rgb = io_utils.read_points_txt(fpath)
 
-def test_read_points_txt() -> None:
-    """ """
-    fpath = TEST_DATA_ROOT / "crane_mast_8imgs_colmap_output" / "points3D.txt"
-    point_cloud, rgb = io_utils.read_points_txt(fpath)
+        self.assertIsInstance(point_cloud, np.ndarray)
+        self.assertIsInstance(rgb, np.ndarray)
 
-    assert isinstance(point_cloud, np.ndarray)
-    assert isinstance(rgb, np.ndarray)
+        self.assertEqual(point_cloud.shape, (2122, 3))
+        self.assertEqual(point_cloud.dtype, np.float64)
 
-    assert point_cloud.shape == (2122, 3)
-    assert point_cloud.dtype == np.float64
+        self.assertEqual(rgb.shape, (2122, 3))
+        self.assertEqual(rgb.dtype, np.uint8)
 
-    assert rgb.shape == (2122, 3)
-    assert rgb.dtype == np.uint8
+    def test_read_points_txt_nonexistent_file(self) -> None:
+        """Ensure that providing a path to a nonexistent file returns None for both return args."""
+        fpath = "nonexistent_dir/points.txt"
+        point_cloud, rgb = io_utils.read_points_txt(fpath)
 
+        self.assertIsNone(point_cloud)
+        self.assertIsNone(rgb)
 
-def test_read_points_txt_nonexistent_file() -> None:
-    """Ensure that providing a path to a nonexistent file returns None for both return args."""
-    fpath = "nonexistent_dir/points.txt"
-    point_cloud, rgb = io_utils.read_points_txt(fpath)
+    def test_read_images_txt(self) -> None:
+        """ """
+        fpath = TEST_DATA_ROOT / "crane_mast_8imgs_colmap_output" / "images.txt"
+        wTi_list, img_filenames = io_utils.read_images_txt(fpath)
 
-    assert point_cloud is None
-    assert rgb is None
+        self.assertTrue(all([isinstance(wTi, Pose3) for wTi in wTi_list]))
+        self.assertTrue(all([isinstance(img_fname, str) for img_fname in img_filenames]))
 
-
-def test_read_images_txt() -> None:
-    """ """
-    fpath = TEST_DATA_ROOT / "crane_mast_8imgs_colmap_output" / "images.txt"
-    wTi_list, img_fnames = io_utils.read_images_txt(fpath)
-
-    assert all([isinstance(wTi, Pose3) for wTi in wTi_list])
-    assert all([isinstance(img_fname, str) for img_fname in img_fnames])
-
-    expected_img_fnames = [
-        "crane_mast_1.jpg",
-        "crane_mast_2.jpg",
-        "crane_mast_3.jpg",
-        "crane_mast_4.jpg",
-        "crane_mast_5.jpg",
-        "crane_mast_6.jpg",
-        "crane_mast_7.jpg",
-        "crane_mast_8.jpg",
-    ]
-    assert img_fnames == expected_img_fnames
-
-
-def test_read_images_txt_nonexistent_file() -> None:
-    """Ensure that providing a path to a nonexistent file returns None for both return args."""
-    fpath = "nonexistent_dir/images.txt"
-    wTi_list, img_fnames = io_utils.read_images_txt(fpath)
-    assert wTi_list is None
-    assert img_fnames is None
-
-
-def test_read_cameras_txt() -> None:
-    """Ensure that shared calibration from COLMAP output is read in as a single calibration."""
-    fpath = TEST_DATA_ROOT / "crane_mast_8imgs_colmap_output" / "cameras.txt"
-    calibrations = io_utils.read_cameras_txt(fpath)
-
-    assert isinstance(calibrations, list)
-    assert all([isinstance(calibration, Cal3Bundler) for calibration in calibrations])
-
-    assert len(calibrations) == 1
-    K = calibrations[0]
-    assert K.fx() == 2332.47
-    assert K.px() == 2028
-    assert K.py() == 1520
-
-
-def test_read_cameras_txt_nonexistent_file() -> None:
-    """Ensure that providing a path to a nonexistent file returns None for calibrations return arg."""
-    fpath = "nonexistent_dir/cameras.txt"
-    calibrations = io_utils.read_cameras_txt(fpath)
-    assert calibrations is None
-
-
-def test_round_trip_images_txt() -> None:
-    """Starts with a pose. Writes the pose to images.txt (in a temporary directory). Then reads images.txt to recover
-    that same pose. Checks if the original wTc and recovered wTc match up."""
-
-    # fmt: off
-    # Rotation 45 degrees about the z-axis.
-    original_wRc = np.array(
-        [
-            [np.cos(np.pi / 4), -np.sin(np.pi / 4), 0],
-            [np.sin(np.pi / 4), np.cos(np.pi / 4), 0],
-            [0, 0, 1]
+        expected_img_filenames = [
+            "crane_mast_1.jpg",
+            "crane_mast_2.jpg",
+            "crane_mast_3.jpg",
+            "crane_mast_4.jpg",
+            "crane_mast_5.jpg",
+            "crane_mast_6.jpg",
+            "crane_mast_7.jpg",
+            "crane_mast_8.jpg",
         ]
-    )
-    original_wtc = np.array([3,-2,1])
-    # fmt: on
+        self.assertEqual(img_filenames, expected_img_filenames)
 
-    # Setup dummy GtsfmData Object with one image
-    original_wTc = Pose3(Rot3(original_wRc), original_wtc)
-    default_intrinsics = Cal3Bundler(fx=100, k1=0, k2=0, u0=0, v0=0)
-    camera = PinholeCameraCal3Bundler(original_wTc, default_intrinsics)
-    gtsfm_data = GtsfmData(number_images=1)
-    gtsfm_data.add_camera(0, camera)
+    def test_read_images_txt_nonexistent_file(self) -> None:
+        """Ensure that providing a path to a nonexistent file returns None for both return args."""
+        fpath = "nonexistent_dir/images.txt"
+        wTi_list, img_filenames = io_utils.read_images_txt(fpath)
+        self.assertIsNone(wTi_list)
+        self.assertIsNone(img_filenames)
 
-    image = Image(value_array=None, file_name="dummy_image.jpg")
-    images = [image]
+    def test_read_cameras_txt(self) -> None:
+        """Ensure that shared calibration from COLMAP output is read in as a single calibration."""
+        fpath = TEST_DATA_ROOT / "crane_mast_8imgs_colmap_output" / "cameras.txt"
+        calibrations = io_utils.read_cameras_txt(fpath)
 
-    # Perform write and read operations inside a temporary directory
-    with tempfile.TemporaryDirectory() as tempdir:
-        images_fpath = os.path.join(tempdir, "images.txt")
+        self.assertIsInstance(calibrations, list)
+        self.assertTrue(all([isinstance(calibration, Cal3Bundler) for calibration in calibrations]))
 
-        io_utils.write_images(gtsfm_data, images, tempdir)
-        wTi_list, _ = io_utils.read_images_txt(images_fpath)
-        recovered_wTc = wTi_list[0]
+        self.assertEqual(len(calibrations), 1)
+        K = calibrations[0]
+        self.assertEqual(K.fx(), 2332.47)
+        self.assertEqual(K.px(), 2028)
+        self.assertEqual(K.py(), 1520)
 
-    npt.assert_almost_equal(original_wTc.matrix(), recovered_wTc.matrix(), decimal=3)
+    def test_read_cameras_txt_nonexistent_file(self) -> None:
+        """Ensure that providing a path to a nonexistent file returns None for calibrations return arg."""
+        fpath = "nonexistent_dir/cameras.txt"
+        calibrations = io_utils.read_cameras_txt(fpath)
+        self.assertIsNone(calibrations)
+
+    def test_round_trip_images_txt(self) -> None:
+        """Starts with a pose. Writes the pose to images.txt (in a temporary directory). Then reads images.txt to recover
+        that same pose. Checks if the original wTc and recovered wTc match up."""
+
+        # fmt: off
+        # Rotation 45 degrees about the z-axis.
+        original_wRc = np.array(
+            [
+                [np.cos(np.pi / 4), -np.sin(np.pi / 4), 0],
+                [np.sin(np.pi / 4), np.cos(np.pi / 4), 0],
+                [0, 0, 1]
+            ]
+        )
+        original_wtc = np.array([3,-2,1])
+        # fmt: on
+
+        # Setup dummy GtsfmData Object with one image
+        original_wTc = Pose3(Rot3(original_wRc), original_wtc)
+        default_intrinsics = Cal3Bundler(fx=100, k1=0, k2=0, u0=0, v0=0)
+        camera = PinholeCameraCal3Bundler(original_wTc, default_intrinsics)
+        gtsfm_data = GtsfmData(number_images=1)
+        gtsfm_data.add_camera(0, camera)
+
+        image = Image(value_array=None, file_name="dummy_image.jpg")
+        images = [image]
+
+        # Perform write and read operations inside a temporary directory
+        with tempfile.TemporaryDirectory() as tempdir:
+            images_fpath = os.path.join(tempdir, "images.txt")
+
+            io_utils.write_images(gtsfm_data, images, tempdir)
+            wTi_list, _ = io_utils.read_images_txt(images_fpath)
+            recovered_wTc = wTi_list[0]
+
+        npt.assert_almost_equal(original_wTc.matrix(), recovered_wTc.matrix(), decimal=3)
+
+    def test_save_point_cloud_as_ply(self) -> None:
+        """Round-trip test on .ply file read/write, with a point cloud colored as all red."""
+        N = 10000
+        # generate a cuboid of size 1 x 2 x 3 meters.
+        points = np.random.uniform(low=[0, 0, 0], high=[1, 2, 3], size=(N, 3))
+        # Color uniformly as red.
+        rgb = np.zeros((N, 3), dtype=np.uint8)
+        rgb[:, 0] = 255
+
+        with tempfile.TemporaryDirectory() as tempdir:
+            save_fpath = f"{tempdir}/pointcloud.ply"
+            io_utils.save_point_cloud_as_ply(save_fpath=save_fpath, points=points, rgb=rgb)
+            points_read, rgb_read = io_utils.read_point_cloud_from_ply(ply_fpath=save_fpath)
+
+        np.testing.assert_allclose(points_read, points)
+        np.testing.assert_allclose(rgb_read, rgb)
+        self.assertEqual(rgb_read.dtype, np.uint8)
+
+    def test_save_point_cloud_as_ply_uncolored(self) -> None:
+        """Round-trip test on .ply file read/write, with an uncolored point cloud."""
+        N = 10000
+        points = np.random.uniform(low=[0, 0, 0], high=[1, 2, 3], size=(N, 3))
+
+        with tempfile.TemporaryDirectory() as tempdir:
+            save_fpath = f"{tempdir}/pointcloud.ply"
+            io_utils.save_point_cloud_as_ply(save_fpath=save_fpath, points=points)
+            points_read, rgb_read = io_utils.read_point_cloud_from_ply(ply_fpath=save_fpath)
+
+        rgb_expected = np.zeros((N, 3), dtype=np.uint8)
+        np.testing.assert_allclose(points, points_read)
+        np.testing.assert_allclose(rgb_read, rgb_expected)
+        self.assertEqual(rgb_read.dtype, np.uint8)
+
+    def test_read_bal(self) -> None:
+        """Check that read_bal creates correct GtsfmData object."""
+        filename: str = gtsam.findExampleDataFile("5pointExample1.txt")
+        data: GtsfmData = io_utils.read_bal(filename)
+        self.assertEqual(data.number_images(), 2)
+        self.assertEqual(data.number_tracks(), 5)
+
+    def test_read_bundler(self) -> None:
+        """Check that read_bal creates correct GtsfmData object."""
+        filename: str = gtsam.findExampleDataFile("Balbianello.out")
+        data: GtsfmData = io_utils.read_bundler(filename)
+        self.assertEqual(data.number_images(), 5)
+        self.assertEqual(data.number_tracks(), 544)
 
 
-def test_save_point_cloud_as_ply() -> None:
-    """Round-trip test on .ply file read/write, with a point cloud colored as all red."""
-    N = 10000
-    # generate a cuboid of size 1 x 2 x 3 meters.
-    points = np.random.uniform(low=[0, 0, 0], high=[1, 2, 3], size=(N, 3))
-    # Color uniformly as red.
-    rgb = np.zeros((N, 3), dtype=np.uint8)
-    rgb[:, 0] = 255
-
-    with tempfile.TemporaryDirectory() as tempdir:
-        save_fpath = f"{tempdir}/pointcloud.ply"
-        io_utils.save_point_cloud_as_ply(save_fpath=save_fpath, points=points, rgb=rgb)
-        points_read, rgb_read = io_utils.read_point_cloud_from_ply(ply_fpath=save_fpath)
-
-    assert np.allclose(points_read, points)
-    assert np.allclose(rgb_read, rgb)
-    assert rgb_read.dtype == np.uint8
-
-
-def test_save_point_cloud_as_ply_uncolored() -> None:
-    """Round-trip test on .ply file read/write, with an uncolored point cloud."""
-    N = 10000
-    points = np.random.uniform(low=[0, 0, 0], high=[1, 2, 3], size=(N, 3))
-
-    with tempfile.TemporaryDirectory() as tempdir:
-        save_fpath = f"{tempdir}/pointcloud.ply"
-        io_utils.save_point_cloud_as_ply(save_fpath=save_fpath, points=points)
-        points_read, rgb_read = io_utils.read_point_cloud_from_ply(ply_fpath=save_fpath)
-
-    rgb_expected = np.zeros((N, 3), dtype=np.uint8)
-    assert np.allclose(points, points_read)
-    assert np.allclose(rgb_read, rgb_expected)
-    assert rgb_read.dtype == np.uint8
+if __name__ == "__main__":
+    unittest.main()
