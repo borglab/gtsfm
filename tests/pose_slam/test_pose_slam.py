58710e245a7cecc43d6714e4dff1d585a04ffa0a
Akshay Krishnan <akshay.krishnan.30@gmail.com>
2022-05-15 15:17:05 -0700

    simple example passes

--- tests/pose_slam/test_pose_slam.py
+++ tests/pose_slam/test_pose_slam.py
@@ -10,12 +10,15 @@ import gtsam
 import numpy as np
 from dask.delayed import Delayed
 from gtsam import Point3, Pose3, Rot3, Unit3
-from gtsfm.common.pose_prior import PosePrior
+from gtsam.utils.test_case import GtsamTestCase
+from gtsfm.common.pose_prior import PosePrior, PosePriorType
 from gtsfm.evaluation.metrics import GtsfmMetricsGroup
 
 import gtsfm.utils.geometry_comparisons as geometry_comparisons
 # from gtsfm.pose_slam.pose_slam import PoseSlam
 
+PRIOR_NOISE_SIGMAS = [0.001, 0.001, 0.001, 0.1, 0.1, 0.1]
+POSE_PRIOR_NOISE  = gtsam.noiseModel.Diagonal.Sigmas(PRIOR_NOISE_SIGMAS)
 
 class PoseSlam:
     def run_pose_slam(
@@ -35,6 +38,25 @@ class PoseSlam:
             List of Optional[Pose3], 
             GtsfmMetricsGroup of 1DSfM metrics.
         """
+        pose_init_graph = gtsam.NonlinearFactorGraph()
+
+        for (i1, i2), i1Ti2_prior in relative_pose_priors.items():
+            pose_init_graph.push_back(
+                gtsam.BetweenFactorPose3(
+                    i1,
+                    i2,
+                    i1Ti2_prior.value,
+                    gtsam.noiseModel.Gaussian.Covariance(i1Ti2_prior.covariance),
+                )
+            )
+        pose_init_graph.push_back(
+          gtsam.PriorFactorPose3(0, Pose3(), POSE_PRIOR_NOISE)
+        )
+
+        values = gtsam.InitializePose3.initialize(pose_init_graph)
+        poses = [values.atPose3(i) if values.exists(i) else None for i in range(num_images)]
+
+        return poses, None
 
     def create_computation_graph(
         self,
@@ -57,7 +79,7 @@ class PoseSlam:
             num_images, relative_pose_priors, gt_wTi_list
         )
 
-class TestPoseSlam(unittest.TestCase):
+class TestPoseSlam(GtsamTestCase):
     """Test class for 1DSFM rotation averaging."""
 
     def setUp(self):
@@ -65,14 +87,48 @@ class TestPoseSlam(unittest.TestCase):
 
         self.slam: PoseSlam = PoseSlam()
 
-    def test_all(self):
+    def test_create_computation_graph(self):
         """Check that the interface works."""
         relative_pose_priors = {}
         gt_wTi_list = [None, None, None]
-        delayed_poses = self.slam.create_computation_graph(
+        delayed_poses, metrics = self.slam.create_computation_graph(
             3, relative_pose_priors=relative_pose_priors, gt_wTi_list=gt_wTi_list
-        )        
+        )
+        self.assertIsInstance(delayed_poses, Delayed)
+        self.assertIsInstance(metrics, Delayed)
 
+    def test_run_pose_slam(self):
+        """Check that a simple pose graph works."""
+        sigma = np.deg2rad(5)
+        cov = np.diag([sigma, sigma, sigma, 0.1, 0.1, 0.1])
+        relative_pose_priors = {
+          (0, 1): PosePrior(Pose3(Rot3(), Point3(1, 0, 0)), cov, type=PosePriorType.SOFT_CONSTRAINT),
+          (0, 2): PosePrior(Pose3(Rot3(), Point3(2, 0, 0)), cov, type=PosePriorType.SOFT_CONSTRAINT),
+          (1, 2): PosePrior(Pose3(Rot3(), Point3(1, 0, 0)), cov, type=PosePriorType.SOFT_CONSTRAINT),
+        }
+
+        gt_wTi_list = [None, None, None]
+        poses, metrics = self.slam.run_pose_slam(
+            3, relative_pose_priors=relative_pose_priors, gt_wTi_list=gt_wTi_list
+        )
+        self.assertEqual(len(poses), 3)
+        for pose in poses:
+            self.assertIsInstance(pose, Pose3)
+        self.gtsamAssertEquals(poses[0], Pose3())
+        self.gtsamAssertEquals(poses[1], Pose3(Rot3(), Point3(1, 0, 0)))
+        self.gtsamAssertEquals(poses[2], Pose3(Rot3(), Point3(2, 0, 0)))
+        
+
+
+    def test_create_computation_graph(self):
+        """Check that the interface works."""
+        relative_pose_priors = {}
+        gt_wTi_list = [None, None, None]
+        delayed_poses, metrics = self.slam.create_computation_graph(
+            3, relative_pose_priors=relative_pose_priors, gt_wTi_list=gt_wTi_list
+        )
+        self.assertIsInstance(delayed_poses, Delayed)
+        self.assertIsInstance(metrics, Delayed)
 
 
 if __name__ == "__main__":
