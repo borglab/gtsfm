34e518e445c23c25aff9e8adff4f6a3933e0784b
kfu02 <kevin.fu842@gmail.com>
2022-07-26 00:45:02 -0400

    add unit tests to assert that simplejson works as intended

--- tests/utils/test_io_utils.py
+++ tests/utils/test_io_utils.py
@@ -15,7 +15,7 @@ from gtsfm.common.image import Image
 TEST_DATA_ROOT = Path(__file__).resolve().parent.parent / "data"
 
 
-class TestUoUtils(unittest.TestCase):
+class TestIoUtils(unittest.TestCase):
     def test_load_image(self) -> None:
         """Ensure focal length can be read from EXIF, for an image w/ known EXIF."""
         img_fpath = TEST_DATA_ROOT / "set2_lund_door_nointrinsics/images/DSC_0001.JPG"
@@ -173,6 +173,18 @@ class TestUoUtils(unittest.TestCase):
         data: GtsfmData = io_utils.read_bundler(filename)
         self.assertEqual(data.number_images(), 5)
         self.assertEqual(data.number_tracks(), 544)
+
+    def test_json_roundtrip(self) -> None:
+        """Test that basic read/write to JSON works as intended."""
+        data = {"data": [np.NaN, -2.0, 999.0, 0.0]}
+        with tempfile.TemporaryDirectory() as tempdir:
+            json_fpath = f"{tempdir}/list_with_nan.json"
+            io_utils.save_json_file(json_fpath=json_fpath, data=data)
+            data_from_json = io_utils.read_json_file(fpath=json_fpath)
+
+            # np.NaN is output as null, then read in as None
+            self.assertEqual(data_from_json["data"][0], None)
+            np.testing.assert_allclose(data["data"][1:], data_from_json["data"][1:])
 
 
 if __name__ == "__main__":
