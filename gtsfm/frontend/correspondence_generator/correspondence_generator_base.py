b9167f05903de2f57b97e6df9df41ea218094bf0
John Lambert <johnlambert@gatech.edu>
2022-08-07 04:02:47 -0400

    Add correspondence generator base class.

---
+++ gtsfm/frontend/correspondence_generator/correspondence_generator_base.py
@@ -0,0 +1,35 @@
+"""Base class for correspondence generators.
+
+Authors: John Lambert
+"""
+
+import abc
+from typing import Dict, List, Tuple
+
+from dask.delayed import Delayed
+import numpy as np
+
+from gtsfm.common.keypoints import Keypoints
+
+
+class CorrespondenceGeneratorBase:
+    """ """
+
+    @abc.abstractmethod
+    def create_computation_graph(
+        self,
+        image_graph: List[Delayed],
+        image_shapes: List[Tuple[int, int]],
+        image_pair_indices: List[Tuple[int, int]],
+    ) -> Tuple[List[Keypoints], Dict[Tuple[int, int], np.ndarray]]:
+        """Create Dask computation graph for correspondence generation.
+
+        Args:
+            image_graph: list of N images.
+            image_shapes: list of N image shapes, as tuples (height,width) in pixels.
+            image_pair_indices: list of image pairs, each represented by a tuple (i1,i2).
+
+        Return:
+            delayed_keypoints: list of keypoints, for each image.
+            delayed_putative_corr_idxs_dict dictionary of putative correspondence indices, per image pair.
+        """
