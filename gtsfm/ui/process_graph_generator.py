cc11acbd97c049a877e7f0c66fbad6803b97739a
Hayk Stepanyan <haykstepayan02@gmail.com>
2022-09-01 15:52:40 -0400

    fix lint error

--- gtsfm/ui/process_graph_generator.py
+++ gtsfm/ui/process_graph_generator.py
@@ -60,7 +60,8 @@ class ProcessGraphGenerator:
         for metadata in sorted_metadata:
             if self.is_image_correspondence and metadata.display_name in ["Matcher", "DetectorDescriptor"]:
                 continue
-            if not self.is_image_correspondence and metadata.display_name in ["Direct Image Matcher", "KeypointAggregator"]:
+            if not self.is_image_correspondence and (
+                    metadata.display_name in ["Direct Image Matcher", "KeypointAggregator"]):
                 continue
             self._add_nodes_to_graph(metadata)
 
