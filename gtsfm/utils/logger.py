7d3f9203b3ee3fccc178cef89ccbd0c78724577a
John Lambert <johnlambert@gatech.edu>
2022-09-04 01:53:16 -0400

    Reduce verbosity of Sim(3) alignment (#563)
    * Reduce verbosity of Sim(3) alignment
    
    * Set default logging level to INFO, instead of DEBUG

--- gtsfm/utils/logger.py
+++ gtsfm/utils/logger.py
@@ -11,7 +11,7 @@ def get_logger() -> Logger:
     """Getter for the main logger."""
     logger_name = "main-logger"
     logger = logging.getLogger(logger_name)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
     if not logger.handlers:
         handler = logging.StreamHandler(sys.stdout)
         fmt = "[%(asctime)s %(levelname)s %(filename)s line %(lineno)d %(process)d] %(message)s"
