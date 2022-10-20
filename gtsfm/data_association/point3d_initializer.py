450a09c1c4c52e6e3a2c0b8a1fdc4cc471044a85
Ayush Baid <ayushrakeshbaid@gmail.com>
2022-10-19 23:48:26 -0700

    Enforcing min triangulation angle in point3d initializer

--- gtsfm/data_association/point3d_initializer.py
+++ gtsfm/data_association/point3d_initializer.py
@@ -19,6 +19,7 @@ from gtsam import CameraSetCal3Bundler, CameraSetCal3Fisheye, PinholeCameraCal3B
 import gtsfm.common.types as gtsfm_types
 import gtsfm.utils.logger as logger_utils
 import gtsfm.utils.reprojection as reproj_utils
+import gtsfm.utils.tracks as track_utils
 from gtsfm.common.sfm_track import SfmTrack2d
 
 NUM_SAMPLES_PER_RANSAC_HYPOTHESIS = 2
@@ -41,6 +42,7 @@ class TriangulationExitCode(Enum):
     INLIERS_UNDERCONSTRAINED = 2  # insufficent number of inlier measurements
     POSES_UNDERCONSTRAINED = 3  # insufficent number of estimated camera poses
     EXCEEDS_REPROJ_THRESH = 4  # estimated 3d point exceeds reprojection threshold
+    LOW_TRIANGULATION_ANGLE = 5  # estimated 3d point exceeds reprojection threshold
 
 
 class TriangulationSamplingMode(str, Enum):
@@ -68,6 +70,7 @@ class TriangulationOptions(NamedTuple):
 
     Args:
         reproj_error_threshold: the maximum reprojection error allowed.
+        min_triangulation_angle: the minimum angle subtended at the triangulated point from 2 cameras.
         mode: triangulation mode, which dictates whether or not to use robust estimation.
         min_inlier_ratio: a priori assumed minimum probability that a point is an inlier.
         confidence: desired confidence that at least one hypothesis is outlier free.
@@ -77,6 +80,7 @@ class TriangulationOptions(NamedTuple):
     """
 
     reproj_error_threshold: float
+    min_triangulation_angle: float  # in degrees
     mode: TriangulationSamplingMode
 
     # RANSAC parameters
@@ -196,6 +200,15 @@ class Point3dInitializer:
                 self.track_camera_dict, triangulated_pt, track_2d.measurements
             )
 
+            dummy_track: SfmTrack = SfmTrack(triangulated_pt)
+            dummy_track.addMeasurement(i1, uv1)
+            dummy_track.addMeasurement(i2, uv2)
+            if (
+                track_utils.get_triangulation_angle(dummy_track, self.track_camera_dict)
+                < self.options.min_triangulation_angle
+            ):
+                continue
+
             # The best solution should correspond to the one with most inliers
             # If the inlier number are the same, check the average error of inliers
             is_inlier = errors < self.options.reproj_error_threshold
@@ -276,6 +289,13 @@ class Point3dInitializer:
         track_3d = SfmTrack(triangulated_pt)
         for i, uv in inlier_track.measurements:
             track_3d.addMeasurement(i, uv)
+
+        # Check that there is sufficient angle
+        if (
+            track_utils.get_triangulation_angle(track_3d, cameras=self.track_camera_dict)
+            < self.options.min_triangulation_angle
+        ):
+            return None, avg_track_reproj_error, TriangulationExitCode.LOW_TRIANGULATION_ANGLE
 
         return track_3d, avg_track_reproj_error, TriangulationExitCode.SUCCESS
 
--- gtsfm/utils/tracks.py
+++ gtsfm/utils/tracks.py
@@ -2,9 +2,11 @@
 
 Authors: Ayush Baid
 """
+import itertools
 from typing import Dict, List
 
-from gtsam import PinholeCameraCal3Bundler, SfmTrack
+import numpy as np
+from gtsam import SfmTrack
 
 from gtsfm.common.sfm_track import SfmMeasurement, SfmTrack2d
 from gtsfm.data_association.point3d_initializer import (
@@ -13,10 +15,11 @@ from gtsfm.data_association.point3d_initializer import (
     TriangulationOptions,
     TriangulationSamplingMode,
 )
+from gtsfm.common.types import CAMERA_TYPE
 
 
 def classify_tracks2d_with_gt_cameras(
-    tracks: List[SfmTrack2d], cameras_gt: List[PinholeCameraCal3Bundler], reproj_error_thresh_px: float = 3
+    tracks: List[SfmTrack2d], cameras_gt: List[CAMERA_TYPE], reproj_error_thresh_px: float = 3
 ) -> List[TriangulationExitCode]:
     """Classifies the 2D tracks w.r.t ground truth cameras by performing triangulation and collecting exit codes.
 
@@ -30,11 +33,13 @@ def classify_tracks2d_with_gt_cameras(
         The triangulation exit code for each input track, as list of the same length as of tracks.
     """
     # do a simple triangulation with the GT cameras
-    cameras_dict: Dict[int, PinholeCameraCal3Bundler] = {i: cam for i, cam in enumerate(cameras_gt)}
+    cameras_dict: Dict[int, CAMERA_TYPE] = {i: cam for i, cam in enumerate(cameras_gt)}
     point3d_initializer = Point3dInitializer(
         track_camera_dict=cameras_dict,
         options=TriangulationOptions(
-            reproj_error_threshold=reproj_error_thresh_px, mode=TriangulationSamplingMode.NO_RANSAC
+            reproj_error_threshold=reproj_error_thresh_px,
+            min_triangulation_angle=-1,
+            mode=TriangulationSamplingMode.NO_RANSAC,
         ),
     )
 
@@ -47,7 +52,7 @@ def classify_tracks2d_with_gt_cameras(
 
 
 def classify_tracks3d_with_gt_cameras(
-    tracks: List[SfmTrack], cameras_gt: List[PinholeCameraCal3Bundler], reproj_error_thresh_px: float = 3
+    tracks: List[SfmTrack], cameras_gt: List[CAMERA_TYPE], reproj_error_thresh_px: float = 3
 ) -> List[TriangulationExitCode]:
     """Classifies the 3D tracks w.r.t ground truth cameras by performing triangulation and collecting exit codes.
 
@@ -74,3 +79,39 @@ def classify_tracks3d_with_gt_cameras(
         tracks_2d.append(SfmTrack2d(measurements))
 
     return classify_tracks2d_with_gt_cameras(tracks_2d, cameras_gt, reproj_error_thresh_px)
+
+
+def get_triangulation_angle(track3d: SfmTrack, cameras: Dict[int, CAMERA_TYPE]) -> float:
+    """Get the angle subtended by the cameras at the 3D landmark of the track.
+
+    Args:
+        track3d: the track with the landmark.
+        cameras: cameras which have been used to triangulate the landmark.
+
+    Returns:
+        float: the minimum triangulation angle (minimum over all pairs of cameras associated with the track).
+    """
+    landmark: np.ndarray[np.float64[3, 1]] = track3d.point3()
+
+    camera_idxs: List[int] = []
+    for k in range(track3d.numberMeasurements()):
+        i, _ = track3d.measurement(k)
+        camera_idxs.append(i)
+
+    angles: List[float] = []
+    for i1, i2 in itertools.combinations(camera_idxs, 2):
+        camera_i1_center: np.ndarray[np.float64[3, 1]] = cameras[i1].pose().translation()
+        camera_i2_center: np.ndarray[np.float64[3, 1]] = cameras[i2].pose().translation()
+
+        camera_i1_ray: np.ndarray[np.float64[3, 1]] = landmark - camera_i1_center
+        camera_i2_ray: np.ndarray[np.float64[3, 1]] = landmark - camera_i2_center
+
+        subtended_angle: float = np.rad2deg(
+            np.arccos(
+                np.dot(camera_i1_ray, camera_i2_ray) / (np.linalg.norm(camera_i1_ray) * np.linalg.norm(camera_i2_ray))
+            )
+        )
+
+        angles.append(subtended_angle)
+
+    return min(angles)
