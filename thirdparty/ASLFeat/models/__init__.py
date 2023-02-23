668ae004fe49b0ef12e1f109a786a9a475fa9b26
Travis Driver <travisdriver@gatech.edu>
2023-02-23 18:43:29 -0500

    Remove ASLFeat

--- thirdparty/ASLFeat/models/__init__.py
+++
@@ -1,8 +0,0 @@
-def get_model(name):
-    mod = __import__('models.{}'.format(name), fromlist=[''])
-    return getattr(mod, _module_to_class(name))
-
-
-def _module_to_class(name):
-    return ''.join(n.capitalize() for n in name.split('_'))
-
