cad05175e40d24843feecc4527fc82595787ad96
Travis Driver <travisdriver@gatech.edu>
2023-03-02 15:57:40 -0500

    Testing CI env

--- gtsfm/frontend/detector_descriptor/aslfeat.py
+++ gtsfm/frontend/detector_descriptor/aslfeat.py
@@ -9,6 +9,9 @@ Authors: Travis Driver
 from pathlib import Path
 from typing import Tuple, Dict, Any
 import sys
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 import numpy as np
 import tensorflow.compat.v1 as tf
