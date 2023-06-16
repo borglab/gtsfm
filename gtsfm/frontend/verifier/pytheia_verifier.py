5cac8b5ddeac419c9585eded5d8e677e88b39f97
Travis Driver <drivertp@gmail.com>
2023-06-15 23:40:31 -0700

    Increase max iterations

--- gtsfm/frontend/verifier/pytheia_verifier.py
+++ gtsfm/frontend/verifier/pytheia_verifier.py
@@ -110,7 +110,7 @@ class PyTheiaVerifier(VerifierBase):
 
         options.estimate_twoview_info_options.max_sampson_error_pixels = self._estimation_threshold_px
         options.estimate_twoview_info_options.expected_ransac_confidence = RANSAC_SUCCESS_PROB
-        options.estimate_twoview_info_options.max_ransac_iterations = 1000
+        options.estimate_twoview_info_options.max_ransac_iterations = 10000
         options.estimate_twoview_info_options.use_lo = True
         options.estimate_twoview_info_options.ransac_type = pt.sfm.RansacType(0)
 
