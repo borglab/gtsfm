79cfd0ce95a2623de777d539d7dcaff1d49a65f3
Travis Driver <drivertp@gmail.com>
2023-06-16 16:26:23 -0700

    Download datasets once

---
+++ .github/scripts/execute_single_benchmark_wildcat.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Datasets are stored in ~/sfm_datasets in order to reduce runtimes by not 
+# redownloading each (very large) dataset every CI run. Any new datasets must be
+# downloaded and stored in ~/sfm_datasets before running this action.
+DATASET_PREFIX=/home/tdriver6/sfm_datasets
+
+DATASET_NAME=$1
+CONFIG_NAME=$2
+MAX_FRAME_LOOKAHEAD=$3
+IMAGE_EXTENSION=$4
+LOADER_NAME=$5
+MAX_RESOLUTION=$6
+SHARE_INTRINSICS=$7
+
+# Extract the data, configure arguments for runner.
+if [ "$DATASET_NAME" == "skydio-501" ]; then
+  IMAGES_DIR="skydio-crane-mast-501-images"
+  COLMAP_FILES_DIRPATH="skydio-501-colmap-pseudo-gt"
+fi
+
+echo "Config: ${CONFIG_NAME}, Loader: ${LOADER_NAME}"
+echo "Max. Frame Lookahead: ${MAX_FRAME_LOOKAHEAD}, Image Extension: ${IMAGE_EXTENSION}, Max. Resolution: ${MAX_RESOLUTION}"
+echo "Share intrinsics for all images? ${SHARE_INTRINSICS}"
+
+# Setup the command line arg if intrinsics are to be shared
+if [ "$SHARE_INTRINSICS" == "true" ]; then
+  export SHARE_INTRINSICS_ARG="--share_intrinsics"
+else
+  export SHARE_INTRINSICS_ARG=""
+fi
+
+echo "Share intrinsics CLI argument: ${SHARE_INTRINSICS_ARG}"
+
+# Run GTSFM on the dataset.
+if [ "$LOADER_NAME" == "olsson-loader" ]; then
+  python gtsfm/runner/run_scene_optimizer_olssonloader.py \
+    --dataset_root $DATASET_PREFIX/$DATASET_ROOT \
+    --image_extension $IMAGE_EXTENSION \
+    --config_name ${CONFIG_NAME}.yaml \
+    --max_frame_lookahead $MAX_FRAME_LOOKAHEAD \
+    --max_resolution ${MAX_RESOLUTION} \
+    ${SHARE_INTRINSICS_ARG}
+
+elif [ "$LOADER_NAME" == "colmap-loader" ]; then
+  python gtsfm/runner/run_scene_optimizer_colmaploader.py \
+    --images_dir $DATASET_PREFIX/${IMAGES_DIR} \
+    --colmap_files_dirpath $DATASET_PREFIX/$COLMAP_FILES_DIRPATH \
+    --config_name ${CONFIG_NAME}.yaml \
+    --max_frame_lookahead $MAX_FRAME_LOOKAHEAD \
+    --max_resolution ${MAX_RESOLUTION} \
+    ${SHARE_INTRINSICS_ARG} \
+    --
+
+elif [ "$LOADER_NAME" == "astrovision" ]; then
+  python gtsfm/runner/run_scene_optimizer_astrovision.py \
+    --data_dir $DATASET_PREFIX/$DATASET_ROOT \
+    --config_name ${CONFIG_NAME}.yaml \
+    --max_frame_lookahead $MAX_FRAME_LOOKAHEAD \
+    --max_resolution ${MAX_RESOLUTION} \
+    ${SHARE_INTRINSICS_ARG}
+fi
