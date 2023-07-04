324fc9869708d498f822c42da7e789eb75c556c2
John Lambert <johnlambert@gatech.edu>
2023-07-04 16:20:08 -0400

    Update test_tanks_and_temples_loader.py

--- tests/loader/test_tanks_and_temples_loader.py
+++ tests/loader/test_tanks_and_temples_loader.py
@@ -9,37 +9,37 @@ from pathlib import Path
 _TEST_DATA_ROOT = Path(__file__).resolve().parent.parent / "data" / "tanks_and_temples_barn"
 
 
-class TanksAndTemplesLoaderTest(unittest.TestCase)
+class TanksAndTemplesLoaderTest(unittest.TestCase):
 
-	def setUp(self) -> None:
+    def setUp(self) -> None:
 
-		img_dir = _TEST_DATA_ROOT / 'Barn'
-		log_fpath = _TEST_DATA_ROOT / 'Barn_COLMAP_SfM.log'
-		ply_fpath = _TEST_DATA_ROOT / 'Barn.ply'
-		#ply_fpath = scene_data_dir / 'Barn_COLMAP.ply'
-		ply_alignment_fpath = _TEST_DATA_ROOT / 'Barn_trans.txt'
-		bounding_polyhedron_json_fpath = _TEST_DATA_ROOT / 'Barn.json'
+        img_dir = _TEST_DATA_ROOT / 'Barn'
+        log_fpath = _TEST_DATA_ROOT / 'Barn_COLMAP_SfM.log'
+        ply_fpath = _TEST_DATA_ROOT / 'Barn.ply'
+        #ply_fpath = scene_data_dir / 'Barn_COLMAP.ply'
+        ply_alignment_fpath = _TEST_DATA_ROOT / 'Barn_trans.txt'
+        bounding_polyhedron_json_fpath = _TEST_DATA_ROOT / 'Barn.json'
 
-		loader = TanksAndTemplesLoader(
-		    img_dir=img_dir,
-		    poses_fpath=log_fpath,
-		    ply_fpath=ply_fpath,
-		    ply_alignment_fpath=ply_alignment_fpath,
-		    bounding_polyhedron_json_fpath=bounding_polyhedron_json_fpath,
-		)
+        loader = TanksAndTemplesLoader(
+        img_dir=img_dir,
+        poses_fpath=log_fpath,
+        ply_fpath=ply_fpath,
+        ply_alignment_fpath=ply_alignment_fpath,
+        bounding_polyhedron_json_fpath=bounding_polyhedron_json_fpath,
+        )
 
-	def test_get_camera_intrinsics_full_res(self) -> None:
+    def test_get_camera_intrinsics_full_res(self) -> None:
 
-		intrinsics = loader.get_camera_intrinsics_full_res(index=0)
+        intrinsics = loader.get_camera_intrinsics_full_res(index=0)
 
-	def test_get_camera_pose(self) -> None:
+    def test_get_camera_pose(self) -> None:
 
-		wTi = loader.get_camera_pose(index=0)
-		"""
-		Unit Test
-		(Pdb) p wTi_gt[0]
-		array([[-0.43321999, -0.05555365, -0.89957447,  3.24710662],
-		[ 0.05678138,  0.99443357, -0.08875668,  0.14032715],
-		[ 0.89949781, -0.08953024, -0.42765409,  0.55723886],
-		[ 0.        ,  0.        ,  0.        ,  1.        ]])
-		"""
+        wTi = loader.get_camera_pose(index=0)
+        """
+        Unit Test
+        (Pdb) p wTi_gt[0]
+        array([[-0.43321999, -0.05555365, -0.89957447,  3.24710662],
+        [ 0.05678138,  0.99443357, -0.08875668,  0.14032715],
+        [ 0.89949781, -0.08953024, -0.42765409,  0.55723886],
+        [ 0.        ,  0.        ,  0.        ,  1.        ]])
+        """
