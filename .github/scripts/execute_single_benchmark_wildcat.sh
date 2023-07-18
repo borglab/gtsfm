b3d0d9e1938b431236cfa2e4f051afe6a62a875e
senselessdev1 <johnlambert@Hayoungs-MacBook-Pro.local>
2023-07-17 23:57:03 -0700

    run tanks & temples on wildcat

--- .github/scripts/execute_single_benchmark_wildcat.sh
+++ .github/scripts/execute_single_benchmark_wildcat.sh
@@ -17,7 +17,22 @@ SHARE_INTRINSICS=$7
 if [ "$DATASET_NAME" == "skydio-501" ]; then
   IMAGES_DIR="skydio-crane-mast-501-images"
   COLMAP_FILES_DIRPATH="skydio-501-colmap-pseudo-gt"
+elif [ "$DATASET_NAME" == "tanks-and-temples-barn-410" ]; then
+  DATASET_ROOT="Tanks_and_Temples_Barn_410"
+  SCENE_NAME="Barn"
 fi
+
+## Download the Tanks & Temples dataset. ################
+WGET_URL1=https://github.com/johnwlambert/gtsfm-datasets-mirror/releases/download/tanks-and-temples-barn/Tanks_and_Temples_Barn_410.zip
+ZIP_FNAME=Tanks_and_Temples_Barn_410.zip
+echo "Downloading ${ZIP_FNAME} with WGET"
+wget -O $DATASET_PREFIX/Tanks_and_Temples_Barn_410.zip $WGET_URL1
+unzip -qq $DATASET_PREFIX/$ZIP_FNAME -d $DATASET_PREFIX
+pwd $DATASET_PREFIX
+ls -ltrh $DATASET_PREFIX
+ls -ltrh $DATASET_PREFIX/$DATASET_ROOT
+#########################################
+
 
 echo "Config: ${CONFIG_NAME}, Loader: ${LOADER_NAME}"
 echo "Max. Frame Lookahead: ${MAX_FRAME_LOOKAHEAD}, Image Extension: ${IMAGE_EXTENSION}, Max. Resolution: ${MAX_RESOLUTION}"
@@ -60,4 +75,10 @@ elif [ "$LOADER_NAME" == "astrovision" ]; then
     --max_frame_lookahead $MAX_FRAME_LOOKAHEAD \
     --max_resolution ${MAX_RESOLUTION} \
     ${SHARE_INTRINSICS_ARG}
+
+elif [ "$LOADER_NAME" == "tanks-and-temples" ]; then
+  python gtsfm/runner/run_scene_optimizer_synthetic_tanks_and_temples.py \
+    --config_name ${CONFIG_NAME}.yaml \
+    --dataset_root $DATASET_ROOT \
+    --scene_name $SCENE_NAME
 fi
