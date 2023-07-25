2aba78c9701bd2540a59cf77f905f8458e243a98
Travis Driver <drivertp@gmail.com>
2023-07-25 12:57:18 -0700

    Debugging

--- .github/scripts/execute_single_benchmark_self_hosted.sh
+++ .github/scripts/execute_single_benchmark_self_hosted.sh
@@ -5,7 +5,7 @@ which python
 # Datasets are stored in /usr/local/gtsfm-data in order to reduce runtimes by not 
 # redownloading each (very large) dataset every CI run. Any new datasets must be
 # downloaded and stored in /usr/local/gtsfm-data before running this action.
-DATASET_PREFIX=/usr/local/gtsfm-data
+DATASET_PREFIX=/home/tdriver6/sfm_datasets
 
 DATASET_NAME=$1
 CONFIG_NAME=$2
