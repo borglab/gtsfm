9d99a95b7c24f42f484ce2483623d048f0034a9b
senselessdev1 <johnlambert@Hayoungs-MacBook-Pro.local>
2023-08-26 16:18:59 -0400

    improve comments

--- tests/data_association/test_dsf_tracks_estimator.py
+++ tests/data_association/test_dsf_tracks_estimator.py
@@ -61,8 +61,10 @@ class TestDsfTracksEstimator(GtsamTestCase):
         self.assertEqual(len(tracks), 0, "Tracks not filtered correctly")
 
     def test_track_generation(self) -> None:
-        """Ensures that DSF generates three tracks from measurements
-        in 3 images (H=200,W=400)."""
+        """Ensures that DSF generates three tracks from measurements in 3 images (H=200,W=400).
+
+        Analogous to test found at github.com/borglab/gtsam/blob/develop/python/gtsam/tests/test_DsfTrackGenerator.py
+        """
         kps_i0 = Keypoints(np.array([[10.0, 20], [30, 40]]))
         kps_i1 = Keypoints(np.array([[50.0, 60], [70, 80], [90, 100]]))
         kps_i2 = Keypoints(np.array([[110.0, 120], [130, 140]]))
@@ -72,8 +74,7 @@ class TestDsfTracksEstimator(GtsamTestCase):
         keypoints_list.append(kps_i1)
         keypoints_list.append(kps_i2)
 
-        # For each image pair (i1,i2), we provide a (K,2) matrix
-        # of corresponding image indices (k1,k2).
+        # For each image pair (i1,i2), we provide a (K,2) matrix of corresponding keypoint indices (k1,k2).
         matches_dict = {}
         matches_dict[(0, 1)] = np.array([[0, 0], [1, 1]])
         matches_dict[(1, 2)] = np.array([[2, 0], [1, 1]])
