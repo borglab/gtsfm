a050854a16f0573a1c10d6980e8d82ff1331ae8a
senselessdev1 <johnlambert@Hayoungs-MacBook-Pro.local>
2023-08-26 16:20:27 -0400

    add code to prevent heartbeat timeout

--- gtsfm/scene_optimizer.py
+++ gtsfm/scene_optimizer.py
@@ -11,6 +11,7 @@ import dask
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+from dask import config as dask_config
 from dask.delayed import Delayed
 from gtsam import Pose3, Similarity3, Rot3, Unit3
 from trimesh import Trimesh
@@ -56,6 +57,8 @@ pil_logger.setLevel(logging.INFO)
 
 # number of digits (significant figures) to include in each entry of error metrics
 PRINT_NUM_SIG_FIGS = 2
+
+dask_config.set({'distributed.scheduler.worker-ttl': None})
 
 
 class SceneOptimizer:
