8093fb1357b42f54ef3a3ae465249b728495e477
stepanyanhayk <haykstepanyan02@gmail.com>
2023-08-27 19:41:00 +0000

    socket handler

--- gtsfm/utils/logger.py
+++ gtsfm/utils/logger.py
@@ -3,6 +3,7 @@
 Authors: Ayush Baid, John Lambert.
 """
 import logging
+import logging.handlers
 import sys
 from logging import Logger
 
@@ -13,8 +14,8 @@ def get_logger() -> Logger:
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging.INFO)
     if not logger.handlers:
-        handler = logging.StreamHandler(sys.stdout)
-        fmt = "[%(asctime)s %(levelname)s %(filename)s line %(lineno)d %(process)d] %(message)s"
-        handler.setFormatter(logging.Formatter(fmt))
+        handler = logging.handlers.SocketHandler('wildcat.cc.gatech.edu', 5000)
+        # fmt = "[%(asctime)s %(levelname)s %(filename)s line %(lineno)d %(process)d] %(message)s"
+        # handler.setFormatter(logging.Formatter(fmt))
         logger.addHandler(handler)
     return logger
