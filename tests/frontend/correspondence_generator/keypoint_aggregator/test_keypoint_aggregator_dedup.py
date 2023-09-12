aab676f7f5127f4a12cc0de1148e18087b39fed7
senselessdev1 <johnlambert@jl-macbook-pro-19.local>
2023-09-12 00:34:27 -0400

    fix test

--- tests/frontend/correspondence_generator/keypoint_aggregator/test_keypoint_aggregator_dedup.py
+++ tests/frontend/correspondence_generator/keypoint_aggregator/test_keypoint_aggregator_dedup.py
@@ -2,16 +2,23 @@
 
 Authors: John Lambert
 """
+import pathlib
 import unittest
 from typing import Dict, Tuple
 
 import numpy as np
 
 from gtsfm.common.keypoints import Keypoints
+from gtsfm.frontend.cacher.image_matcher_cacher import ImageMatcherCacher
+from gtsfm.frontend.matcher.loftr import LOFTR
+from gtsfm.loader.olsson_loader import OlssonLoader
 from gtsfm.frontend.correspondence_generator.keypoint_aggregator.keypoint_aggregator_dedup import (
     KeypointAggregatorDedup,
 )
 from tests.frontend.correspondence_generator.keypoint_aggregator import test_keypoint_aggregator_base
+
+DATA_ROOT_PATH = pathlib.Path(__file__).resolve().parent.parent.parent.parent / "data"
+DEFAULT_FOLDER = DATA_ROOT_PATH / "set1_lund_door"
 
 
 class TestKeypointAggregatorDedup(test_keypoint_aggregator_base.TestKeypointAggregatorBase):
@@ -22,7 +29,7 @@ class TestKeypointAggregatorDedup(test_keypoint_aggregator_base.TestKeypointAggr
 
     def setUp(self):
         super().setUp()
-        self.aggregator = KeypointAggregatorDedup()
+        self.aggregator = KeypointAggregatorDedup(nms_radius=0.0)
 
     def test_keypoint_aggregator_repeated_keypoints(self) -> None:
         """Ensure aggregation works over 3 images, with duplicate keypoints in the same image from separate pairs.
@@ -59,7 +66,7 @@ class TestKeypointAggregatorDedup(test_keypoint_aggregator_base.TestKeypointAggr
         assert len(keypoints_list) == 3
         assert all([isinstance(kps, Keypoints) for kps in keypoints_list])
 
-        # removing duplicates
+        # Duplicates should have been removed.
         expected_putative_corr_idxs_dict = {
             (0, 1): np.array([[0, 0]]),
             (1, 2): np.array([[1, 0]]),
@@ -83,6 +90,25 @@ class TestKeypointAggregatorDedup(test_keypoint_aggregator_base.TestKeypointAggr
 
         expected_image2_kps = np.array([[2.0, 2.0], [3.0, 3.0], [4.0, 4.0]])
         assert np.allclose(keypoints_list[2].coordinates, expected_image2_kps)
+
+    def test_dedup_nms_radius_3(self) -> None:
+        loader = OlssonLoader(str(DEFAULT_FOLDER), max_frame_lookahead=4)
+        image_matcher = ImageMatcherCacher(matcher_obj=LOFTR())
+
+        images = [loader.get_image(i) for i in range(4)]
+        image_pairs = [(0, 1), (1, 2), (2, 3), (0, 2), (1, 3)]
+        aggregator = KeypointAggregatorDedup(nms_radius=3.0)
+
+        pairwise_correspondences = {
+            (i1, i2): image_matcher.match(image_i1=images[i1], image_i2=images[i2]) for i1, i2 in image_pairs
+        }
+
+        keypoints_list, putative_corr_idxs_dict = aggregator.aggregate(keypoints_dict=pairwise_correspondences)
+
+        for (i1, i2), corr_idxs in putative_corr_idxs_dict.items():
+            assert corr_idxs.dtype == np.int32
+            assert len(corr_idxs.shape) == 2
+            assert corr_idxs.shape[-1] == 2
 
 
 if __name__ == "__main__":
