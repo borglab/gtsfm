93853376ef2ac9d108d5d3915bfa63b8833a8674
senselessdev1 <johnlambert@jl-macbook-pro-19.local>
2023-10-03 23:04:21 -0400

    add more documentation

--- gtsfm/two_view_estimator_cacher.py
+++ gtsfm/two_view_estimator_cacher.py
@@ -16,8 +16,12 @@ from gtsfm.common.keypoints import Keypoints
 from gtsfm.common.pose_prior import PosePrior
 from gtsfm.two_view_estimator import TwoViewEstimator, TWO_VIEW_OUTPUT
 
+# Number of first K keypoints from each image to use to create cache key.
 NUM_KEYPOINTS_TO_SAMPLE_FOR_HASH = 10
+
+# Number of first K correspondence indices from image pair to use to create cache key.
 NUM_CORRESPONDENCES_TO_SAMPLE_FOR_HASH = 10
+
 CACHE_ROOT_PATH = Path(__file__).resolve().parent.parent / "cache"
 
 logger = logger_utils.get_logger()
@@ -30,7 +34,7 @@ pil_logger.setLevel(logging.INFO)
 
 
 class TwoViewEstimatorCacher(TwoViewEstimator):
-    """Wrapper for running two-view relative pose estimation on image pairs in the dataset."""
+    """Caches two-view relative pose estimation results for an image pair."""
 
     def __init__(self, two_view_estimator_obj: TwoViewEstimator) -> None:
         self._two_view_estimator = two_view_estimator_obj
@@ -42,6 +46,8 @@ class TwoViewEstimatorCacher(TwoViewEstimator):
     def __generate_cache_key(
         self, keypoints_i1: Keypoints, keypoints_i2: Keypoints, putative_corr_idxs: np.ndarray
     ) -> str:
+        """Generates a cache key according to keypoint coordinates and putative correspondence indices."""
+
         # Subsample correspondence indices.
         sampled_idxs = putative_corr_idxs[:NUM_CORRESPONDENCES_TO_SAMPLE_FOR_HASH]
 
@@ -56,20 +62,19 @@ class TwoViewEstimatorCacher(TwoViewEstimator):
     def __load_result_from_cache(
         self, keypoints_i1: Keypoints, keypoints_i2: Keypoints, putative_corr_idxs: np.ndarray
     ) -> Optional[TWO_VIEW_OUTPUT]:
-        """Load cached result, if they exist."""
-        cache_path = self.__get_cache_path(
-            cache_key=self.__generate_cache_key(keypoints_i1, keypoints_i2, putative_corr_idxs)
-        )
+        """Loads cached result, if it exists."""
+        cache_key = self.__generate_cache_key(keypoints_i1, keypoints_i2, putative_corr_idxs)
+        cache_path = self.__get_cache_path(cache_key=cache_key)
+        # If bz2 file does not exist, `None` will be returned.
         cached_data = io_utils.read_from_bz2_file(cache_path)
         return cached_data
 
     def __save_result_to_cache(
         self, keypoints_i1: Keypoints, keypoints_i2: Keypoints, putative_corr_idxs: np.ndarray, result: TWO_VIEW_OUTPUT
     ) -> None:
-        """Save the results (match indices) to the cache."""
-        cache_path = self.__get_cache_path(
-            cache_key=self.__generate_cache_key(keypoints_i1, keypoints_i2, putative_corr_idxs)
-        )
+        """Saves the result (`TWO_VIEW_OUTPUT` 6-tuple) to the cache."""
+        cache_key = self.__generate_cache_key(keypoints_i1, keypoints_i2, putative_corr_idxs)
+        cache_path = self.__get_cache_path(cache_key=cache_key)
         io_utils.write_to_bz2_file(result, cache_path)
 
     def run_2view(
@@ -84,6 +89,7 @@ class TwoViewEstimatorCacher(TwoViewEstimator):
         gt_camera_i2: Optional[gtsfm_types.CAMERA_TYPE],
         gt_scene_mesh: Optional[Any] = None,
     ) -> TWO_VIEW_OUTPUT:
+        """Loads 2-view estimation result if it exists in cache, otherwise re-runs two view estimator from scratch."""
         result = self.__load_result_from_cache(keypoints_i1, keypoints_i2, putative_corr_idxs)
 
         if result is not None:
@@ -102,5 +108,4 @@ class TwoViewEstimatorCacher(TwoViewEstimator):
         )
 
         self.__save_result_to_cache(keypoints_i1, keypoints_i2, putative_corr_idxs, result)
-
         return result
