7f1bd9de317bc9a3bab6f27aa361b31d7f0937b7
senselessdev1 <johnlambert@jl-macbook-pro-19.local>
2023-10-03 22:50:36 -0400

    add unit test

---
+++ tests/test_two_view_estimator_cacher.py
@@ -0,0 +1,14 @@
+"""Unit tests for two-view estimator cacher.
+
+"""
+
+import unittest
+
+from gtsfm.two_view_estimator_cacher import TwoViewEstimatorCacher
+
+
+class TestTwoViewEstimatorCacher(unittest.TestCase):
+
+	def test_blah(self) -> None:
+		""" """
+		cacher = TwoViewEstimatorCacher(two_view_estimator_obj=None)
