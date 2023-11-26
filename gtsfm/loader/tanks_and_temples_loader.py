3ca455904c88e31f93453aa60a9fb1d1f826dec2
senselessdev1 <johnlambert@jl-macbook-pro-19.local>
2023-11-26 02:21:43 -0500

    move algorithm outside of loader

--- gtsfm/loader/tanks_and_temples_loader.py
+++ gtsfm/loader/tanks_and_temples_loader.py
@@ -12,11 +12,9 @@ from typing import Dict, List, Optional, Tuple
 
 import numpy as np
 import open3d
-import trimesh
 from gtsam import Cal3Bundler, Rot3, Pose3
 from dask.distributed import Future
 
-import gtsfm.common.types as gtsfm_types
 import gtsfm.utils.geometry_comparisons as geom_comp_utils
 import gtsfm.utils.io as io_utils
 import gtsfm.utils.logger as logger_utils
@@ -24,13 +22,6 @@ import gtsfm.visualization.open3d_vis_utils as open3d_vis_utils
 from gtsfm.common.image import Image
 from gtsfm.common.keypoints import Keypoints
 from gtsfm.loader.loader_base import LoaderBase
-from gtsfm.frontend.correspondence_generator.keypoint_aggregator.keypoint_aggregator_base import KeypointAggregatorBase
-from gtsfm.frontend.correspondence_generator.keypoint_aggregator.keypoint_aggregator_dedup import (
-    KeypointAggregatorDedup,
-)
-from gtsfm.frontend.correspondence_generator.keypoint_aggregator.keypoint_aggregator_unique import (
-    KeypointAggregatorUnique,
-)
 
 
 logger = logger_utils.get_logger()
@@ -57,7 +48,7 @@ class MeshReconstructionType(Enum):
 
     ALPHA_SHAPE = auto()
     BALL_PIVOTING = auto()
-    POISSION_SURFACE = auto()
+    POISSON_SURFACE = auto()
 
 
 class TanksAndTemplesLoader(LoaderBase):
@@ -224,7 +215,7 @@ class TanksAndTemplesLoader(LoaderBase):
             Point cloud reconstructed by COLMAP, in the COLMAP world frame.
         """
         if not Path(self.colmap_ply_fpath).exists():
-            raise ValueError("")
+            raise ValueError("Cannot retrieve COLMAP-reconstructed point cloud if `colmap_ply_fpath` not provided.")
         pcd = open3d.io.read_point_cloud(self.colmap_ply_fpath)
         points, rgb = open3d_vis_utils.convert_colored_open3d_point_cloud_to_numpy(pointcloud=pcd)
 
@@ -275,203 +266,9 @@ class TanksAndTemplesLoader(LoaderBase):
                 pcd, open3d.utility.DoubleVector(radii)
             )
 
-        elif reconstruction_algorithm == MeshReconstructionType.POISSION_SURFACE:
+        elif reconstruction_algorithm == MeshReconstructionType.POISSON_SURFACE:
             mesh, densities = open3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=9)
         return mesh
-
-    def generate_synthetic_correspondences(
-        self,
-        images: List[Future],
-        image_pairs: List[Tuple[int, int]],
-        deduplicate: bool = False,
-        num_sampled_3d_points: int = 2000,
-    ) -> Tuple[List[Keypoints], Dict[Tuple[int, int], np.ndarray]]:
-        """Generates synthetic correspondences from virtual cameras and a ground-truth mesh.
-
-        Args:
-            images: List of input images.
-            image_pairs: Tuples (i1,i2) indicating image indices to use as image pairs.
-            deduplicate: Whether to de-duplicate with a single image the detections received from each image pair.
-            num_sampled_3d_points: Number of 3d points to sample from the mesh surface and to project.
-
-        Returns:
-            List of keypoints, one entry for each input image.
-            Putative correspondences as indices of keypoints, for pairs of images. Mapping from image pair
-                (i1,i2) to delayed task to compute putative correspondence indices. Correspondence indices
-                are represented by an array of shape (K,2), for K correspondences.
-        """
-        mesh = self.reconstruct_mesh()
-
-        # Sample random 3d points. This sampling must occur only once, to avoid clusters from repeated sampling.
-        pcd = mesh.sample_points_uniformly(number_of_points=num_sampled_3d_points)
-        pcd = mesh.sample_points_poisson_disk(number_of_points=num_sampled_3d_points, pcl=pcd)
-        points = np.asarray(pcd.points)
-
-        open3d_mesh_path = tempfile.NamedTemporaryFile(suffix=".obj").name
-        open3d.io.write_triangle_mesh(filename=open3d_mesh_path, mesh=mesh)
-
-        camera_dict = {}
-
-        aggregator: KeypointAggregatorBase = KeypointAggregatorDedup() if deduplicate else KeypointAggregatorUnique()
-        keypoints_dict = {}
-        putative_corr_idxs_dict = {}
-
-        for i1, i2 in image_pairs:
-            if i1 not in camera_dict:
-                camera_dict[i1] = self.get_camera(index=i1)
-            if i2 not in camera_dict:
-                camera_dict[i2] = self.get_camera(index=i2)
-            keypoints_i1, keypoints_i2 = self.generate_synthetic_correspondences_for_image_pair(
-                camera_i1=camera_dict[i1], camera_i2=camera_dict[i2], open3d_mesh_fpath=open3d_mesh_path, points=points
-            )
-            num_kpts = len(keypoints_i1)
-            putative_corr_idxs = np.stack([np.arange(num_kpts), np.arange(num_kpts)], axis=-1)
-            keypoints_dict[(i1, i2)] = (keypoints_i1, keypoints_i2)
-            putative_corr_idxs_dict[(i1, i2)] = putative_corr_idxs
-            print(f"Number of keypoints in image {i1}: ", len(keypoints_i1))
-            # import matplotlib.pyplot as plt
-            # img = self.get_image_full_res(index=0)
-            # plt.imshow(img.value_array.astype(np.uint8))
-            # plt.scatter(keypoints_i1.coordinates[:,0], keypoints_i1.coordinates[:,1], 10, color='r', marker='.')
-            # plt.show()
-
-        keypoints_list, putative_corr_idxs_dict = aggregator.aggregate(keypoints_dict=keypoints_dict)
-        return keypoints_list, putative_corr_idxs_dict
-
-    def generate_synthetic_correspondences_for_image_pair(
-        self,
-        camera_i1: gtsfm_types.CAMERA_TYPE,
-        camera_i2: gtsfm_types.CAMERA_TYPE,
-        open3d_mesh_fpath: str,
-        points: np.ndarray,
-    ) -> Tuple[Keypoints, Keypoints]:
-        """Generates synthetic correspondences for image pair.
-
-        Args:
-            camera_i1: First camera.
-            camera_i2: Second camera.
-            open3d_mesh_fpath: Path to saved Open3d mesh.
-            points: 3d points sampled from mesh surface.
-
-        Returns:
-            Tuple of `Keypoints` objects, one for each image in the input image pair.
-        """
-        mesh = open3d.io.read_triangle_mesh(filename=open3d_mesh_fpath)
-        trimesh_mesh = load_from_trimesh(open3d_mesh_fpath)
-
-        wTi_list = [camera_i1.pose(), camera_i2.pose()]
-        calibrations = [camera_i1.calibration(), camera_i2.calibration()]
-
-        keypoints_i1 = []
-        keypoints_i2 = []
-
-        # TODO(johnwlambert): Vectorize this code. On CPU, rays cannot be simultaneously cast against mesh
-        # due to RAM limitations.
-        for point in points:
-            # Try projecting point into each camera. If inside FOV of both and unoccluded by mesh, keep.
-            uv_i1 = verify_camera_fov_and_occlusion(camera_i1, point, trimesh_mesh)
-            uv_i2 = verify_camera_fov_and_occlusion(camera_i2, point, trimesh_mesh)
-            if uv_i1 is not None and uv_i2 is not None:
-                keypoints_i1.append(uv_i1)
-                keypoints_i2.append(uv_i2)
-
-            visualize = False
-            if visualize:
-                visualize_ray_to_sampled_mesh_point(camera_i1, point, wTi_list, calibrations, mesh)
-
-        keypoints_i1 = Keypoints(coordinates=np.array(keypoints_i1))
-        keypoints_i2 = Keypoints(coordinates=np.array(keypoints_i2))
-        print(f"Generated {len(keypoints_i1)} keypoints from sampled {points.shape[0]} 3d points.")
-        return keypoints_i1, keypoints_i2
-
-
-def visualize_ray_to_sampled_mesh_point(
-    camera: gtsfm_types.CAMERA_TYPE,
-    point: np.ndarray,
-    wTi_list: List[Pose3],
-    calibrations: List[gtsfm_types.CALIBRATION_TYPE],
-    mesh: open3d.geometry.TriangleMesh,
-) -> None:
-    """Visualizes ray from camera center to 3d point, along with camera frustum, mesh, and 3d point as ball.
-
-    Args:
-        camera: Camera to use.
-        point: 3d point as (3,) array.
-        wTi_list: All camera poses.
-        calibrations: Calibration for each camera.
-        mesh:
-    """
-    frustums = open3d_vis_utils.create_all_frustums_open3d(wTi_list, calibrations, frustum_ray_len=0.3)
-
-    # Create line segment to represent ray.
-    cam_center = camera.pose().translation()
-    ray_dirs = point - camera.pose().translation()
-    line_set = _make_line_plot(cam_center, cam_center + ray_dirs)
-    # line_set = _make_line_plot(cam_center, camera.backproject(uv_reprojected, depth=1.0))
-
-    # Plot 3d point as red sphere.
-    point_cloud = np.reshape(point, (1, 3))
-    rgb = np.array([255, 0, 0]).reshape(1, 3).astype(np.uint8)
-    spheres = open3d_vis_utils.create_colored_spheres_open3d(point_cloud, rgb, sphere_radius=0.5)
-
-    # Plot all camera frustums and mesh, with sphere and ray line segment.
-    open3d.visualization.draw_geometries([mesh] + frustums + spheres + [line_set])
-
-
-def verify_camera_fov_and_occlusion(
-    camera: gtsfm_types.CAMERA_TYPE,
-    point: np.ndarray,
-    trimesh_mesh: trimesh.Trimesh,
-) -> Optional[np.ndarray]:
-    """Verifies point lies within camera FOV and is unoccluded by mesh faces.
-
-    Args:
-        camera: Camera to use.
-        point: 3d point as (3,) array.
-        trimesh_mesh: Trimesh mesh object to raycast against.
-
-    Returns:
-        2d keypoint as (2,) array.
-    """
-    # Project to camera.
-    uv_reprojected, success_flag = camera.projectSafe(point)
-    # Check for projection error in camera.
-    if not success_flag:
-        return None
-
-    if (
-        (uv_reprojected[0] < 0)
-        or (uv_reprojected[0] > _DEFAULT_IMAGE_WIDTH_PX)
-        or (uv_reprojected[1] < 0)
-        or (uv_reprojected[1] > _DEFAULT_IMAGE_HEIGHT_PX)
-    ):
-        # Outside of synthetic camera's FOV.
-        return None
-
-    # Cast ray through keypoint back towards scene.
-    cam_center = camera.pose().translation()
-    # Choose an arbitrary depth for the ray direction.
-    ray_dirs = point - camera.pose().translation()
-
-    # Returns the location of where a ray hits a surface mesh.
-    # keypoint_ind: (M,) array of keypoint indices whose corresponding ray intersected the ground truth mesh.
-    # intersections_locations: (M, 3), array of ray intersection locations.
-    intersections, keypoint_ind, _ = trimesh_mesh.ray.intersects_location(
-        ray_origins=cam_center.reshape(1, 3), ray_directions=ray_dirs.reshape(1, 3), multiple_hits=False
-    )
-
-    if intersections.shape[0] > 1 or intersections.shape[0] == 0:
-        print(f"Unknown failure: intersections= {intersections} with shape {intersections.shape}")
-        return None
-
-    # TODO(johnwlambert): Tune this tolerance threshold to allow looser matches.
-    eps = 0.1
-    if not np.linalg.norm(intersections[0] - point) < eps:
-        # print("Skip occluded: ", intersections[0], ", vs. : ", point)
-        # There was a closer intersection along the ray than `point`, meaning `point` is occluded by the mesh.
-        return None
-
-    return uv_reprojected
 
 
 def crop_points_to_bounding_polyhedron(pcd: open3d.geometry.PointCloud, json_fpath: str) -> open3d.geometry.PointCloud:
@@ -526,35 +323,6 @@ def _parse_redwood_data_log_file(log_fpath: str) -> Dict[int, Pose3]:
         wTi_gt = np.array([parse_matrix_row(line) for line in lines])
         wTi_gt_dict[cam_idx] = Pose3(wTi_gt)
     return wTi_gt_dict
-
-
-def _make_line_plot(point1: np.ndarray, point2: np.ndarray) -> open3d.geometry.LineSet:
-    """ """
-    verts_worldfr = np.array([point1, point2])
-    lines = [[0, 1]]
-    # color is in range [0,1]
-    color = (0, 0, 1)
-    colors = [color for i in range(len(lines))]
-
-    line_set = open3d.geometry.LineSet(
-        points=open3d.utility.Vector3dVector(verts_worldfr),
-        lines=open3d.utility.Vector2iVector(lines),
-    )
-    line_set.colors = open3d.utility.Vector3dVector(colors)
-    return line_set
-
-
-def load_from_trimesh(mesh_path: str) -> trimesh.Trimesh:
-    """Read in scene mesh as Trimesh object."""
-    if not Path(mesh_path).exists():
-        raise FileNotFoundError(f"No mesh found at {mesh_path}")
-    mesh = trimesh.load(mesh_path, process=False, maintain_order=True)
-    logger.info(
-        "Tanks & Temples loader read in mesh with %d vertices and %d faces.",
-        mesh.vertices.shape[0],
-        mesh.faces.shape[0],
-    )
-    return mesh
 
 
 def _create_Sim3_from_tt_dataset_alignment_transform(lidar_Sim3_colmap: np.ndarray) -> np.ndarray:
