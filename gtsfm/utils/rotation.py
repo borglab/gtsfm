e6d200a5ecf1625ddd85de61d2c5d1df71eeab94
senselessdev1 <johnlambert@jl-macbook-pro-19.lan>
2024-03-11 16:23:24 -0400

    python black fixes

--- gtsfm/utils/rotation.py
+++ gtsfm/utils/rotation.py
@@ -44,13 +44,12 @@ def initialize_global_rotations_using_mst(
 
     # Ignore 0th node, as we already set its global pose as the origin
     for dst_node in list(G.nodes)[1:]:
-
         # Determine the path to this node from the origin. ordered from [origin_node,...,dst_node]
         path = nx.shortest_path(G, source=origin_node, target=dst_node)
 
         # Chain relative rotations w.r.t. origin node. Initialize as identity Rot3 w.r.t origin node `i1`.
         wRi1 = Rot3()
-        for (i1, i2) in zip(path[:-1], path[1:]):
+        for i1, i2 in zip(path[:-1], path[1:]):
             # NOTE: i1, i2 may not be in sorted order here. May need to reverse ordering.
             if i1 < i2:
                 i1Ri2 = i2Ri1_dict[(i1, i2)].inverse()
@@ -64,10 +63,9 @@ def initialize_global_rotations_using_mst(
     return wRi_list
 
 
-
 # def initialize_mst(
-#         num_images: int, 
-#         i2Ri1_dict: Dict[Tuple[int, int], Optional[Rot3]], 
+#         num_images: int,
+#         i2Ri1_dict: Dict[Tuple[int, int], Optional[Rot3]],
 #         corr_idxs: Dict[Tuple[int, int], np.ndarray],
 #         old_to_new_idxs: Dict[int, int],
 #     ) -> gtsam.Values:
@@ -76,8 +74,8 @@ def initialize_global_rotations_using_mst(
 #     Args:
 #         num_images: Number of images in the scene.
 #         i2Ri1_dict: Dictionary of relative rotations (i1, i2): i2Ri1.
-#         corr_idxs: 
-#         old_to_new_idxs: 
+#         corr_idxs:
+#         old_to_new_idxs:
 
 #     Returns:
 #         Initialization of global rotations for Values.
@@ -120,10 +118,10 @@ def initialize_global_rotations_using_mst(
 #         # if iter >= max_iters:
 #         #     logger.info("Reached max MST iters.")
 #         #     assert False
-    
+
 #     # Add to Values object.
 #     initial = gtsam.Values()
 #     for i, iR0 in iR0_dict.items():
 #         initial.insert(old_to_new_idxs[i], Rot3(iR0))
-    
+
 #     return initial
