8e0bafe5f5c86a01e3d5008721e20f100f19dc30
senselessdev1 <johnlambert@jl-macbook-pro-19.lan>
2024-03-11 16:22:17 -0400

    clean up notation

--- gtsfm/utils/geometry_comparisons.py
+++ gtsfm/utils/geometry_comparisons.py
@@ -30,7 +30,8 @@ def compare_rotations(
     Args:
         aTi_list: 1st list of rotations.
         bTi_list: 2nd list of rotations.
-        angular_error_threshold_degrees: the threshold for angular error between two rotations.
+        angular_error_threshold_degrees: Threshold for angular error between two rotations.
+    
     Returns:
         Result of the comparison.
     """
