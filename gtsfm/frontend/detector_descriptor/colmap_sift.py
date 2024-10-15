fb227a2eb7340a055c707e6e64654b676dccc049
Travis Driver <drivertp@gmail.com>
2024-10-14 21:13:44 -0700

    Test with Colmap SIFT Features

---
+++ gtsfm/frontend/detector_descriptor/colmap_sift.py
@@ -0,0 +1,58 @@
+"""SIFT Detector-Descriptor implementation.
+
+The detector was proposed in 'Distinctive Image Features from Scale-Invariant Keypoints' and is implemented by wrapping
+over OpenCV's API.
+
+References:
+- https://www.cs.ubc.ca/~lowe/papers/ijcv04.pdf
+- https://docs.opencv.org/3.4.2/d5/d3c/classcv_1_1xfeatures2d_1_1SIFT.html
+
+Authors: Ayush Baid
+"""
+from typing import Tuple
+
+import numpy as np
+import pycolmap
+
+import gtsfm.utils.features as feature_utils
+import gtsfm.utils.images as image_utils
+from gtsfm.common.image import Image
+from gtsfm.common.keypoints import Keypoints
+from gtsfm.frontend.detector_descriptor.detector_descriptor_base import DetectorDescriptorBase
+
+
+class ColmapSIFTDetectorDescriptor(DetectorDescriptorBase):
+    """SIFT detector-descriptor using OpenCV's implementation."""
+
+    def detect_and_describe(self, image: Image) -> Tuple[Keypoints, np.ndarray]:
+        """Perform feature detection as well as their description.
+
+        Refer to detect() in DetectorBase and describe() in DescriptorBase for details about the output format.
+
+        Args:
+            image: the input image.
+
+        Returns:
+            Detected keypoints, with length N <= max_keypoints.
+            Corr. descriptors, of shape (N, D) where D is the dimension of each descriptor.
+        """
+
+        # Convert to grayscale.
+        gray_image = image_utils.rgb_to_gray_cv(image)
+
+        # Create OpenCV object every time as the object is not pickle-able.
+        # TODO (travisdriver): Add GPU support
+        colmap_obj = pycolmap.Sift()
+
+        # Run the OpenCV code.
+        features, descriptors = colmap_obj.extract(gray_image.value_array)
+
+        # Convert to GTSFM's keypoints.
+        # TODO (travisdriver): Add scales and orientations
+        keypoints = Keypoints(coordinates=features[..., :2])
+
+        # # Filter features.
+        # keypoints, selection_idxs = keypoints.get_top_k(self.max_keypoints)
+        # descriptors = descriptors[selection_idxs]
+
+        return keypoints, descriptors
---
+++ tests/frontend/detector_descriptor/test_colmap_sift.py
@@ -0,0 +1,28 @@
+"""Tests for SIFT detector descriptor
+
+Authors: Ayush Baid
+"""
+import unittest
+
+import tests.frontend.detector_descriptor.test_detector_descriptor_base as test_detector_descriptor_base
+from gtsfm.frontend.detector.detector_from_joint_detector_descriptor import DetectorFromDetectorDescriptor
+from gtsfm.frontend.detector_descriptor.colmap_sift import ColmapSIFTDetectorDescriptor
+
+
+class TestColmapSIFTDetectorDescriptor(test_detector_descriptor_base.TestDetectorDescriptorBase):
+    """Main test class for detector-description combination base class in frontend.
+
+    All unit test functions defined in TestDetectorDescriptorBase are run automatically.
+    """
+
+    def setUp(self):
+        """Setup the attributes for the tests."""
+        super().setUp()
+        self.detector_descriptor = ColmapSIFTDetectorDescriptor()
+
+        # explicitly set the detector
+        self.detector = DetectorFromDetectorDescriptor(self.detector_descriptor)
+
+
+if __name__ == "__main__":
+    unittest.main()
