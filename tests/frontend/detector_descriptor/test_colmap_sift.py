02bf2d0dec39d13301b37025a2918206af19b42e
Travis Driver <drivertp@gmail.com>
2024-11-05 14:43:43 -0800

    Fix ColmapSift unit test and update documentation

--- tests/frontend/detector_descriptor/test_colmap_sift.py
+++ tests/frontend/detector_descriptor/test_colmap_sift.py
@@ -18,7 +18,11 @@ class TestColmapSIFTDetectorDescriptor(test_detector_descriptor_base.TestDetecto
     def setUp(self):
         """Setup the attributes for the tests."""
         super().setUp()
-        self.detector_descriptor = ColmapSIFTDetectorDescriptor()
+        # Note: pycolmap does not guarantee that the number of keypoints will not exceed the specified maximum, as there
+        # can be ties in terms of the response scores. E.g., if the 5000th keypoint and the 5001st keypoint have the
+        # same response, pycolmap will return 5001 keypoints. Setting the number of maximum keypoints lower reduces the
+        # risk of this happening.
+        self.detector_descriptor = ColmapSIFTDetectorDescriptor(max_keypoints=2000)
 
         # explicitly set the detector
         self.detector = DetectorFromDetectorDescriptor(self.detector_descriptor)
