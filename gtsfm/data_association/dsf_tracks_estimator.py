65e6e77885c37338ade995c895f8ec444fcc9743
akshay-krishnan <akshay.krishnan.30@gmail.com>
2025-07-15 04:08:05 -0400

    change to np.nan

--- gtsfm/data_association/dsf_tracks_estimator.py
+++ gtsfm/data_association/dsf_tracks_estimator.py
@@ -84,7 +84,7 @@ class DsfTracksEstimator(TracksEstimatorBase):
             else:
                 erroneous_track_count += 1
 
-        erroneous_track_pct = erroneous_track_count / len(key_set) * 100 if len(key_set) > 0 else np.NaN
+        erroneous_track_pct = erroneous_track_count / len(key_set) * 100 if len(key_set) > 0 else np.nan
         logger.info(
             f"DSF Union-Find: {erroneous_track_pct:.2f}% of tracks discarded from multiple obs. in a single image."
         )
