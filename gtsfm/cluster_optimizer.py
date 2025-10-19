134fcbece0925d0dcd4c3d640d934356211ff40e
Frank Dellaert <dellaert@gmail.com>
2025-10-19 15:34:58 -0400

    Re-add post-ISP report

--- gtsfm/cluster_optimizer.py
+++ gtsfm/cluster_optimizer.py
@@ -355,6 +355,14 @@ class ClusterOptimizer:
         if output_paths.metrics_sink is not None:
             side_effect_tasks.append(
                 delayed(two_view_estimator.aggregate_frontend_metrics)(
+                    post_isp_reports_graph,
+                    self._pose_angular_error_thresh,
+                    metric_group_name=f"verifier_summary_{two_view_estimator.POST_ISP_REPORT_TAG}",
+                    metrics_sink=output_paths.metrics_sink,
+                )
+            )
+            side_effect_tasks.append(
+                delayed(two_view_estimator.aggregate_frontend_metrics)(
                     view_graph_two_view_reports,
                     self._pose_angular_error_thresh,
                     metric_group_name=f"verifier_summary_{two_view_estimator.VIEWGRAPH_REPORT_TAG}",
