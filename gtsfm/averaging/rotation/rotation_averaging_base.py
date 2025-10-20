Fix averaging tests

gtsfm/averaging/rotation/rotation_averaging_base.py
@@ -27,6 +27,7 @@ class RotationAveragingBase(GTSFMProcess):
     rotations.
     """
 
+    @staticmethod
     def get_ui_metadata() -> UiMetadata:
         """Returns data needed to display node and edge info for this process in the process graph."""
 
@@ -129,7 +130,7 @@ class RotationAveragingBase(GTSFMProcess):
         v_corr_idxs: Dict[Tuple[int, int], np.ndarray],
         gt_wTi_list: List[Optional[Pose3]],
         outputs: Optional[Outputs] = None,
-    ) -> Delayed:
+    ) -> Tuple[Delayed, List[Delayed]]:
         """Create the computation graph for performing rotation averaging.
 
         Args:
@@ -144,7 +145,7 @@ class RotationAveragingBase(GTSFMProcess):
             Global rotations wrapped using dask.delayed.
         """
 
-        return dask.delayed(self._run_rotation_averaging_base)(
+        rotation_graph = dask.delayed(self._run_rotation_averaging_base)(  # type: ignore
             num_images,
             i2Ri1_dict=i2Ri1_graph,
             i1Ti2_priors=i1Ti2_priors,
@@ -152,3 +153,5 @@ class RotationAveragingBase(GTSFMProcess):
             wTi_gt=gt_wTi_list,
             outputs=outputs,
         )
+
+        return rotation_graph, []

tests/averaging/translation/test_averaging_1dsfm.py
@@ -62,9 +62,9 @@ class TestTranslationAveraging1DSFM(unittest.TestCase):
         """Tests that the binary measurements are correctly extracted from priors dict."""
 
         test_prior = PosePrior(
-            value=Pose3(Rot3(), np.array([1, 1, 0])), covariance=np.eye(6), type=PosePriorType.SOFT_CONSTRAINT
+            value=Pose3(Rot3(), Point3(1, 1, 0)), covariance=np.eye(6), type=PosePriorType.SOFT_CONSTRAINT
         )
-        test_wRi_mat = np.eye(3)
+        test_wRi_mat: np.ndarray = np.eye(3)
         test_wRi_mat[1, 1] = -1
         wRi_list = [Rot3(test_wRi_mat)] * 4
         i2Ti1_dict = {(0, 1): test_prior, (0, 3): test_prior, (1, 2): test_prior}
@@ -114,7 +114,7 @@ class TestTranslationAveraging1DSFM(unittest.TestCase):
         tracks = [SfmTrack2d(measurements=[SfmMeasurement(0, test_measurement)])]
 
         intrinsics = [Cal3_S2(20, 20, 0.0, 0, 0)]
-        test_wRi_mat = np.eye(3)
+        test_wRi_mat: np.ndarray = np.eye(3)
         test_wRi_mat[1, 1] = -1  # flip y axis just to check if rotation is being applied.
         wRi_list = [Rot3(test_wRi_mat)]
 