f9b1514a13ed3559524d77a238b7c582668165ad
Kathirvel Gounder <kathirgounder@gmail.com>
2025-10-20 20:25:48 -0400

    Cacher error thing

--- gtsfm/retriever/image_pairs_generator.py
+++ gtsfm/retriever/image_pairs_generator.py
@@ -54,7 +54,8 @@ class ImagePairsGenerator:
         
         if self._global_descriptor is not None:
             # Ensure Model has loaded on client before scattering, otherwise we get a race condition
-            self._global_descriptor._ensure_model_loaded()
+            # Access the inner object first.
+            self._global_descriptor._global_descriptor_obj._ensure_model_loaded()
 
             # Scatter descriptor to all workers for efficient parallel processing
             global_descriptor_future = client.scatter(self._global_descriptor, broadcast=False)
