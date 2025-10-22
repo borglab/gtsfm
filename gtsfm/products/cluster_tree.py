37bd626963bcbae7b9d5a607884aeb16ab8f9fe7
Frank Dellaert <dellaert@gmail.com>
2025-10-21 23:11:43 -0400

    Show how many keys

--- gtsfm/products/cluster_tree.py
+++ gtsfm/products/cluster_tree.py
@@ -60,8 +60,8 @@ class ClusterTree(Tree[VisibilityGraph]):
             prefix = " " * indent
             node_type = "Leaf" if node.is_leaf() else "Cluster"
             # Sorting keys makes the output deterministic and easier to read
-            keys_str = sorted(list(node.local_keys()))
-            lines = [f"{prefix}{node_type}: {keys_str}"]
+            keys = sorted(list(node.local_keys()))
+            lines = [f"{prefix}{node_type} ({len(keys)}): {keys}"]
             for child in node._child_clusters():
                 lines.append(repr_helper(child, indent + 2))
             return "\n".join(lines)
