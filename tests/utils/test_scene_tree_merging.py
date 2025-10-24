071d2bdb4eeb3d49bd893d9ae7a306d5be94c590
Frank Dellaert <dellaert@gmail.com>
2025-10-24 00:47:16 -0400

    Working version, 1M points!

--- tests/utils/test_scene_tree_merging.py
+++ tests/utils/test_scene_tree_merging.py
@@ -8,6 +8,9 @@ import random
 import unittest
 from pathlib import Path
 
+import numpy as np
+from gtsam import Similarity3  # type: ignore
+
 import gtsfm.products.scene_tree as scene_tree
 import gtsfm.utils.merging as merging_utils
 from gtsfm.common.gtsfm_data import GtsfmData
@@ -27,14 +30,35 @@ def merge(a: GtsfmData, b: GtsfmData) -> GtsfmData:
     Returns:
         Merged GtsfmData, a new object. Coordinate frame A is used as prime.
     """
-    print(f"merge A:{a}, {sorted(a.poses().keys())[:3]}")
-    print(f"merge B:{b}, {sorted(b.poses().keys())[:3]}")
+    ap = a.poses()
+    bp = b.poses()
+    print(f"merge A:{a}, {sorted(ap.keys())[:3]}")
+    print(f"merge B:{b}, {sorted(bp.keys())[:3]}")
     try:
-        aTb = merging_utils.calculate_transform(a.poses(), b.poses())
+        aTb = merging_utils.calculate_transform(ap, bp)
         print(f"Calculated aTb: {aTb}")
     except ValueError:
         print("Cannot merge GtsfmData objects.")
         return a
+
+    # poor man scale estimation
+    common_keys = [i for i in ap if i in bp]
+
+    # For some random pair in common_keys, print out ratio of distances before and after
+    i, j = common_keys[0], common_keys[-1]
+    b_norm = np.linalg.norm(bp[i].translation() - bp[j].translation())
+    a_norm = np.linalg.norm(ap[i].translation() - ap[j].translation())
+    scale = float(a_norm / b_norm)
+    print(f"Distance ratio (b/a) for {i}, {j}: {scale}")
+
+    aSb1 = Similarity3(aTb.rotation().matrix(), aTb.translation(), scale)
+    print(f"Calculated aSb1: {aSb1}")
+
+    pose_pairs = [(ap[i], bp[i]) for i in common_keys]
+    aSb2 = Similarity3.Align(pose_pairs)
+    print(f"Calculated aSb2 from Similarity3.Align: {aSb2}")
+
+    aSb = aSb2
 
     # Create merged cameras with updated poses. Only b-poses need to be updated.
     merged_cameras = a.cameras().copy()
@@ -42,13 +66,13 @@ def merge(a: GtsfmData, b: GtsfmData) -> GtsfmData:
         # TODO: what to do if we have conflicting calibrations?
         if i not in merged_cameras:
             bTi = cam.pose()
-            merged_cameras[i] = create_camera(aTb * bTi, cam.calibration())
+            merged_cameras[i] = create_camera(aSb.transformFrom(bTi), cam.calibration())
 
     # Create merged tracks
     merged_tracks = a.tracks().copy()
     # For all b_tracks, update the point by multiplying with aTb:
     for track in b.tracks():
-        track.p = aTb.transformFrom(track.p)  # from b to a
+        track.p = aSb.transformFrom(track.p)  # from b to a
         merged_tracks.append(track)
 
     # Create merged GtsfmData
@@ -98,8 +122,19 @@ class TestClusterTreeIO(unittest.TestCase):
         """Test reading the COLMAP hierarchy as a tree of (Path, GtsfmData)."""
         # TODO(frank): we need somehow to have images names and shapes if we want to save.
         # Add into GtsfmData?
-        tree = scene_tree.read_colmap(str(self.base_dir))
+        palace_metis = Path(__file__).resolve().parent.parent.parent / "palace_metis"
+        print("Reading COLMAP tree from", palace_metis)
+        tree = scene_tree.read_colmap(str(palace_metis), name="sparse")
+        # tree = scene_tree.read_colmap(str(self.base_dir))
         assert tree is not None
+
+        pseudo_color = False
+        down_sample = True
+
+        if down_sample:
+            seed = 42
+            fraction_points_to_keep = 0.1
+            tree = scene_tree.downsample(tree, fraction_points_to_keep=fraction_points_to_keep, seed=seed)
 
         with open(self.base_dir / "cluster_tree.pkl", "rb") as f:
             cluster_tree: ClusterTree = pickle.load(f)
@@ -112,14 +147,15 @@ class TestClusterTreeIO(unittest.TestCase):
             new_data = GtsfmData(len(new_cameras), new_cameras, [])
             # HACK(Frank): don't remap tracks for now, just copy them over.
             new_data._tracks = scene.tracks().copy()
-            r = random.randint(0, 255)
-            g = random.randint(0, 255)
-            b = random.randint(0, 255)
-            print(f"Reordering scene at {path}, color=({r},{g},{b})")
-            for track in new_data.tracks():
-                track.r = r
-                track.g = g
-                track.b = b
+            if pseudo_color:
+                r = random.randint(0, 255)
+                g = random.randint(0, 255)
+                b = random.randint(0, 255)
+                print(f"\033[38;2;{r};{g};{b}mReordering scene at {path}, color=({r},{g},{b})\033[0m")
+                for track in new_data.tracks():
+                    track.r = r
+                    track.g = g
+                    track.b = b
             return path, new_data
 
         print(cluster_tree)
@@ -137,7 +173,7 @@ class TestClusterTreeIO(unittest.TestCase):
         image_filenames = [f"San_Francisco_{i:04d}.jpg" for i in range(281)]  # Dummy filenames
         image_shapes = [(1936, 1296)] * len(image_filenames)  # Dummy shapes
         merged_scene.export_as_colmap_text(
-            TEST_DATA_ROOT / "palace_merged_colmap" / "sparse",
+            TEST_DATA_ROOT / "palace_merged_colmap" / "color-10%",
             image_filenames=image_filenames,
             image_shapes=image_shapes,
         )
