6983c2c4d9d0ac14f78f0bc21e00b67bfba2f97d
dellaert@gmail.com <dellaert@gmail.com>
2025-11-03 16:07:19 -0500

    Tracking params in Hydra

--- gtsfm/cluster_optimizer/cluster_vggt.py
+++ gtsfm/cluster_optimizer/cluster_vggt.py
@@ -124,6 +124,9 @@ class ClusterVGGT(ClusterOptimizerBase):
         inference_resolution: int = 518,
         conf_threshold: float = 5.0,
         max_num_points: int = 100000,
+        tracking_max_query_pts: int = 1000,
+        tracking_query_frame_num: int = 4,
+        tracking_fine_tracking: bool = True,
         camera_type: str = "PINHOLE",
         seed: int = 42,
         copy_results_to_react: bool = True,
@@ -141,6 +144,9 @@ class ClusterVGGT(ClusterOptimizerBase):
         self._inference_resolution = inference_resolution
         self._conf_threshold = conf_threshold
         self._max_points_for_colmap = max_num_points
+        self._tracking_max_query_pts = tracking_max_query_pts
+        self._tracking_query_frame_num = tracking_query_frame_num
+        self._tracking_fine_tracking = tracking_fine_tracking
         self._camera_type = camera_type
         self._seed = seed
         self._copy_results_to_react = copy_results_to_react
@@ -195,6 +201,9 @@ class ClusterVGGT(ClusterOptimizerBase):
             img_load_resolution=self._image_load_resolution,
             confidence_threshold=self._conf_threshold,
             max_num_points=self._max_points_for_colmap,
+            max_query_pts=self._tracking_max_query_pts,
+            query_frame_num=self._tracking_query_frame_num,
+            fine_tracking=self._tracking_fine_tracking,
             tracking=True,
         )
 
