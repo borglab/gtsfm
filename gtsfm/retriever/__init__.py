44e5a4aad7ff1c969e81e751673eb8819bf1f36c
Kathir Gounder <kathirgounder@gmail.com>
2026-01-29 22:38:16 -0800

    Fixes

--- gtsfm/retriever/__init__.py
+++ gtsfm/retriever/__init__.py
@@ -10,7 +10,7 @@ from .exhaustive_retriever import ExhaustiveRetriever
 from .joint_similarity_sequential_retriever import JointSimilaritySequentialRetriever
 from .sequential_retriever import SequentialRetriever
 from .similarity_retriever import SimilarityRetriever
-from .faiss_retriever import FaissRetriever
+from .batched_similarity_retriever import BatchedSimilarityRetriever
 
 Exhaustive = ExhaustiveRetriever
 JointSimilaritySequential = JointSimilaritySequentialRetriever
